1b53ba15f5706b10ecdbf1b09783ccd8e3ed6b1a
Joe McBride <joe@uicraftsman.com>
2015-07-09 09:09:30 -0700

    Use Expect.Throws.

--- src/GraphQL.Tests/Types/FieldRegistrationTests.cs
+++ src/GraphQL.Tests/Types/FieldRegistrationTests.cs
@@ -1,54 +1,18 @@
 ﻿using System;
-using Should;
 
 namespace GraphQL.Tests.Types
 {
     public class FieldRegistrationTests
     {
         [Test]
-        public void throws_error_when_trying_to_register_type_with_same_name()
+        public void throws_error_when_trying_to_register_field_with_same_name()
         {
             var graphType = new ObjectGraphType();
             graphType.Field<StringGraphType>("id");
 
-            Exception exc = null;
-
-            try
-            {
-                graphType.Field<StringGraphType>("id");
-            }
-            catch (Exception ex)
-            {
-                exc = ex;
-            }
-            finally
-            {
-                exc.ShouldNotBeNull();
-                exc.ShouldBeType<ArgumentOutOfRangeException>();
-            }
-        }
-    }
-
-    public static class Expect
-    {
-        public static void Throws<TException>(Action action)
-            where TException : Exception
-        {
-            var threwException = false;
-
-            try
-            {
-                action();
-            }
-            catch (Exception exc)
-            {
-                threwException = true;
-                exc.ShouldBeType<TException>();
-            }
-            finally
-            {
-                threwException.ShouldBeTrue("Expected exception '{0}' was not thrown.".ToFormat(typeof(TException).Name));
-            }
+            Expect.Throws<ArgumentOutOfRangeException>(
+                () => graphType.Field<StringGraphType>("id")
+            );
         }
     }
 }
