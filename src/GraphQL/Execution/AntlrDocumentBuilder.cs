15f8146d861d72527b310ebe78141f659569cba9
Joe McBride <joe@uicraftsman.com>
2015-12-11 14:07:11 -0800

    Clean up stream after use.

--- src/GraphQL/Execution/AntlrDocumentBuilder.cs
+++ src/GraphQL/Execution/AntlrDocumentBuilder.cs
@@ -10,15 +10,17 @@ namespace GraphQL.Execution
     {
         public Document Build(string data)
         {
-            var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
-            var reader = new StreamReader(stream);
-            var input = new AntlrInputStream(reader);
-            var lexer = new GraphQLLexer(input);
-            var tokens = new CommonTokenStream(lexer);
-            var parser = new GraphQLParser(tokens);
-            var documentTree = parser.document();
-            var vistor = new GraphQLVisitor();
-            return vistor.Visit(documentTree) as Document;
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
+            using (var reader = new StreamReader(stream))
+            {
+                var input = new AntlrInputStream(reader);
+                var lexer = new GraphQLLexer(input);
+                var tokens = new CommonTokenStream(lexer);
+                var parser = new GraphQLParser(tokens);
+                var documentTree = parser.document();
+                var vistor = new GraphQLVisitor();
+                return vistor.Visit(documentTree) as Document;
+            }
         }
     }
 }
