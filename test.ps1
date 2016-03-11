bc9687a8353e94575bfa434d161453d1987d87c6
Joe McBride <joe@uicraftsman.com>
2016-03-11 15:27:42 -0800

    Check exit code and throw error if > 0.

--- test.ps1
+++ test.ps1
@@ -1,6 +1,18 @@
 # run tests
-.\packages\Fixie.1.0.0.33\lib\net45\fixie.console.exe .\src\GraphQL.Tests\bin\debug\GraphQL.Tests.dll --xUnitXml .\fixie-results.xml
+# run tests
+$output = (.\packages\Fixie.1.0.0.33\lib\net45\fixie.console.exe .\src\GraphQL.Tests\bin\debug\GraphQL.Tests.dll --xUnitXml .\fixie-results.xml) -join "`r`n"
+
+$testResult = $LASTEXITCODE
+
+if ($testResult -eq 0)
+{
+  Write-Host $output
+}
 
 # upload results to AppVeyor
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\fixie-results.xml))
+
+if ($testResult -ne 0) {
+    throw "{0}." -f ([string] $output)
+}
