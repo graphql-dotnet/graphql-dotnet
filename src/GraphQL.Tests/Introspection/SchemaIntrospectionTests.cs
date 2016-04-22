2fa16b5e8941bc4d66b7296da54ed41eda296e6f
Joe McBride <joe@uicraftsman.com>
2016-04-22 16:20:57 -0700

    Add back checking for List/NonNull types in ofType.

---
+++ src/GraphQL.Tests/Introspection/SchemaIntrospectionTests.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Http;
+using GraphQL.Introspection;
+using GraphQL.Types;
+using Should;
+
+namespace GraphQL.Tests.Introspection
+{
+    public class SchemaIntrospectionTests
+    {
+        [Test]
+        public void validate_core_schema()
+        {
+            var documentExecuter = new DocumentExecuter();
+            var executionResult = documentExecuter.ExecuteAsync(new Schema(), null, SchemaIntrospection.IntrospectionQuery, null).Result;
+            var json = new DocumentWriter(true).Write(executionResult.Data);
+
+            json.ShouldEqual(IntrospectionResult.Data);
+        }
+    }
+}
