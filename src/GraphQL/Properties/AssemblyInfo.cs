c70db05f9d3cd5c56e66a60df26b295b96ccdf82
Joe McBride <joe@uicraftsman.com>
2016-05-11 17:15:16 -0700

    Bump to 0.6.6

--- src/GraphQL/Properties/AssemblyInfo.cs
+++ src/GraphQL/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyProduct("GraphQL")]
 [assembly: AssemblyDescription("GraphQL for .NET")]
 [assembly: AssemblyCopyright("Copyright 2015-2016 Joseph T. McBride, et al.  All rights reserved.")]
-[assembly: AssemblyVersion("0.6.5.0")]
-[assembly: AssemblyFileVersion("0.6.5.0")]
+[assembly: AssemblyVersion("0.6.6.0")]
+[assembly: AssemblyFileVersion("0.6.6.0")]
