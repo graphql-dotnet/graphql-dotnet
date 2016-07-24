52c9406d63936442668b171ba3ce881876326812
Joe McBride <joe@uicraftsman.com>
2016-07-23 23:35:33 -0700

    KnownTypeNames validation rule
    * Re-order validation rules based on presidence
    * Add StringUtils which includes a QuotedOrList and a SuggestionList
      which is based on string similarity of a given misspelled input and
      possible matching options.

---
+++ src/GraphQL.Tests/Utilities/StringUtilsTests.cs
@@ -0,0 +1,26 @@
+﻿using GraphQL.Utilities;
+using Should;
+
+namespace GraphQL.Tests.Utilities
+{
+    public class StringUtilsTests
+    {
+        [Fact]
+        public void quoted_or_list_one()
+        {
+            StringUtils.QuotedOrList(new[] {"A"}).ShouldEqual("\"A\"");
+        }
+
+        [Fact]
+        public void quoted_or_list_two()
+        {
+            StringUtils.QuotedOrList(new[] {"A", "B"}).ShouldEqual("\"A\" or \"B\"");
+        }
+
+        [Fact]
+        public void quoted_or_list_three()
+        {
+            StringUtils.QuotedOrList(new[] {"A", "B", "C"}).ShouldEqual("\"A\", \"B\", or \"C\"");
+        }
+    }
+}
---
+++ src/GraphQL.Tests/Validation/KnownTypeNamesTests.cs
@@ -0,0 +1,47 @@
+﻿using GraphQL.Validation.Rules;
+
+namespace GraphQL.Tests.Validation
+{
+    public class KnownTypeNamesTests : ValidationTestBase<KnownTypeNames, ValidationSchema>
+    {
+        [Fact]
+        public void known_type_names_are_valid()
+        {
+            ShouldPassRule(@"
+              query Foo($var: String, $required: [String!]!) {
+                user(id: 4) {
+                  pets {
+                    ... on Pet { name },
+                    ...PetFields
+                  }
+                }
+              }
+              fragment PetFields on Pet {
+                name
+              }
+            ");
+        }
+
+        [Fact]
+        public void unknown_type_names_are_invalid()
+        {
+            ShouldFailRule(_ =>
+            {
+                _.Query = @"
+                  query Foo($var: JumbledUpLetters) {
+                    user(id: 4) {
+                      name
+                      pets { ... on Badger { name }, ...PetFields }
+                    }
+                  }
+                  fragment PetFields on Peettt {
+                    name
+                  }
+                ";
+                _.Error(Rule.UnknownTypeMessage("JumbledUpLetters", null), 2, 35);
+                _.Error(Rule.UnknownTypeMessage("Badger", null), 5, 37);
+                _.Error(Rule.UnknownTypeMessage("Peettt", new[] {"Pet"}), 8, 41);
+            });
+        }
+    }
+}
