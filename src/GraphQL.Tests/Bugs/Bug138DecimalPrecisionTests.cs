88a0084730b75a759f60bdcbeec55a375f46c25f
Joe McBride <joe@uicraftsman.com>
2016-08-16 23:15:55 -0700

    Switch to double.Parse for FloatValue
    * When FloatValue was updated to use double the GraphQLVisitor was
      not updated to match.

---
+++ src/GraphQL.Tests/Bugs/Bug138DecimalPrecisionTests.cs
@@ -0,0 +1,46 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL.Tests.Bugs
+{
+    public class Bug138DecimalPrecisionTests : QueryTestBase<DecimalSchema>
+    {
+        [Fact]
+        public void double_to_decimal_does_not_lose_precision()
+        {
+            var query = @"
+                mutation SetState{
+                    set(request:24.15)
+                }
+            ";
+
+            var expected = @"{
+              set: 24.15
+            }";
+
+            AssertQuerySuccess(query, expected);
+        }
+    }
+
+    public class DecimalSchema : Schema
+    {
+        public DecimalSchema()
+        {
+            Mutation = new DecimalMutation();
+        }
+    }
+
+    public class DecimalMutation : ObjectGraphType
+    {
+        public DecimalMutation()
+        {
+            Field<DecimalGraphType>(
+                "set",
+                arguments: new QueryArguments(new QueryArgument<DecimalGraphType> { Name = "request"}),
+                resolve: context =>
+                {
+                    var val = context.Argument<decimal>("request");
+                    return val;
+                });
+        }
+    }
+}
