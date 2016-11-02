a2667f6f87525bcee029849514e8f4136cb67965
Farzad E <dme@dnetguru.com>
2016-11-02 02:33:45 -0700

    Works fine for now, need to add fragments (non-internal).

--- src/GraphQL/Validation/ComplexityAnalyzer.cs
+++ src/GraphQL/Validation/ComplexityAnalyzer.cs
@@ -9,17 +9,17 @@ namespace GraphQL.Validation
     {
         public class ComplexityResult
         {
-            public Dictionary<INode, float> ComplexityMap { get; set; } = new Dictionary<INode, float>();
-            public int Complexity { get; set; }
+            public Dictionary<INode, double> ComplexityMap { get; } = new Dictionary<INode, double>();
+            public double Complexity { get; set; }
             public int TotalQueryDepth { get; set; }
         }
 
         private ComplexityResult _result = new ComplexityResult();
-        private readonly float _avgImpact;
+        private readonly double _avgImpact;
         private readonly int _maxRecursionCount;
         private int _loopCounter;
 
-        public ComplexityAnalyzer(float avgImpact = 1.5f, int maxRecursionCount = 100)
+        public ComplexityAnalyzer(double avgImpact = 2.0d, int maxRecursionCount = 100)
         {
             if (avgImpact <= 1) throw new ArgumentOutOfRangeException(nameof(avgImpact));
 
@@ -29,36 +29,44 @@ namespace GraphQL.Validation
         public ComplexityResult Analyze(Document doc)
         {
             TreeIterator(doc, _avgImpact);
+
+            // Cleanup in case Analyze is called again
             _loopCounter = 0;
             var retVal = _result;
             _result = new ComplexityResult();
+
             return retVal;
         }
 
-        private void TreeIterator(INode node, float impact)
+        private void TreeIterator(INode node, double impact)
         {
             if (_loopCounter++ > _maxRecursionCount)
                 throw new InvalidOperationException("Query is too complex to validate.");
 
-            // if (impact <= 1) impact = _avgImpact;
-
             if (node.Children != null && node.Children.Any(n => n is Field || (n is SelectionSet && ((SelectionSet)n).Children.Any()) || n is Operation))
             {
-                var nextimpact = _avgImpact * impact;
                 if (node is Field)
                 {
                     _result.TotalQueryDepth++;
+                    RecordFieldComplexity(node, impact);
                     foreach (var nodeChild in node.Children.Where(n => n is SelectionSet))
-                        TreeIterator(nodeChild, nextimpact);
+                        TreeIterator(nodeChild, _avgImpact * impact);
                 }
-                else foreach (var nodeChild in node.Children)
-                        TreeIterator(nodeChild, nextimpact);
+                else
+                    foreach (var nodeChild in node.Children)
+                        TreeIterator(nodeChild, impact);
             }
             else if (node is Field)
-                if (_result.ComplexityMap.ContainsKey(node))
-                    _result.ComplexityMap[node] += impact;
-                else
-                    _result.ComplexityMap.Add(node, impact);
+                RecordFieldComplexity(node, impact);
+        }
+
+        private void RecordFieldComplexity(INode node, double impact)
+        {
+            _result.Complexity += impact;
+            if (_result.ComplexityMap.ContainsKey(node))
+                _result.ComplexityMap[node] += impact;
+            else
+                _result.ComplexityMap.Add(node, impact);
         }
     }
 }
