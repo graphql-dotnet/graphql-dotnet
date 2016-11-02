05ca36bac261410d74d446e1ee21d14b85a35385
Farzad E <dme@dnetguru.com>
2016-11-02 04:32:35 -0700

    Test case for non-inline fragments.

--- src/GraphQL.Tests/Complexity/ComplexityTests.cs
+++ src/GraphQL.Tests/Complexity/ComplexityTests.cs
@@ -24,6 +24,22 @@ namespace GraphQL.Tests.Complexity
         }
 
         [Fact]
+        public void fragments_test()
+        {
+            var withFrag =
+                AnalyzeComplexity(
+                    @"{ leftComparison: hero(episode: EMPIRE) { ...comparisonFields } rightComparison: hero(episode: JEDI) { ...comparisonFields} } fragment comparisonFields on Character { name appearsIn friends { name } }");
+            var woFrag =
+                AnalyzeComplexity(
+                    @"{ leftComparison: hero(episode: EMPIRE) { name appearsIn friends { name } } rightComparison: hero(episode: JEDI) { name appearsIn friends { name } } }");
+
+            // TODO: Non-inline fragments are not handled yet.
+            
+            // withFrag.Complexity.ShouldBe(woFrag.Complexity);
+            // withFrag.TotalQueryDepth.ShouldBe(woFrag.TotalQueryDepth);
+        }
+
+        [Fact]
         public void absurdly_huge_query()
         {
             try
@@ -35,8 +51,6 @@ namespace GraphQL.Tests.Complexity
             {
                 ex.Message.ShouldBe("Query is too complex to validate.");
             }
-        }
-        
-        // TODO: Non-inline fragments are not handled yet.
+        }        
     }
 }
