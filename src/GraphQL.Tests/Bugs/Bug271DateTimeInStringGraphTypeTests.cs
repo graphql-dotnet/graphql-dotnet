67bf2ed015753801787f9f13931be7ba0c3b7186
mamasselin <masselin@genetec.com>
2017-02-17 17:43:43 -0500

    bug with datetime inside string fields (#272)
    * bug with datetime inside string fields
    
    * Update BugDateTimeInStringGraphTypeTests.cs
    
    * fix for issue #271
    
    * remove unreachable code and added test coverage lib
    
    * updated unit test to only contain covered scenario
    
    * - removed unit test coverage dependency
    - extracted logic of converting the DateTime into the DateTimeConverter
    
    * removed unused directive

---
+++ src/GraphQL.Tests/Bugs/Bug271DateTimeInStringGraphTypeTests.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Tests.Execution;
+using Xunit;
+
+namespace GraphQL.Tests.Bugs
+{
+    public class Bug271DateTimeInStringGraphTypeTests : QueryTestBase<VariablesSchema>
+    {
+        [Fact]
+        public void supports_date_inputs_inside_string_fields()
+        {
+            var query = @"query q($input: TestInputObject) {
+                              fieldWithObjectInput(input: $input)
+                            }";
+            
+            var expected = "{ \"fieldWithObjectInput\": \"{\\\"a\\\":\\\"2017-01-27T15:19:53.000Z\\\",\\\"b\\\":[\\\"bar\\\"],\\\"c\\\":\\\"baz\\\"}\" }";
+            var inputs = "{'input': {'a':'2017-01-27T15:19:53.000Z', 'b':['bar'], 'c': 'baz'} }".ToInputs();
+            AssertQuerySuccess(query, expected, inputs);
+        }
+    }
+}
