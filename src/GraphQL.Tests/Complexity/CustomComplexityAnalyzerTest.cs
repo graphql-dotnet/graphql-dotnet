0aec9985d4688d0a35b70134e1564fe61b359a8b
Farzad E <dme@dnetguru.com>
2017-03-28 17:21:30 -0700

    Test case for custom ComplexityAnalyzer.

---
+++ src/GraphQL.Tests/Complexity/CustomComplexityAnalyzerTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using GraphQL.Tests.Complexity.CustomComplexityAnalyzer;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests.Complexity
+{
+    public class CustomComplexityAnalyzerTest : CustomComplexityAnalyzerTestBase
+    {
+
+        [Fact]
+        public void should_work_when_complexity_within_params()
+        {
+            var query = @"
+                query HeroNameQuery {
+                  hero {
+                    name
+                  }
+                }
+            ";
+
+            var complexityConfiguration = new ExtendedComplexityConfig { MaxComplexity = 105, complexityMap = new Dictionary<string, double>() { { "hero", 100f } } };
+            var res = Execute(complexityConfiguration, query);
+
+            res.Result.Errors.ShouldBe(null);
+        }
+
+        [Fact]
+        public void should_not_work_when_complexity_within_params()
+        {
+            var query = @"
+                query HeroNameQuery {
+                  hero {
+                    name
+                  }
+                }
+            ";
+
+            var complexityConfiguration = new ExtendedComplexityConfig
+            {
+                MaxComplexity = 100,
+                complexityMap = new Dictionary<string, double>() { { "hero", 100f }, { "droid", 5f } }
+            };
+            var res = Execute(complexityConfiguration, query);
+
+            res.Result.Errors.ShouldNotBe(null);
+        }
+    }
+}
---
+++ src/GraphQL.Tests/Complexity/CustomComplexityAnalyzerTestBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Execution;
+using GraphQL.Language.AST;
+using GraphQL.Tests.StarWars;
+using GraphQL.Validation;
+using GraphQL.Validation.Complexity;
+
+namespace GraphQL.Tests.Complexity.CustomComplexityAnalyzer
+{
+    public class CustomComplexityAnalyzerTestBase
+    {
+        public IComplexityAnalyzer Analyzer { get; } = new CustomComplexityAnalyzerDefinition();
+
+        public async Task<ExecutionResult> Execute(ExtendedComplexityConfig complexityConfig, string query) =>
+            await new DocumentExecuter(new GraphQLDocumentBuilder(), new DocumentValidator(), Analyzer).ExecuteAsync(
+                options =>
+                {
+                    options.Schema = new StarWarsTestBase().Schema;
+                    options.Query = query;
+                    options.ComplexityConfiguration = complexityConfig;
+                });
+    }
+
+
+    public class CustomComplexityAnalyzerDefinition : IComplexityAnalyzer
+    {
+        private readonly ComplexityResult _result = new ComplexityResult();
+
+        public void Validate(Document document, IComplexityConfiguration parameters)
+        {
+            if (!(parameters is ExtendedComplexityConfig)) throw new ArgumentException();
+            var extendedConfig = (ExtendedComplexityConfig)parameters;
+
+            TreeIterator(document, _result, extendedConfig.complexityMap);
+            if (extendedConfig.MaxComplexity.HasValue &&
+                _result.Complexity > extendedConfig.MaxComplexity.Value)
+                throw new InvalidOperationException("Query is too complex to execute.");
+
+        }
+
+        private void TreeIterator(INode node, ComplexityResult result, Dictionary<string, double> map)
+        {
+            if (node is Field)
+            {
+                result.Complexity += map.ContainsKey(((Field)node).Name) ? map[((Field)node).Name] : 1;
+                foreach (var nodeChild in node.Children.Where(n => n is SelectionSet))
+                    TreeIterator(nodeChild, result, map);
+            }
+
+            if (node.Children.Any(n => n is Field || n is SelectionSet && ((SelectionSet)n).Children.Any() || n is Operation))
+            {
+                foreach (var nodeChild in node.Children)
+                    TreeIterator(nodeChild, result, map);
+            }
+        }
+    }
+    public class ExtendedComplexityConfig : IComplexityConfiguration
+    {
+        public double? FieldImpact { get; set; }
+        public int? MaxComplexity { get; set; }
+        public int? MaxDepth { get; set; }
+        public Dictionary<string, double> complexityMap { get; set; }
+    }
+}
