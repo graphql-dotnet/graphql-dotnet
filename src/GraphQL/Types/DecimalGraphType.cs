25d5a3e8c4f971bcec1539d38cc70fecca1cbf86
Jukka Puranen <jukka.puranen@gmail.com>
2017-06-16 14:16:06 +0300

    Forced parsing to use invariant culture and removed uneccessary parsing

--- src/GraphQL/Types/DecimalGraphType.cs
+++ src/GraphQL/Types/DecimalGraphType.cs
@@ -1,3 +1,5 @@
+﻿using System;
+using System.Globalization;
 using GraphQL.Language.AST;
 
 namespace GraphQL.Types
@@ -17,7 +19,8 @@ namespace GraphQL.Types
         public override object ParseValue(object value)
         {
             decimal result;
-            if (decimal.TryParse(value?.ToString() ?? string.Empty, out result))
+
+            if (decimal.TryParse(value?.ToString() ?? string.Empty, NumberStyles.Float, CultureInfo.InvariantCulture, out result))
             {
                 return result;
             }
@@ -30,25 +33,20 @@ namespace GraphQL.Types
             {
                 return ParseValue(((StringValue)value).Value);
             }
-
-            if (value is IntValue)
-            {
-                return ParseValue(((IntValue)value).Value);
-            }
-
+  
             if (value is LongValue)
             {
-                return ParseValue(((LongValue)value).Value);
+                return Convert.ToDecimal(((LongValue)value).Value);
             }
 
             if (value is FloatValue)
             {
-                return ParseValue(((FloatValue)value).Value);
+                return Convert.ToDecimal(((FloatValue)value).Value);
             }
 
             if (value is DecimalValue)
             {
-                return ParseValue(((DecimalValue)value).Value);
+                return ((DecimalValue) value).Value;
             }
 
             return null;
