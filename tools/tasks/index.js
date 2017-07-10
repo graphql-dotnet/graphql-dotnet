95f4be62957626f7c033f88add5394e7337a0b29
Joe McBride <joe@uicraftsman.com>
2017-07-10 16:50:08 -0700

    Remove unused files

--- tools/tasks/index.js
+++ tools/tasks/index.js
@@ -3,8 +3,6 @@ import compile from './compile';
 import clean from './clean';
 import dotnetPack from './dotnetPack';
 import dotnetTest from './test.dotnet';
-import nuget from './nuget';
-import nuspec from './nuspec';
 import restore from './restore';
 import settings from './settings';
 import setVersion from './setVersion';
@@ -16,8 +14,6 @@ export default {
   clean,
   dotnetPack,
   dotnetTest,
-  nuget: () => nuget({ version: settings.version, target: settings.target }),
-  nuspec,
   restore,
   settings,
   setVersion,
