409bf6b6840a8099561b198fadeea7e0e646260a
Giorgi <giorgi@aboutmycode.com>
2017-08-06 00:43:32 +0400

    Do not use auto registration for StarWars types.

--- src/GraphQL.StarWars/Types/HumanType.cs
+++ src/GraphQL.StarWars/Types/HumanType.cs
@@ -2,17 +2,22 @@ using GraphQL.Types;
 
 namespace GraphQL.StarWars.Types
 {
-    public class HumanType : AutoRegisteringObjectGraphType<Human>
+    public class HumanType : ObjectGraphType<Human>
     {
         public HumanType(StarWarsData data)
         {
             Name = "Human";
+
+            Field(h => h.Id).Description("The id of the human.");
+            Field(h => h.Name, nullable: true).Description("The name of the human.");
 
             Field<ListGraphType<CharacterInterface>>(
                 "friends",
                 resolve: context => data.GetFriends(context.Source)
             );
             Field<ListGraphType<EpisodeEnum>>("appearsIn", "Which movie they appear in.");
+
+            Field(h => h.HomePlanet, nullable: true).Description("The home planet of the human.");
 
             Interface<CharacterInterface>();
         }
