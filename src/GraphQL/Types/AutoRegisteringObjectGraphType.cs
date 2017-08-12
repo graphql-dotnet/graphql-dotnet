f40f3ddedffe2499f72800fc647d23f224a474eb
Giorgi <giorgi@aboutmycode.com>
2017-08-12 21:47:18 +0400

    Inline virtual method.

--- src/GraphQL/Types/AutoRegisteringObjectGraphType.cs
+++ src/GraphQL/Types/AutoRegisteringObjectGraphType.cs
@@ -8,19 +8,13 @@ namespace GraphQL.Types
     {
         public AutoRegisteringObjectGraphType()
         {
-            var properties = GetPropertiesToRegister();
+            var properties = typeof(TSourceType).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                            .Where(p => p.PropertyType.GetTypeInfo().IsValueType || p.PropertyType == typeof(string));
 
             foreach (var propertyInfo in properties)
             {
                 Field(propertyInfo.PropertyType.GetGraphTypeFromType(propertyInfo.PropertyType.IsNullable()), propertyInfo.Name);
             }
-        }
-
-        protected virtual IEnumerable<PropertyInfo> GetPropertiesToRegister()
-        {
-            return typeof(TSourceType)
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(p => p.PropertyType.GetTypeInfo().IsValueType || p.PropertyType == typeof(string));
         }
     }
 }
