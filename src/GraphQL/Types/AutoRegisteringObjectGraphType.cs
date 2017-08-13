fc6dd5fa707b0cb8fe249a7b8a1583154ad2d199
Giorgi <Giorgi@users.noreply.github.com>
2017-08-14 03:06:52 +0400

    Added AutoRegisteringObjectGraphType (#386)
    * Added AutoRegisteringObjectGraphType which registers scalar properties automatically.
    
    * Call as extension method.
    
    * Do not use auto registration for StarWars types.
    
    * Inline virtual method.

---
+++ src/GraphQL/Types/AutoRegisteringObjectGraphType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace GraphQL.Types
+{
+    public class AutoRegisteringObjectGraphType<TSourceType> : ObjectGraphType<TSourceType>
+    {
+        public AutoRegisteringObjectGraphType()
+        {
+            var properties = typeof(TSourceType).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                            .Where(p => p.PropertyType.GetTypeInfo().IsValueType || p.PropertyType == typeof(string));
+
+            foreach (var propertyInfo in properties)
+            {
+                Field(propertyInfo.PropertyType.GetGraphTypeFromType(propertyInfo.PropertyType.IsNullable()), propertyInfo.Name);
+            }
+        }
+    }
+}
