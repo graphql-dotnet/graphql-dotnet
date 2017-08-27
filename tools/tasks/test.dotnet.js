9a51c5d4ba068115b7f93d920181b1e39cd0956c
Srini Vasudevan <srinivasa.vasudevan@honeywell.com>
2017-08-28 09:13:19 +1000

    updated to 2.0

--- tools/tasks/test.dotnet.js
+++ tools/tasks/test.dotnet.js
@@ -6,7 +6,7 @@ export default function testDotnet() {
   const deferred = new Deferred();
 
   const platform = process.platform === 'darwin'
-    ? '-f netcoreapp1.1'
+    ? '-f netcoreapp2.0'
     : '';
   const test = `dotnet test ${platform} -c ${settings.target}`;
 
