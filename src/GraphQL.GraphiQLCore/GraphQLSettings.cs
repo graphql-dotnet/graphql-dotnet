e0e688b9bcc9ac2138bd2376783a2174e299444a
Joe McBride <joe@uicraftsman.com>
2017-09-05 17:09:22 -0700

    Fix after rebase

--- src/GraphQL.GraphiQLCore/GraphQLSettings.cs
+++ src/GraphQL.GraphiQLCore/GraphQLSettings.cs
@@ -6,7 +6,6 @@ namespace GraphQL.GraphiQLCore
     public class GraphQLSettings
     {
         public PathString Path { get; set; } = "/api/graphql";
-        public ISchema Schema { get; set; }
         public Func<HttpContext, object> BuildUserContext { get; set; }
     }
 }
