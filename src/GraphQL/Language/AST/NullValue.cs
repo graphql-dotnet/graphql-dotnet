1075ac8ed8ce0c251d42bf24d27c28164f664f08
Joe McBride <joe@uicraftsman.com>
2017-09-08 13:35:53 -0700

    Bump parser version

--- src/GraphQL/Language/AST/NullValue.cs
+++ src/GraphQL/Language/AST/NullValue.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GraphQL.Language.AST
+﻿namespace GraphQL.Language.AST
 {
     public class NullValue : AbstractNode, IValue
     {
