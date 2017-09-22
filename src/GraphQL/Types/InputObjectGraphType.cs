8a07dd7bcc8ab0fb0427655c52bd57267d3efad4
Jason Kingsmill <jason.kingsmill@viatek.com.au>
2017-09-23 08:00:14 +1000

    Added IInputObjectGraphType interface, inverted InputObjectGraphType inheritance

--- src/GraphQL/Types/InputObjectGraphType.cs
+++ src/GraphQL/Types/InputObjectGraphType.cs
@@ -7,102 +7,16 @@ using System.Linq.Expressions;
 
 namespace GraphQL.Types
 {
-    public class InputObjectGraphType : ComplexGraphType<object>
+    public interface IInputObjectGraphType : IComplexGraphType
     {
     }
 
-
-
-    public class InputObjectGraphType<TSourceType> : InputObjectGraphType
+    public class InputObjectGraphType : InputObjectGraphType<object>
     {
+    }
 
-        public FieldType Field(
-            Type type,
-            string name,
-            string description = null,
-            QueryArguments arguments = null,
-            Func<ResolveFieldContext<TSourceType>, object> resolve = null,
-            string deprecationReason = null)
-        {
-            return AddField(new FieldType
-            {
-                Name = name,
-                Description = description,
-                DeprecationReason = deprecationReason,
-                Type = type,
-                Arguments = arguments,
-                Resolver = resolve != null
-                    ? new FuncFieldResolver<TSourceType, object>(resolve)
-                    : null,
-            });
-        }
-
-        public FieldType Field<TGraphType>(
-            string name,
-            string description = null,
-            QueryArguments arguments = null,
-            Func<ResolveFieldContext<TSourceType>, object> resolve = null,
-            string deprecationReason = null)
-            where TGraphType : IGraphType
-        {
-            return AddField(new FieldType
-            {
-                Name = name,
-                Description = description,
-                DeprecationReason = deprecationReason,
-                Type = typeof(TGraphType),
-                Arguments = arguments,
-                Resolver = resolve != null
-                    ? new FuncFieldResolver<TSourceType, object>(resolve)
-                    : null,
-            });
-        }
-
-        public FieldBuilder<TSourceType, TProperty> Field<TProperty>(
-            Expression<Func<TSourceType, TProperty>> expression,
-            bool nullable = false,
-            Type type = null)
-        {
-            string name;
-            try
-            {
-                name = expression.NameOf();
-            }
-            catch
-            {
-                throw new ArgumentException(
-                    $"Cannot infer a Field name from the expression: '{expression.Body.ToString()}' " +
-                    $"on parent GraphQL type: '{Name ?? GetType().Name}'.");
-            }
-            return Field(name, expression, nullable, type);
-        }
-
-        public FieldBuilder<TSourceType, TProperty> Field<TProperty>(
-           string name,
-           Expression<Func<TSourceType, TProperty>> expression,
-           bool nullable = false,
-           Type type = null)
-        {
-            try
-            {
-                if (type == null)
-                    type = typeof(TProperty).GetGraphTypeFromType(nullable);
-            }
-            catch (ArgumentOutOfRangeException exp)
-            {
-                throw new ArgumentException(
-                    $"The GraphQL type for Field: '{name}' on parent type: '{Name ?? GetType().Name}' could not be derived implicitly. \n",
-                    exp
-                 );
-            }
-
-            var builder = FieldBuilder.Create<TSourceType, TProperty>(type)
-                .Resolve(new ExpressionFieldResolver<TSourceType, TProperty>(expression))
-                .Name(name);
-
-            AddField(builder.FieldType);
-            return builder;
-        }
+    public class InputObjectGraphType<TSourceType> : ComplexGraphType<TSourceType>, IInputObjectGraphType
+    {        
 
     }
 }
