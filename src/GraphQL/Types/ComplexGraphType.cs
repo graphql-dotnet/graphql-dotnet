6646d6f14303ceae1b6f1ce5f5ef075b1ba8d4c5
Derek Holmes <derekholms@navitaire.com>
2017-10-06 16:36:04 -0600

    added async helper method

--- src/GraphQL/Types/ComplexGraphType.cs
+++ src/GraphQL/Types/ComplexGraphType.cs
@@ -119,6 +119,27 @@ namespace GraphQL.Types
             });
         }
 
+        public FieldType FieldAsync(
+            Type type,
+            string name,
+            string description = null,
+            QueryArguments arguments = null,
+            Func<ResolveFieldContext<TSourceType>, object> resolve = null,
+            string deprecationReason = null)
+        {
+            return AddField(new FieldType
+            {
+                Name = name,
+                Description = description,
+                DeprecationReason = deprecationReason,
+                Type = type,
+                Arguments = arguments,
+                Resolver = resolve != null
+                    ? new SlowFuncFieldResolver<TSourceType, object>(resolve)
+                    : null,
+            });
+        }
+
         public FieldType FieldAsync<TGraphType>(
             string name,
             string description = null,
