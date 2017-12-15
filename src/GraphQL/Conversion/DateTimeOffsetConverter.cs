09e05e9a9b8302e4c5524fd7da05e055b8f5223b
roman-kozachenko <pomkaster@gmail.com>
2017-12-15 18:31:40 +0300

    Update DateTimeOffsetConverter.cs (#1)
    Register DateTimeOffsetConverter

--- src/GraphQL/Conversion/DateTimeOffsetConverter.cs
+++ src/GraphQL/Conversion/DateTimeOffsetConverter.cs
@@ -15,7 +15,7 @@ namespace GraphQL.Conversion
 
         private static readonly Regex iso8601Expression = new Regex(@"^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$");
 
-        public static DateTimeOffset GetDateTime(string dateString)
+        public static DateTimeOffset GetDateTimeOffset(string dateString)
         {
             var trimmedString = dateString.Trim();
             if (trimmedString == Today)
