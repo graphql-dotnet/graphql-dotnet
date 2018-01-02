c1579c7cb538ec1a4c965d0eccbce5e1f1a95214
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-01-02 02:29:35 +0100

    ScalarGraphType

--- src/GraphQL/Types/ScalarGraphType.cs
+++ src/GraphQL/Types/ScalarGraphType.cs
@@ -2,6 +2,21 @@ using GraphQL.Language.AST;
 
 namespace GraphQL.Types
 {
+
+    /// <summary>
+    /// Represents a primitive value in GraphQL.
+    /// GraphQL responses take the form of a hierarchical tree; the leaves on these trees are GraphQL scalars.
+    /// </summary>
+    /// <typeparam name="Type">The Primitive Type</typeparam>
+    public abstract class ScalarGraphType<Type> : GraphType
+    {
+        public abstract object Serialize(object value);
+
+        public abstract Type ParseValue(object value);
+
+        public abstract object ParseLiteral(IValue value);
+    }
+
     public abstract class ScalarGraphType : GraphType
     {
         public abstract object Serialize(object value);
