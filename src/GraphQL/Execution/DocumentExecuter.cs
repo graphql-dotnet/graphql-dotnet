b9772ae72bad47b52f011f5a6e60b633d2891079
Derek Holmes <holm0563@yahoo.com>
2018-01-25 15:40:26 -0700

    Removed blocking of main thread. Passed CancellationToken into spawned threads (#525)
    * Removed blocking of main thread. Passed cancellation token into spawned threads.
    * remove unecessary .tolist()

--- src/GraphQL/Execution/DocumentExecuter.cs
+++ src/GraphQL/Execution/DocumentExecuter.cs
@@ -318,14 +318,14 @@ namespace GraphQL
                 var field = fieldCollection.Value;
                 var fieldType = GetFieldDefinition(context.Document, context.Schema, rootType, field);
 
-                if (fieldType?.Resolver == null || !fieldType.Resolver.RunThreaded() || context.Operation.OperationType == OperationType.Mutation)
+                if (fieldType?.Resolver == null || !fieldType.Resolver.RunThreaded() || context.Operation.OperationType == OperationType.Mutation || fields.Count == 1)
                 {
                     await ExtractFieldAsync(context, rootType, source, field, fieldType, data, currentPath)
                         .ConfigureAwait(false);
                 }
                 else
                 {
-                    var task = Task.Run(() => ExtractFieldAsync(context, rootType, source, field, fieldType, data, currentPath));
+                    var task = Task.Run(() => ExtractFieldAsync(context, rootType, source, field, fieldType, data, currentPath), context.CancellationToken);
 
                     externalTasks.Add(task);
                 }
@@ -333,7 +333,7 @@ namespace GraphQL
 
             if (externalTasks.Count > 0)
             {
-                await Task.WhenAll(externalTasks.ToArray())
+                await Task.WhenAll(externalTasks)
                     .ConfigureAwait(false);
             }
 
