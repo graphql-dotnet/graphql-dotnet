d2d53cca1be9d68b0d3bf5d9222ea9ebbda48f5c
John Rutherford <sectrean@gmail.com>
2018-03-15 05:09:16 +0000

    Add documentation for the DataLoader implementation (#598)

--- src/GraphQL/DataLoader/IDataLoaderContextAccessor.cs
+++ src/GraphQL/DataLoader/IDataLoaderContextAccessor.cs
@@ -1,7 +1,13 @@
 namespace GraphQL.DataLoader
 {
+    /// <summary>
+    /// Provides access to a <seealso cref="DataLoaderContext"/>
+    /// </summary>
     public interface IDataLoaderContextAccessor
     {
+        /// <summary>
+        /// The current <seealso cref="DataLoaderContext"/>
+        /// </summary>
         DataLoaderContext Context { get; set; }
     }
 }
