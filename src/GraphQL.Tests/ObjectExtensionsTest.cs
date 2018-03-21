f7479025b21c8279fdc6fbd2dd089471616df07b
Wiktor Golonka <wiktor.golonka@ist.technology>
2018-03-21 13:24:51 +0000

    Unit tests for array conversion

--- src/GraphQL.Tests/ObjectExtensionsTest.cs
+++ src/GraphQL.Tests/ObjectExtensionsTest.cs
@@ -73,5 +73,109 @@ namespace GraphQL.Tests
             /* Then */
             actual.ShouldBe(value);
         }
+        
+                [Fact]
+        public void convert_double_array_to_array()
+        {
+            // Arrange
+            var doubles = new double[] { 1.00, 2.01, 3.14 };
+
+            // Act 
+            var result = doubles.GetPropertyValue(typeof(double[]));
+
+            // Assert
+            Assert.Equal(result, doubles);
+        }
+
+        [Fact]
+        public void convert_double_list_to_array()
+        {
+            // Arrange
+            var doubles = new List<double>() { 1.00, 2.01, 3.14 };
+
+            // Act 
+            var result = doubles.GetPropertyValue(typeof(double[]));
+
+            // Assert
+            Assert.Equal(result, doubles);
+        }
+
+        [Fact]
+        public void convert_ndouble_list_to_array()
+        {
+            // Arrange
+            var doubles = new List<double?>() { 1.00, 2.01, 3.14, null };
+
+            // Act 
+            var result = doubles.GetPropertyValue(typeof(double?[]));
+
+            // Assert
+            Assert.Equal(result, doubles);
+        }
+
+        [Fact]
+        public void convert_ndouble_array_to_array()
+        {
+            // Arrange
+            var doubles = new double?[] { 1.00, 2.01, 3.14 };
+
+            // Act 
+            var result = doubles.GetPropertyValue(typeof(double?[]));
+
+            // Assert
+            Assert.Equal(result, doubles);
+        }
+
+        [Fact]
+        public void convert_double_list_to_list()
+        {
+            // Arrange
+            var doubles = new List<double>() { 1.00, 2.01, 3.14 };
+
+            // Act 
+            var result = doubles.GetPropertyValue(typeof(List<double>));
+
+            // Assert
+            Assert.Equal(result, doubles);
+        }
+
+        [Fact]
+        public void convert_strings_array_to_array()
+        {
+            // Arrange
+            var strings = new string[] { "foo", "bar", "new" };
+
+            // Act 
+            var result = strings.GetPropertyValue(typeof(string[]));
+
+            // Assert
+            Assert.Equal(result, strings);
+        }
+
+        [Fact]
+        public void convert_strings_list_to_array()
+        {
+            // Arrange
+            var strings = new List<string>() { "foo", "bar", "new" };
+
+            // Act 
+            var result = strings.GetPropertyValue(typeof(string[]));
+
+            // Assert
+            Assert.Equal(result, strings);
+        }
+
+        [Fact]
+        public void convert_strings_list_to_list()
+        {
+            // Arrange
+            var strings = new List<string>() { "foo", "bar", "new" };
+
+            // Act 
+            var result = strings.GetPropertyValue(typeof(List<string>));
+
+            // Assert
+            Assert.Equal(result, strings);
+        }
     }
 }
