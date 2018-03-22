083b9b3df275aac781d6eafa774c048f570d0957
Denys Zhuravel <denys.zhuravel@albelli.com>
2018-03-22 10:40:53 +0100

    add --include-symblos to the main nuget package in order to get pdbs and source available via a source server

--- tools/tasks/dotnetPack.js
+++ tools/tasks/dotnetPack.js
@@ -24,7 +24,7 @@ export default function compile() {
     versionSuffix = `--version-suffix ${settings.versionSuffix}${settings.revision}`;
   }
 
-  const cmd = `dotnet pack src/GraphQL -o ${settings.artifacts} -c ${settings.target} ${versionSuffix}`
+  const cmd = `dotnet pack src/GraphQL -o ${settings.artifacts} -c ${settings.target} ${versionSuffix} --include-symbols`
   const one = run(cmd)
 
   const cmd2 = `dotnet pack src/GraphQL.StarWars -o ${settings.artifacts} -c ${settings.target}`
