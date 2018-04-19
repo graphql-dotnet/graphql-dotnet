d89d4d96482b0f6aa751973ea84c5bfd8a892460
Tommy Lillehagen <tommy.lillehagen@r3.com>
2018-04-19 02:16:14 +0100

    Add error code and data to ExecutionError (#627)
    - The error code is derived from the exception name; e.g.,
       DatabaseQueryException results in an error code of DATABASE_QUERY.
    
     - Any data put into Exception.Data gets put into ExecutionError.Data
       and consequently included in the serialised output.

---
+++ src/GraphQL.Tests/Errors/ErrorCodeTests.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests.Errors
+{
+    public class ErrorCodeTests : QueryTestBase<ErrorCodeTests.TestSchema>
+    {
+        [Theory]
+        [InlineData("{ firstSync }", "FIRST")]
+        [InlineData("{ secondSync }", "SECOND_TEST")]
+        [InlineData("{ uncodedSync }", "")]
+        public async Task should_show_code_when_exception_is_thrown_with_sync_field(string query, string code)
+        {
+            var result = await Executer.ExecuteAsync(_ =>
+            {
+                _.Schema = Schema;
+                _.Query = query;
+            });
+
+            result.Errors.Count.ShouldBe(1);
+            var error = result.Errors.First();
+            error.Code.ShouldBe(code);
+        }
+
+        [Theory]
+        [InlineData("{ firstAsync }", "FIRST")]
+        [InlineData("{ secondAsync }", "SECOND_TEST")]
+        [InlineData("{ uncodedAsync }", "")]
+        public async Task should_show_code_when_exception_thrown_with_async_field(string query, string code)
+        {
+            var result = await Executer.ExecuteAsync(_ =>
+            {
+                _.Schema = Schema;
+                _.Query = query;
+            });
+
+            result.Errors.Count.ShouldBe(1);
+            var error = result.Errors.First();
+            error.Code.ShouldBe(code);
+        }
+
+        [Fact]
+        public async Task should_propagate_exception_data_when_exception_is_thrown_in_field_resolver()
+        {
+            var result = await Executer.ExecuteAsync(_ =>
+            {
+                _.Schema = Schema;
+                _.Query = @"{ secondSync }";
+            });
+
+            result.Errors.Count.ShouldBe(1);
+            var error = result.Errors.First();
+            error.Data["Foo"].ShouldBe("Bar");
+        }
+
+        public class TestQuery : ObjectGraphType
+        {
+            public TestQuery()
+            {
+                Name = "Query";
+                Field<StringGraphType>(
+                    "firstSync",
+                    resolve: _ => throw new FirstException("Exception from synchronous resolver")
+                );
+                FieldAsync<StringGraphType>(
+                    "firstAsync",
+                    resolve: async _ => throw new FirstException("Exception from asynchronous resolver")
+                );
+                Field<StringGraphType>(
+                    "secondSync",
+                    resolve: _ => throw new SecondTestException("Exception from synchronous resolver")
+                );
+                FieldAsync<StringGraphType>(
+                    "secondAsync",
+                    resolve: async _ => throw new SecondTestException("Exception from asynchronous resolver")
+                );
+                Field<StringGraphType>(
+                    "uncodedSync",
+                    resolve: _ => throw new Exception("Exception from synchronous resolver")
+                );
+                FieldAsync<StringGraphType>(
+                    "uncodedAsync",
+                    resolve: async _ => throw new Exception("Exception from asynchronous resolver")
+                );
+            }
+        }
+
+        public class TestSchema : Schema
+        {
+            public TestSchema()
+            {
+                Query = new TestQuery();
+            }
+        }
+
+        public class FirstException : Exception
+        {
+            public FirstException(string message)
+                : base(message)
+            {
+            }
+        }
+
+        public class SecondTestException : Exception
+        {
+            public SecondTestException(string message)
+                : base(message)
+            {
+                Data["Foo"] = "Bar";
+            }
+        }
+    }
+}
