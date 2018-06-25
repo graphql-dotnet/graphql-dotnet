53d1602a36c6df05cc52d0744966228c464fc8d6
Vedant Koditkar <vedkoditkar@gmail.com>
2018-06-26 03:27:57 +0530

    #606 boolean graph type not converting numerical string (#701)
    * #606 Add support for converting "1" and "0" to boolean
    
    * #606 Change BooleanGraphType Serialize method to use ParseValue method
    
    * let exceptions bubble up.
    
    * Use ShouldBe string test exception.
    
    * Add an actual Boolean value in test data.

--- src/GraphQL.Tests/Types/BooleanGraphTypeTests.cs
+++ src/GraphQL.Tests/Types/BooleanGraphTypeTests.cs
@@ -1,5 +1,6 @@
-﻿using GraphQL.Types;
+using GraphQL.Types;
 using Shouldly;
+using System;
 using Xunit;
 
 namespace GraphQL.Tests.Types
@@ -42,6 +43,49 @@ namespace GraphQL.Tests.Types
         public void coerces_string_True()
         {
             type.ParseValue("True").ShouldBe(true);
+        }
+
+        [Fact]
+        public void coerces_string_1_to_true()
+        {
+            type.ParseValue("1").ShouldBe(true);
+        }
+
+        [Fact]
+        public void coerces_zero_string_to_false()
+        {
+            type.ParseValue("0").ShouldBe(false);
+        }
+
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("21")]
+        public void coerces_input_to_exception(string input)
+        {
+            FormatException formatException = Assert.Throws<FormatException>(() => type.ParseValue(input));
+            formatException.Message.ShouldBe("String was not recognized as a valid Boolean.");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData("0")]
+        [InlineData("False")]
+        [InlineData("false")]
+        [InlineData(false)]
+        public void serialize_input_to_false(object input)
+        {
+            type.Serialize(input).ShouldBe(false);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData("1")]
+        [InlineData("True")]
+        [InlineData("true")]
+        [InlineData(true)]
+        public void serialize_input_to_true(object input)
+        {
+            type.Serialize(input).ShouldBe(true);
         }
     }
 }
