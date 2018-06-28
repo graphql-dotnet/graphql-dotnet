2aa41884b8c0e37efc9ac9559bc04fc565496bf4
Vedant Koditkar <vedkoditkar@gmail.com>
2018-06-28 22:01:48 +0530

    Add field name validator as stated in spec.
    * null is allowed for now

---
+++ src/GraphQL/Utilities/FieldValidator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace GraphQL.Utilities
+{
+    public class FieldValidator
+    {
+        private static readonly string RESERVED_PREFIX = "__";
+        private static readonly string NAME_RX = @"^[_A-Za-z][_0-9A-Za-z]*$";
+
+        public static void ValidateName(string name)
+        {
+            if (name == null)
+            {
+                return;
+            }
+
+            if (name.Length > 1 && name.StartsWith(RESERVED_PREFIX))
+            {
+                throw new ArgumentOutOfRangeException(nameof(name),
+                    $"A field name: {name} must not begin with \"__\", which is reserved by GraphQL introspection.");
+            }
+            if (!Regex.IsMatch(name, NAME_RX))
+            {
+                throw new ArgumentOutOfRangeException(nameof(name),
+                    $"A field name must match /^[_a-zA-Z][_a-zA-Z0-9]*$/ but {name} does not.");
+            }
+        }
+    }
+}
