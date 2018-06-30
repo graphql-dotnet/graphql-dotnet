4ae029d81202a5996561d5a11b662e4d8cb22f61
Sam Boling <sboling@indicalab.com>
2018-06-30 15:01:58 -0600

    generate Apollo Tracing report from execution metrics. fixes #381

---
+++ src/GraphQL.Tests/Instrumentation/ApolloTracingTests.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphQL.Instrumentation;
+using GraphQL.Tests.StarWars;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests.Instrumentation
+{
+    public class ApolloTracingTests : StarWarsTestBase
+    {
+        public ApolloTracingTests()
+            : base()
+        {
+        }
+
+        [Fact]
+        public void extension_has_expected_format()
+        {
+            var query = @"
+query {
+  hero {
+    name
+    friends {
+      name
+    }
+  }
+}";
+
+            var start = DateTime.UtcNow;
+            var result = Executer.ExecuteAsync(_ =>
+            {
+                _.Schema = Schema;
+                _.Query = query;
+                _.EnableMetrics = true;
+                _.FieldMiddleware.Use<InstrumentFieldsMiddleware>();
+            }).Result;
+            result.EnrichWithApolloTracing(start);
+            var trace = (ApolloTrace)result.Extensions["tracing"];
+
+            trace.Version.ShouldBe(1);
+            trace.Parsing.StartOffset.ShouldNotBe(0);
+            trace.Parsing.Duration.ShouldNotBe(0);
+
+            var expectedPaths = new HashSet<List<object>>
+            {
+                new List<object> { "hero" },
+                new List<object> { "hero", "name" },
+                new List<object> { "hero", "friends" },
+                new List<object> { "hero", "friends", 0, "name" },
+                new List<object> { "hero", "friends", 1, "name" },
+            };
+
+            var paths = new List<List<object>>();
+            foreach (var resolver in trace.Execution.Resolvers)
+            {
+                resolver.StartOffset.ShouldNotBe(0);
+                resolver.Duration.ShouldNotBe(0);
+                resolver.ParentType.ShouldNotBeNull();
+                resolver.ReturnType.ShouldNotBeNull();
+                resolver.FieldName.ShouldBe((string)resolver.Path.Last());
+                paths.Add(resolver.Path);
+            }
+            paths.Count.ShouldBe(expectedPaths.Count);
+            new HashSet<List<object>>(paths).ShouldBe(expectedPaths);
+        }
+    }
+}
---
+++ src/GraphQL/Instrumentation/ApolloTrace.cs
@@ -0,0 +1,60 @@
+namespace GraphQL.Instrumentation
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class ApolloTrace
+    {
+        public ApolloTrace(DateTime start, double durationMs)
+        {
+            this.StartTime = start;
+            this.EndTime = start.AddMilliseconds(durationMs);
+            this.Duration = ConvertTime(durationMs);
+        }
+
+        public int Version => 1;
+
+        public DateTime StartTime { get; }
+
+        public DateTime EndTime { get; }
+
+        public long Duration { get; }
+
+        public OperationTrace Parsing { get; } = new OperationTrace();
+
+        public OperationTrace Validation { get; } = new OperationTrace();
+
+        public ExecutionTrace Execution { get; } = new ExecutionTrace();
+
+        public static long ConvertTime(double ms) => (int)(ms * 1000 * 1000);
+
+        [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+        public class OperationTrace
+        {
+            public long StartOffset { get; set; }
+
+            public long Duration { get; set; }
+        }
+
+        [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+        public class ExecutionTrace
+        {
+            public List<ResolverTrace> Resolvers { get; } = new List<ResolverTrace>();
+        }
+
+        public class ResolverTrace : OperationTrace
+        {
+            public List<object> Path { get; set; } = new List<object>();
+
+            public string ParentType { get; set; }
+
+            public string FieldName { get; set; }
+
+            public string ReturnType { get; set; }
+        }
+    }
+}
