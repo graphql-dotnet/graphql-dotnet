2ff927122906de9a3679a81ddb414294a0330f36
Joe McBride <joe@uicraftsman.com>
2018-08-10 13:12:16 -0700

    Nested types tests (#774)

---
+++ src/GraphQL.Tests/Utilities/SchemaBuilderNestedTypesTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using GraphQL;
+using Xunit;
+
+namespace GraphQL.Tests.Utilities
+{
+    public class SchemaBuilderNestedTypesTests : SchemaBuilderTestBase
+    {
+        [Fact]
+        public void supports_nested_graph_types()
+        {
+            var defs = @"
+              type Droid {
+                  id: String
+                  name: String
+                  friend: Character
+              }
+
+              type Character {
+                  name: String
+              }
+
+              type Query {
+                  hero: Droid
+              }
+            ";
+
+            Builder.Types.Include<DroidType>();
+            Builder.Types.For("Droid").IsTypeOf<Droid>();
+            Builder.Types.Include<Query>();
+
+            var query = @"{ hero { id name friend { name } } }";
+            var expected = @"{ 'hero': { 'id' : '1', 'name': 'R2-D2', 'friend': { 'name': 'C3-PO' } } }";
+
+            AssertQuery(_ =>
+            {
+                _.Query = query;
+                _.Definitions = defs;
+                _.ExpectedResult = expected;
+            });
+        }
+
+        public class Droid
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Character
+        {
+            public string Name { get; set; }
+        }
+
+        public class MyUserContext
+        {
+        }
+
+        [GraphQLMetadata("Droid")]
+        public class DroidType
+        {
+            public string Id(Droid droid) => droid.Id;
+            public string Name(Droid droid) => droid.Name;
+            public Character Friend(MyUserContext context)
+            {
+                return new Character { Name = "C3-PO" };
+            }
+        }
+
+        public class Query
+        {
+            [GraphQLMetadata("hero")]
+            public Droid GetHero()
+            {
+                return new Droid { Id = "1", Name = "R2-D2" };
+            }
+        }
+    }
+}
