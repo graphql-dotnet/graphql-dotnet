bb608f66408b53dc6ed06864e5f0ef6449031428
Erik Gävert <erik@gavert.net>
2018-10-16 05:10:37 +0200

    Notify listeners during subscription execution (#852)
    This should resolve graphql-dotnet/server#162

---
+++ src/GraphQL.DataLoader.Tests/DataLoaderSubscriptionTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.DataLoader.Tests.Models;
+using GraphQL.DataLoader.Tests.Stores;
+using GraphQL.Subscription;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.DataLoader.Tests
+{
+    public class DataLoaderSubscriptionTest: QueryTestBase
+    {
+        protected async Task<SubscriptionExecutionResult> ExecuteSubscribeAsync(string query)
+        {
+            var result = await ExecuteQueryAsync<DataLoaderTestSchema>(query);
+            result.ShouldBeOfType<SubscriptionExecutionResult>();
+            return (SubscriptionExecutionResult)result;
+        }
+
+        [Fact]
+        public async Task OneResultOverSubscription_Works()
+        {
+            var order = Fake.Orders.Generate();        
+            var ordersMock = Services.GetRequiredService<Mock<IOrdersStore>>();
+            var orderStream = new ReplaySubject<Order>(1);
+
+            ordersMock.Setup(x => x.GetOrderObservable()).Returns(orderStream);
+            orderStream.OnNext(order);
+
+            var result = await ExecuteSubscribeAsync("subscription OrderAdded { orderAdded { orderId } }");
+
+            /* Then */
+            var stream = result.Streams.Values.FirstOrDefault();
+            var message = await stream.FirstOrDefaultAsync();
+
+            ordersMock.Verify(x => x.GetOrderObservable(), Times.Once);
+            ordersMock.VerifyNoOtherCalls();
+
+            message.Data.ShouldNotBeNull();
+        }
+    }
+}
---
+++ src/GraphQL.DataLoader.Tests/Types/SubscriptionType.cs
@@ -0,0 +1,38 @@
+using System;
+using GraphQL.DataLoader.Tests.Models;
+using GraphQL.DataLoader.Tests.Stores;
+using GraphQL.Resolvers;
+using GraphQL.Subscription;
+using GraphQL.Types;
+
+namespace GraphQL.DataLoader.Tests.Types
+{
+    public class SubscriptionType: ObjectGraphType
+    {
+        private readonly IOrdersStore ordersStore;
+        public SubscriptionType(IOrdersStore ordersStore)
+        {
+            this.ordersStore = ordersStore;
+
+            Name = "Subscription";
+
+            AddField(new EventStreamFieldType
+            {
+                Name = "orderAdded",
+                Type = typeof(OrderType),
+                Resolver = new FuncFieldResolver<Order>(ResolveMessage),
+                Subscriber = new EventStreamResolver<Order>(Subscribe)
+            });
+        }
+
+        private Order ResolveMessage(ResolveFieldContext context)
+        {
+            return context.Source as Order;
+        }
+
+        private IObservable<Order> Subscribe(ResolveEventStreamContext context)
+        {
+            return ordersStore.GetOrderObservable();
+        }
+    }
+}
