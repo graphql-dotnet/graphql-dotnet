670116674c27c6c92e50b46e979cb7f0fdbdbd35
Rensvind <tommy@otyglad.net>
2018-12-19 17:42:34 +0100

    Convert from StringValue in GuidGraphType (#952)
    * Convert from StringValue to GuidGraphType
    
    * Variable renaming and removal of unnecessary parse call

--- src/GraphQL/Types/GuidGraphType.cs
+++ src/GraphQL/Types/GuidGraphType.cs
@@ -7,8 +7,17 @@ namespace GraphQL.Types
     {
         public override object ParseLiteral(IValue value)
         {
-            var guidValue = value as GuidValue;
-            return guidValue?.Value;
+            if (value is GuidValue guidValue)
+            {
+                return guidValue.Value;
+            }
+
+            if (value is StringValue stringValue)
+            {
+                return ParseValue(stringValue.Value);
+            }
+
+            return null;
         }
 
         public override object ParseValue(object value) =>
