9396dbb90bdd3c1ff176090f5d539e511c10133e
Simon Cropp <simon.cropp@gmail.com>
2019-01-14 15:09:15 +1100

    remove redundant usings

--- src/GraphQL.DataLoader.Tests/DataLoaderSubscriptionTest.cs
+++ src/GraphQL.DataLoader.Tests/DataLoaderSubscriptionTest.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
 using System.Threading.Tasks;
 using GraphQL.DataLoader.Tests.Models;
 using GraphQL.DataLoader.Tests.Stores;
