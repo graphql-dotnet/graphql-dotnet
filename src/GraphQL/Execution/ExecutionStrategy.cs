282a0bad2c61d43b4e51de1ac376a7907da38bcd
Vedant Koditkar <vedkoditkar@gmail.com>
2019-01-15 23:47:28 +0530

    bubble null for non nullable array node

--- src/GraphQL/Execution/ExecutionStrategy.cs
+++ src/GraphQL/Execution/ExecutionStrategy.cs
@@ -233,9 +233,14 @@ namespace GraphQL.Execution
                 context.Errors.Add(error);
 
                 node.Result = null;
+
                 if (node.Parent is ObjectExecutionNode objectNode)
                 {
                     objectNode.SubFields = null;
+                }
+                else if (node.Parent is ArrayExecutionNode arrayNode)
+                {
+                    arrayNode.Items?.Clear();
                 }
             }
             catch (ExecutionError error)
