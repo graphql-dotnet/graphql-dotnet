d1d2648d50a460fc7453d4f09c90059019c2b55f
Ivan Maximov <sungam3r@yandex.ru>
2019-04-08 01:26:48 +0300

    fixes #1013 (#1104)

---
+++ src/GraphQL.Tests/Bugs/ArrayOfArrayTest.cs
@@ -0,0 +1,71 @@
+using GraphQL.Types;
+using Xunit;
+
+namespace GraphQL.Tests.Bugs
+{
+    public class ArrayOfArrayTest : QueryTestBase<ArrayOfArraySchema>
+    {
+        [Fact]
+        public void ArrayOfArray_Should_Return_As_Is()
+        {
+            var query = @"
+mutation {
+  create(input: {ints: [[1],[2,2],[3,3,3]] })
+  {
+    ints
+  }
+}
+";
+            var expected = @"{
+  ""create"": {
+    ""ints"": [[1],[2,2],[3,3,3]]
+  }
+}";
+            AssertQuerySuccess(query, expected, null);
+        }
+    }
+
+    public class ArrayOfArraySchema : Schema
+    {
+        public ArrayOfArraySchema()
+        {
+            Mutation = new ArrayOfArrayMutation();
+        }
+    }
+
+    public class ArrayOfArrayModel
+    {
+        public int[][] ints { get; set; }
+    }
+
+    public class ArrayOfArrayInput : InputObjectGraphType<ArrayOfArrayModel>
+    {
+        public ArrayOfArrayInput()
+        {
+            Field(o => o.ints);
+        }
+    }
+
+    public class ArrayOfArrayType : ObjectGraphType<ArrayOfArrayModel>
+    {
+        public ArrayOfArrayType()
+        {
+            Field(o => o.ints);
+        }
+    }
+
+    public class ArrayOfArrayMutation : ObjectGraphType
+    {
+        public ArrayOfArrayMutation()
+        {
+            Field<ArrayOfArrayType>(
+                "create",
+                arguments: new QueryArguments(new QueryArgument<ArrayOfArrayInput> { Name = "input" }),
+                resolve: ctx =>
+                {
+                    var arg = ctx.GetArgument<ArrayOfArrayModel>("input");
+                    return arg;
+                });
+        }
+    }
+}
