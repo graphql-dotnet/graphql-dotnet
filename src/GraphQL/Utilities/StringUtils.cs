668814295ab817bedd013f1e5312c0ebb83e4d41
Ivan Maximov <sungam3r@yandex.ru>
2019-04-07 13:27:49 +0300

    string allocations small optimization

--- src/GraphQL/Utilities/StringUtils.cs
+++ src/GraphQL/Utilities/StringUtils.cs
@@ -7,6 +7,7 @@ namespace GraphQL.Utilities
 {
     public static class StringUtils
     {
+        private static readonly string[] empty = new string[0];
         private static Regex reWords = new Regex(@"[A-Z\xc0-\xd6\xd8-\xde]?[a-z\xdf-\xf6\xf8-\xff]+(?:['’](?:d|ll|m|re|s|t|ve))?(?=[\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000]|[A-Z\xc0-\xd6\xd8-\xde]|$)|(?:[A-Z\xc0-\xd6\xd8-\xde]|[^\ud800-\udfff\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\d+\u2700-\u27bfa-z\xdf-\xf6\xf8-\xffA-Z\xc0-\xd6\xd8-\xde])+(?:['’](?:D|LL|M|RE|S|T|VE))?(?=[\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000]|[A-Z\xc0-\xd6\xd8-\xde](?:[a-z\xdf-\xf6\xf8-\xff]|[^\ud800-\udfff\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\d+\u2700-\u27bfa-z\xdf-\xf6\xf8-\xffA-Z\xc0-\xd6\xd8-\xde])|$)|[A-Z\xc0-\xd6\xd8-\xde]?(?:[a-z\xdf-\xf6\xf8-\xff]|[^\ud800-\udfff\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\d+\u2700-\u27bfa-z\xdf-\xf6\xf8-\xffA-Z\xc0-\xd6\xd8-\xde])+(?:['’](?:d|ll|m|re|s|t|ve))?|[A-Z\xc0-\xd6\xd8-\xde]+(?:['’](?:D|LL|M|RE|S|T|VE))?|\d+|(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*");
 
         /// <summary>
@@ -22,14 +23,19 @@ namespace GraphQL.Utilities
 
         public static string Capitalize(string str)
         {
-            str = str.ToLowerInvariant();
-            return str.Substring(0, 1).ToUpperInvariant() + str.Substring(1);
+            switch (str.Length)
+            {
+                case 0: return str;
+                case 1: return str.ToUpperInvariant();
+                default:
+                    str = str.ToLowerInvariant();
+                    return char.ToUpperInvariant(str[0]) + str.Substring(1);
+            }
         }
 
         public static string ToCamelCase(string str)
         {
-            return ChangeCase(str, "", (word, index) =>
-                (index == 0 ? word.ToLowerInvariant() : Capitalize(word)));
+            return ChangeCase(str, "", (word, index) => index == 0 ? word.ToLowerInvariant() : Capitalize(word));
         }
 
         public static string ToConstantCase(string str)
@@ -41,7 +47,6 @@ namespace GraphQL.Utilities
         {
             return ChangeCase(str, "", Capitalize);
         }
-
 
         public static string ChangeCase(string str, string sep, Func<string, string> composer)
         {
@@ -55,12 +60,11 @@ namespace GraphQL.Utilities
 
             foreach (string word in ToWords(str))
             {
-                result += ((index == 0 ? "" : sep) + composer(word, index++));
+                result += (index == 0 ? "" : sep) + composer(word, index++);
             }
 
             return result;
         }
-
 
         /// <summary>
         /// Given [ A, B, C ] return '"A", "B", or "C"'.
@@ -86,7 +90,7 @@ namespace GraphQL.Utilities
         {
             if (options == null)
             {
-                return new string[0];
+                return empty;
             }
 
             var optionsByDistance = new Dictionary<string, int>();
