7e7b23a3e344063b3127ac71430befe0f98ee264
Ivan Maximov <sungam3r@yandex.ru>
2019-07-27 00:38:59 +0300

    test for #1223

--- src/GraphQL.Tests/Extensions/GraphQLExtensionsTests.cs
+++ src/GraphQL.Tests/Extensions/GraphQLExtensionsTests.cs
@@ -1,7 +1,11 @@
-using System;
 using GraphQL.StarWars.Types;
 using GraphQL.Types;
 using Shouldly;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace GraphQL.Tests.Extensions
@@ -13,6 +17,25 @@ namespace GraphQL.Tests.Extensions
         {
             Should.Throw<InvalidOperationException>(
                 () => typeof(ListGraphType<ListGraphType<EpisodeEnum>>).BuildNamedType(_ => null));
+        }
+
+        [Fact]
+        void GetResult_Extension_Should_Work()
+        {
+            var task1 = Task.FromResult(42);
+            task1.GetResult().ShouldBe(42);
+
+            var obj = new object();
+            var task2 = Task.FromResult(obj);
+            task2.GetResult().ShouldBe(obj);
+
+            IEnumerable collection = new List<string>();
+            var task3 = Task.FromResult(collection);
+            task3.GetResult().ShouldBe(collection);
+
+            ILookup<string, EqualityComparer<DateTime>> lookup = new List<EqualityComparer<DateTime>>().ToLookup(i => i.GetHashCode().ToString());
+            var task4 = Task.FromResult(lookup);
+            task4.GetResult().ShouldBe(lookup);
         }
     }
 }
