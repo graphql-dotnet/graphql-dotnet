93155f1d12901be4897044a8953e6270b631b025
Ivan Maximov <sungam3r@yandex.ru>
2019-08-16 15:08:48 +0300

    add test

---
+++ src/GraphQL.Tests/Bugs/Bug947.cs
@@ -0,0 +1,67 @@
+using GraphQL.Types;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Numerics;
+
+namespace GraphQL.Tests.Bugs
+{
+    public class Bug947
+    {
+        [Fact]
+        public void GetArgument_Should_Return_Properly_Converted_Values()
+        {
+            var context = new ResolveFieldContext()
+            {
+                Arguments = new Dictionary<string, object>
+                {
+                    { "int", 10 },
+                    { "string", "hello" },
+                    { "vector", new Vector3(1.1f, 2.2f, 3.3f) },
+                    { "object", new Dictionary<string, object>
+                                {
+                                    { "inner_int", 15 },
+                                    { "inner_string", "ok" }
+                                }
+                    }
+                }
+            };
+
+            context.GetArgument("int", 100).ShouldBe(10);
+            context.GetArgument<object>("int").ShouldBe(10);
+            context.GetArgument("ints", 100).ShouldBe(100);
+
+            context.GetArgument("vector", Vector3.One).ShouldBe(new Vector3(1.1f, 2.2f, 3.3f));
+            context.GetArgument<object>("vector").ShouldBe(new Vector3(1.1f, 2.2f, 3.3f));
+            context.GetArgument("vectors", Vector3.One).ShouldBe(Vector3.One);
+
+            context.GetArgument("string", "bye").ShouldBe("hello");
+            context.GetArgument<object>("string").ShouldBe("hello");
+            context.GetArgument("strong", "bye").ShouldBe("bye");
+
+            context.GetArgument<object>("object").ShouldBeOfType<Dictionary<string, object>>();
+            context.GetArgument<SomeObject>("object").inner_int.ShouldBe(15);
+
+            Should.Throw<InvalidOperationException>(() => context.GetArgument<ResolveFieldContext>("string"));
+
+            var otherObject = context.GetArgument<SomeOtherObject>("object");
+            otherObject.unknown.ShouldBe(0);
+            otherObject.unknown2.ShouldBeNull();
+        }
+    }
+
+    public class SomeObject
+    {
+        public int inner_int { get; set; }
+
+        public string inner_string { get; set; }
+    }
+
+    public class SomeOtherObject
+    {
+        public int unknown { get; set; }
+
+        public string unknown2 { get; set; }
+    }
+}
