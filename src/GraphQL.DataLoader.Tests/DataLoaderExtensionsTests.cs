0dfe3f682df5b188eae6812030d13f2579531819
Ben McCallum <ben.mccallum@live.com.au>
2019-09-03 11:21:39 +0200

    refactor: Remove unused usings

--- src/GraphQL.DataLoader.Tests/DataLoaderExtensionsTests.cs
+++ src/GraphQL.DataLoader.Tests/DataLoaderExtensionsTests.cs
@@ -3,10 +3,8 @@ using GraphQL.DataLoader.Tests.Stores;
 using Moq;
 using Nito.AsyncEx;
 using Shouldly;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
