9bf9bda8f5e039bd35858a6e579cb463b7b8bc8f
Shane Krueger <Shane@acdmail.com>
2019-10-13 10:38:57 -0400

    Unseal ServiceProviderProxy and add comments

--- src/GraphQL/Execution/ServiceProviderProxy.cs
+++ src/GraphQL/Execution/ServiceProviderProxy.cs
@@ -5,28 +5,46 @@ using System.Text;
 
 namespace GraphQL.Execution
 {
-    public sealed class ServiceProviderProxy : IServiceProvider, IDisposable
+    /// <summary>
+    /// A service provider proxy, which creates a service scope upon first use
+    /// </summary>
+    public class ServiceProviderProxy : IServiceProvider, IDisposable
     {
-        private readonly IServiceProvider _rootServiceProvider;
-        private IServiceScope _serviceScope;
-        private IServiceProvider _serviceProvider;
+        protected readonly IServiceProvider _rootServiceProvider;
+        protected IServiceScope _serviceScope;
+        protected IServiceProvider _serviceProvider;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceProviderProxy"/> class.
+        /// </summary>
+        /// <param name="rootServiceProvider">The root service provider.</param>
         public ServiceProviderProxy(IServiceProvider rootServiceProvider)
         {
             _rootServiceProvider = rootServiceProvider ?? throw new ArgumentNullException(nameof(rootServiceProvider));
         }
 
+        /// <summary>
+        /// Disposes of the created service scope, if necessary
+        /// </summary>
         public void Dispose()
         {
             _serviceScope?.Dispose();
         }
 
-        object IServiceProvider.GetService(Type serviceType)
+        /// <summary>
+        /// Gets the service object of the specified type, creating a scope if none has yet been created
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>A service object of type serviceType. -or- null if there is no service object of type serviceType.</returns>
+        public object GetService(Type serviceType)
         {
+            // if the root service provider supports scopes, create a scope
+            // note that DefaultServiceProvider does not support scopes, but nor does it
+            //   need to as it effectively always creates objects as transients
             if (_serviceProvider == null)
             {
-                _serviceScope = _rootServiceProvider.CreateScope();
-                _serviceProvider = _serviceScope.ServiceProvider;
+                _serviceScope = _rootServiceProvider.GetService<IServiceScopeFactory>()?.CreateScope();
+                _serviceProvider = _serviceScope?.ServiceProvider ?? _rootServiceProvider;
             }
             return _serviceProvider.GetService(serviceType);
         }
