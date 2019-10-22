94b386acf4c8efe71589b933be459b3b540e9598
Ivan Maximov <sungam3r@yandex.ru>
2019-10-23 01:48:26 +0300

    NameFieldResolver can call methods

---
+++ src/GraphQL.Tests/NameFieldResolverTests.cs
@@ -0,0 +1,48 @@
+using GraphQL.Language.AST;
+using GraphQL.Resolvers;
+using GraphQL.Types;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace GraphQL.Tests
+{
+    public class NameFieldResolverTests
+    {
+        [Theory]
+        [InlineData("age", 20)]
+        [InlineData("AGE", 20)]
+        [InlineData("Name", "Anyone")]
+        [InlineData("naMe", "Anyone")]
+        [InlineData("FullInfo", "Anyone 20")]
+        [InlineData("fullInfo", "Anyone 20")]
+        [InlineData("unknown", "", true)]
+        [InlineData("FullInfoWithParam", "", true)]
+        public void resolve_should_work_with_properties_and_methods(string name, object expected, bool throws = false)
+        {
+            var person = new Person
+            {
+                Age = 20,
+                Name = "Anyone"
+            };
+
+            Func<object> result = () => NameFieldResolver.Instance.Resolve(new ResolveFieldContext { Source = person, FieldAst = new Field(null, new NameNode(name) { }) });
+
+            if (throws)
+                Should.Throw<InvalidOperationException>(() => result());
+            else
+                result().ShouldBe(expected);
+        }
+
+        public class Person
+        {
+            public int Age { get; set; }
+
+            public string Name { get; set; }
+
+            public string FullInfo() => Name + " " + Age;
+
+            public string FullInfoWithParam(string prefix) => prefix + FullInfo();
+        }
+    }
+}
--- src/GraphQL/Resolvers/NameFieldResolver.cs
+++ src/GraphQL/Resolvers/NameFieldResolver.cs
@@ -7,7 +7,7 @@ namespace GraphQL.Resolvers
 {
     internal class NameFieldResolver : IFieldResolver
     {
-        private static readonly ConcurrentDictionary<(Type targetType, string propertyName), Func<object, object>> _propertyDelegates
+        private static readonly ConcurrentDictionary<(Type targetType, string name), Func<object, object>> _delegates
             = new ConcurrentDictionary<(Type, string), Func<object, object>>();
 
         private NameFieldResolver() { }
@@ -21,15 +21,15 @@ namespace GraphQL.Resolvers
             if (source == null || name == null)
                 return null;
 
-            // We use reflection to create a delegate to access the property
+            // We use reflection to create a delegate to access the property/method
             // Then cache the delegate
-            // This is over 10x faster that just using reflection to get the property value
-            var func = _propertyDelegates.GetOrAdd((source.GetType(), name), t => CreatePropertyDelegate(t.targetType, t.propertyName));
+            // This is over 10x faster that just using reflection to get the property/method value
+            var func = _delegates.GetOrAdd((source.GetType(), name), t => CreateDelegate(t.targetType, t.name));
             return func(source);
         }
 
         /// <summary>
-        /// Dynamically creates the necessary delegate in runtime to get the property value of the specified type.
+        /// Dynamically creates the necessary delegate in runtime to get the property/method value of the specified type.
         ///
         /// Example:
         /// public class Person
@@ -39,21 +39,44 @@ namespace GraphQL.Resolvers
         ///
         /// So resulting Func will be generated as {x => Convert(Convert(x, Person).Age, Object)}
         /// 1. First, the input parameter 'x' is converted from the object to a specific type.
-        /// 2. The required property is extracted from casted value.
+        /// 2. The required property or method is extracted from casted value.
         /// 3. Then result is converted again to the object and returned from the method.
         /// </summary>
-        /// <param name="target"> The type from which you want to get the property. </param>
-        /// <param name="name"> Property name. </param>
-        /// <returns> Compiled delegate to get property value. </returns>
-        private static Func<object, object> CreatePropertyDelegate(Type target, string name)
+        /// <param name="target"> The type from which you want to get the value. </param>
+        /// <param name="name"> Property/method name. </param>
+        /// <returns> Compiled delegate to get the value. </returns>
+        private static Func<object, object> CreateDelegate(Type target, string name)
         {
             var parameter = Expression.Parameter(typeof(object), "x");
-            // Property name resolution works fine even for 'age' name instead of 'Age' since Expression.Property uses
-            // BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy under the hood.
-            // If the property is not found then will throw ArgumentException: Instance property 'name' is not defined for type 'target' (Parameter 'propertyName')
-            var member = Expression.Property(Expression.Convert(parameter, target), name); 
-            var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(member, typeof(object)), parameter);
+
+            var member = Expression.Convert(parameter, target);
+            var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(CreateAccessorExpression(), typeof(object)), parameter);
             return lambda.Compile();
+
+            Expression CreateAccessorExpression()
+            {
+                try
+                {
+                    try
+                    {
+                        // Property name resolution works fine even for 'age' name instead of 'Age' since Expression.Property uses
+                        // BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy under the hood.
+                        // If the property is not found then will throw ArgumentException: Instance property 'name' is not defined for type 'target' (Parameter 'propertyName')
+                        return Expression.Property(member, name);
+                    }
+                    catch (ArgumentException)
+                    {
+                        // Method name resolution works fine even for 'get' name instead of 'Get' since Expression.Call uses
+                        // Type.FilterNameIgnoreCase under the hood.
+                        // If the method is not found then will throw InvalidOperationException: 'No method 'name' on type 'target' is compatible with the supplied arguments.
+                        return Expression.Call(member, name, Type.EmptyTypes);
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    throw new InvalidOperationException($"Expected to find property or method {name} on {target.Name} but it does not exist.");
+                }
+            }
         }
     }
 }
