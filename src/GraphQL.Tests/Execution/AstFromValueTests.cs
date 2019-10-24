35520304472f322f151ef7ee9ff73be51f1739d6
Joseph Moran III <jmoran@clarkinc.biz>
2019-10-24 09:01:39 -0400

    Remove unneeded classes from test case for ByteValueConverter

--- src/GraphQL.Tests/Execution/AstFromValueTests.cs
+++ src/GraphQL.Tests/Execution/AstFromValueTests.cs
@@ -96,7 +96,7 @@ namespace GraphQL.Tests.Execution
             byte value = 12;
             var result = value.AstFromValue(schema, new ByteGraphType());
             result.ShouldNotBeNull();
-            result.ShouldBeOfType<GraphQL.Language.AST.ByteValue>();
+            result.ShouldBeOfType<.ByteValue>();
         }
 
         [Fact]
@@ -128,56 +128,6 @@ namespace GraphQL.Tests.Execution
         public IValue Convert(object value, IGraphType type)
         {
             return new ByteValue((byte)value);
-        }
-    }
-
-    internal class ByteValue : ValueNode<byte>
-    {
-        public ByteValue(byte value)
-        {
-            Value = value;
-        }
-
-        protected override bool Equals(ValueNode<byte> node)
-        {
-            return Value == node.Value;
-        }
-    }
-
-    internal class ByteGraphType : ScalarGraphType
-    {
-        public ByteGraphType()
-        {
-            Name = "Byte";
-        }
-
-        public override object Serialize(object value)
-        {
-            return ParseValue(value);
-        }
-
-        public override object ParseValue(object value)
-        {
-            if (value == null)
-            {
-                return null;
-            }
-
-            try
-            {
-                var result = Convert.ToByte(value);
-                return result;
-            }
-            catch (FormatException)
-            {
-                return null;
-            }
-        }
-
-        public override object ParseLiteral(IValue value)
-        {
-            var byteValue = value as ByteValue;
-            return byteValue?.Value;
         }
     }
 }
