487ee145aab765073c779030ab54dcd02c5b3a66
Shane Krueger <Shane@acdmail.com>
2019-10-29 15:07:14 -0400

    Demonstrate connection types

--- src/GraphQL.StarWars/Types/DroidTypeDI.cs
+++ src/GraphQL.StarWars/Types/DroidTypeDI.cs
@@ -4,6 +4,9 @@ using GraphQL.DI;
 using System.ComponentModel;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using GraphQL.Types.Relay;
+using GraphQL.Types.Relay.DataObjects;
+using GraphQL.Builders;
 
 namespace GraphQL.StarWars.Types
 {
@@ -11,19 +14,10 @@ namespace GraphQL.StarWars.Types
 
     public class DroidTypeDIGraph : DIObjectGraphType<DroidTypeDI, Droid>
     {
-        public DroidTypeDIGraph(StarWarsData data)
+        public DroidTypeDIGraph()
         {
             Field(d => d.Id).Description("The id of the droid.");
             Field(d => d.Name, nullable: true).Description("The name of the droid.");
-
-            Connection<CharacterInterface>()
-                .Name("friendsConnection")
-                .Description("A list of a character's friends.")
-                .Bidirectional()
-                .Resolve(context =>
-                {
-                    return context.GetPagedResults<Droid, StarWarsCharacter>(data, context.Source.Friends);
-                });
 
             Interface<CharacterInterface>();
         }
@@ -42,6 +36,21 @@ namespace GraphQL.StarWars.Types
 
         [GraphType(typeof(ListGraphType<CharacterInterface>))]
         public IEnumerable<StarWarsCharacter> Friends([FromSource] Droid droid) => _data.GetFriends(droid);
+
+        [GraphType(typeof(ConnectionType<CharacterInterface, EdgeType<CharacterInterface>>))]
+        [Description("A list of a character's friends.")]
+        public Connection<StarWarsCharacter> FriendsConnection(ResolveFieldContext context, [FromSource] Droid droid, int? first, int? last, string after, string before)
+        {
+            //for convenience, a typed [FromSource] is used, rather than the ResolveFieldContext
+            //a typed ResolveFieldContext<> could be used, and the typed source argument eliminated, but
+            //  the ResolveConnectionContext constructor would need to accept a typed ResolveFieldContext parameter
+            //also the connection arguments are listed here, although the connectionContext is created
+            //  below which handles the first/last/pagesize/etc in this sample
+            //it's also possible to make the DIObjectGraphType code intelligently detect a ResolveConnectionContext<>
+            //  parameter and add the appropriate arguments (probably the right answer)
+            var connectionContext = new ResolveConnectionContext<Droid>(context, false, 100);
+            return connectionContext.GetPagedResults<Droid, StarWarsCharacter>(_data, droid.Friends);
+        }
 
         [GraphType(typeof(ListGraphType<EpisodeEnum>))]
         [Description("Which movie they appear in.")]
