61277438781efe885e219a6f9f114606c20b611f
Shane Krueger <Shane@acdmail.com>
2019-11-01 09:38:30 -0400

    Added DelayLoader.ContinueWith

---
+++ src/GraphQL.DI/DelayLoader/DelayLoaderExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphQL.DI.DelayLoader
+{
+    public static class DelayLoaderExtensions
+    {
+        public static IDelayLoadedResult<TResult> Then<TOut, TResult>(this IDelayLoadedResult<TOut> parent, Func<TOut, Task<TResult>> func)
+        {
+            return new ContinueWith<TOut, TResult>(parent, func);
+        }
+    }
+}
