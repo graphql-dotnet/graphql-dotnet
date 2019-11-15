7d4c7a851afb99f5749590f4a5b90570ab34a5e6
Simon Cropp <simon.cropp@gmail.com>
2019-11-15 14:03:37 +1100

    remove redundant obsolete and re-approve

--- src/GraphQL/SchemaExtensions.cs
+++ src/GraphQL/SchemaExtensions.cs
@@ -7,12 +7,6 @@ namespace GraphQL
 {
     public static class SchemaExtensions
     {
-        [Obsolete(@"This action is async. Use ExecuteAsync. Will be removed in v4.")]
-        public static string Execute(this ISchema schema, Action<ExecutionOptions> configure)
-        {
-            return ExecuteAsync(schema, configure).GetAwaiter().GetResult();
-        }
-
         public static async Task<string> ExecuteAsync(this ISchema schema, Action<ExecutionOptions> configure)
         {
             var executor = new DocumentExecuter();
