111c5da3080ed55c21779093075ad5bd19967bc0
Ivan Maximov <sungam3r@yandex.ru>
2019-12-27 16:52:50 +0300

    Add more directive locations for visitors (#1452)

--- src/GraphQL/Utilities/ISchemaNodeVisitor.cs
+++ src/GraphQL/Utilities/ISchemaNodeVisitor.cs
@@ -3,11 +3,30 @@ using GraphQL.Types;
 
 namespace GraphQL.Utilities
 {
+    // interface members aligned according to  https://www.apollographql.com/docs/graphql-tools/schema-directives/#implementing-schema-directives
     public interface ISchemaNodeVisitor
     {
-        void VisitObjectGraphType(ObjectGraphType type);
-        void VisitField(FieldType field);
+        void VisitSchema(Schema schema);
+
+        void VisitScalar(ScalarGraphType scalar);
+
+        void VisitObject(IObjectGraphType type);
+
+        void VisitFieldDefinition(FieldType field);
+
+        void VisitArgumentDefinition(QueryArgument argument);
+
+        void VisitInterface(InterfaceGraphType iface);
+
+        void VisitUnion(UnionGraphType union);
+
+        void VisitEnum(EnumerationGraphType type);
+
         void VisitEnumValue(EnumValueDefinition value);
+
+        void VisitInputObject(InputObjectGraphType type);
+
+        void VisitInputFieldDefinition(FieldType field);
     }
 
     public abstract class BaseSchemaNodeVisitor : ISchemaNodeVisitor
@@ -18,15 +37,47 @@ namespace GraphQL.Utilities
             return new SyncToAsyncResolverAdapter(inner);
         }
 
-        public virtual void VisitObjectGraphType(ObjectGraphType type)
+        public virtual void VisitSchema(Schema schema)
         {
         }
 
-        public virtual void VisitField(FieldType field)
+        public virtual void VisitScalar(ScalarGraphType scalar)
+        {
+        }
+
+        public virtual void VisitObject(IObjectGraphType type)
+        {
+        }
+
+        public virtual void VisitFieldDefinition(FieldType field)
+        {
+        }
+
+        public virtual void VisitArgumentDefinition(QueryArgument argument)
+        {
+        }
+
+        public virtual void VisitInterface(InterfaceGraphType iface)
+        {
+        }
+
+        public virtual void VisitUnion(UnionGraphType union)
+        {
+        }
+
+        public virtual void VisitEnum(EnumerationGraphType type)
         {
         }
 
         public virtual void VisitEnumValue(EnumValueDefinition value)
+        {
+        }
+
+        public virtual void VisitInputObject(InputObjectGraphType type)
+        {
+        }
+
+        public virtual void VisitInputFieldDefinition(FieldType field)
         {
         }
     }
