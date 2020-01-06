f00fe35ce9d6d5a56efed6566dd3106585f7194c
Ivan Maximov <sungam3r@yandex.ru>
2020-01-06 15:58:19 +0300

    Field description can be taken from the interface field (#1470)
    * fixes #1004

---
+++ src/GraphQL.Tests/Bugs/Issue1004.cs
@@ -0,0 +1,66 @@
+using GraphQL.Types;
+using Xunit;
+
+namespace GraphQL.Tests.Bugs
+{
+    public class Issue1004 : QueryTestBase<DescriptionFromInterfaceSchema>
+    {
+        [Fact]
+        public void Should_Return_Field_Description_From_Interface_If_Not_Overridden()
+        {
+            var query = @"
+{
+  __type(name: ""Query"")
+  {
+    fields
+    {
+      description
+    }
+  }
+}
+";
+            var expected = @"{
+  ""__type"": {
+    ""fields"": [
+      {
+        ""description"": ""Very important field1""
+      },
+      {
+        ""description"": ""Not so important""
+      }
+    ]
+  }
+}";
+            AssertQuerySuccess(query, expected, null);
+        }
+    }
+
+    public class DescriptionFromInterfaceSchema : Schema
+    {
+        public DescriptionFromInterfaceSchema()
+        {
+            Query = new Issue1004Query();
+        }
+    }
+
+    public class Issue1004Query : ObjectGraphType
+    {
+        public Issue1004Query()
+        {
+            Name = "Query";
+            IsTypeOf = o => true;
+            Field<StringGraphType>("field1", resolve: ctx => throw null);
+            Field<StringGraphType>("field2", description: "Not so important", resolve: ctx => throw null);
+            Interface<Issue1004Interface>();
+        }
+    }
+
+    public class Issue1004Interface : InterfaceGraphType
+    {
+        public Issue1004Interface()
+        {
+            Field<StringGraphType>("field1", "Very important field1");
+            Field<StringGraphType>("field2", "Very important field2");
+        }
+    }
+}
