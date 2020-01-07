488ca2bfad80c9eec14e8a8c4b164f1da5aefe46
Lowell Smith <lowell_smith@trimble.com>
2020-01-07 12:50:55 -0500

    Removed comma

--- src/GraphQL.NewtonsoftJson/StringExtensions.cs
+++ src/GraphQL.NewtonsoftJson/StringExtensions.cs
@@ -47,7 +47,7 @@ namespace GraphQL.NewtonsoftJson
         }
 
         /// <summary>
-        /// Gets the value contained in a JObject, JValue, JProperty, or JArray.
+        /// Gets the value contained in a JObject, JValue, JProperty or JArray.
         /// </summary>
         /// <param name="value">The object containing the value to extract.</param>
         /// <remarks>If the value is a recognized type, it is returned unaltered.</remarks>
