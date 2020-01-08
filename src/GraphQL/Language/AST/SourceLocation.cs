bf6c889345764fd3a1798101be099ce2fbd8173c
Ivan Maximov <sungam3r@yandex.ru>
2020-01-08 12:29:36 +0300

    use is null instead of object.ReferenceEquals

--- src/GraphQL/Language/AST/SourceLocation.cs
+++ src/GraphQL/Language/AST/SourceLocation.cs
@@ -28,7 +28,7 @@ namespace GraphQL.Language.AST
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != GetType()) return false;
             return Equals((SourceLocation)obj);
