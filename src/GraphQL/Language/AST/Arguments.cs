09be0ccede78daceadac85e3cab8059da86132d1
Ivan Maximov <sungam3r@yandex.ru>
2020-01-09 03:54:06 +0300

    use is null instead of object.ReferenceEquals (#1489)

--- src/GraphQL/Language/AST/Arguments.cs
+++ src/GraphQL/Language/AST/Arguments.cs
@@ -22,7 +22,7 @@ namespace GraphQL.Language.AST
 
         public override bool IsEqualTo(INode obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != GetType()) return false;
             return Equals((Arguments)obj);
--- src/GraphQL/Language/AST/NamedType.cs
+++ src/GraphQL/Language/AST/NamedType.cs
@@ -24,7 +24,7 @@ namespace GraphQL.Language.AST
 
         public override bool IsEqualTo(INode obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != GetType()) return false;
 
