83b7a6d4df826a30aac9621c414111496eeff561
Ivan Maximov <sungam3r@yandex.ru>
2020-01-12 00:32:09 +0300

    Add benchmarks (#1297)

---
+++ src/GraphQL.Benchmarks/GraphQL.Benchmarks.Reference.md
@@ -0,0 +1,16 @@
+``` ini
+
+BenchmarkDotNet=v0.12.0, OS=Windows 10.0.17134.1184 (1803/April2018Update/Redstone4)
+Intel Core i5-6200U CPU 2.30GHz (Skylake), 1 CPU, 4 logical and 2 physical cores
+Frequency=2343755 Hz, Resolution=426.6658 ns, Timer=TSC
+.NET Core SDK=3.0.100
+  [Host]     : .NET Core 3.0.0 (CoreCLR 4.700.19.46205, CoreFX 4.700.19.46214), X86 RyuJIT
+  DefaultJob : .NET Core 3.0.0 (CoreCLR 4.700.19.46205, CoreFX 4.700.19.46214), X86 RyuJIT
+
+```
+Baseline:
+
+|        Method |        Mean |     Error |    StdDev |      Median |    Gen 0 |   Gen 1 | Gen 2 | Allocated |
+|-------------- |------------:|----------:|----------:|------------:|---------:|--------:|------:|----------:|
+| Introspection | 4,551.45 us | 80.492 us | 75.292 us | 4,569.91 us | 320.3125 | 93.7500 |     - | 959.38 KB |
+|          Hero |    83.63 us |  1.659 us |  3.910 us |    82.08 us |  11.2305 |       - |     - |  17.26 KB |
---
+++ src/GraphQL.Benchmarks/Program.cs
@@ -0,0 +1,41 @@
+using BenchmarkDotNet.Running;
+using System;
+using System.Threading;
+
+namespace GraphQL.Benchmarks
+{
+    internal static class Program
+    {
+        // Call without args for BenchmarkDotNet
+        // Call with some arbitrary args for any memory profiler
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                BenchmarkRunner.Run<ExecutionBenchmark>();
+            else
+                RunMemoryProfilerPayload();
+        }
+
+        private static void RunMemoryProfilerPayload()
+        {
+            var bench = new ExecutionBenchmark();
+            bench.GlobalSetup();
+
+            int count = 0;
+            while (true)
+            {
+                bench.Hero();
+
+                Thread.Sleep(10);
+
+                ++count;
+                if (count == 100)
+                    break;
+            }
+
+            Console.WriteLine("========== END ==========");
+            Console.ReadLine();
+        }
+    }
+
+}
