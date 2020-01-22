433e6627855af10e619ac4118a3ffb527cf32bb0
Ivan Maximov <sungam3r@yandex.ru>
2020-01-22 14:29:10 +0300

    trigger build

--- src/GraphQL/Utilities/AstPrinter.cs
+++ src/GraphQL/Utilities/AstPrinter.cs
@@ -115,15 +115,7 @@ namespace GraphQL.Utilities
 
         public object Source { get; }
 
-        public TType SourceAs<TType>()
-        {
-            if (Source != null)
-            {
-                return (TType)Source;
-            }
-
-            return default;
-        }
+        public TType SourceAs<TType>() => Source == null ? default : (TType)Source;
     }
 
     public interface IValueResolver
