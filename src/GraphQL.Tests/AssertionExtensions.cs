367b16311f1bd6b8d4cb8493f67dc570f16f1e99
Ben McCallum <ben.mccallum@live.com.au>
2020-02-04 15:59:33 +0100

    Add xml comments to assertion extensions to make them clearer.

--- src/GraphQL.Tests/AssertionExtensions.cs
+++ src/GraphQL.Tests/AssertionExtensions.cs
@@ -5,12 +5,29 @@ namespace GraphQL.Tests
 {
     public static class AssertionExtensions
     {
+        /// <summary>
+        /// Compares two strings after normalizing line breaks.
+        /// </summary>
+        /// <param name="actual">Actual value.</param>
+        /// <param name="expected">Expected value.</param>
+        /// <param name="customMessage">A custom message if they aren't the same.</param>
         public static void ShouldBeCrossPlat(this string actual, string expected, string customMessage)
             => Normalize(actual).ShouldBe(Normalize(expected), customMessage);
 
+        /// <summary>
+        /// Compares two strings after normalizing line breaks.
+        /// </summary>
+        /// <param name="actual">Actual value.</param>
+        /// <param name="expected">Expected value.</param>
         public static void ShouldBeCrossPlat(this string actual, string expected)
             => Normalize(actual).ShouldBe(Normalize(expected));
 
+        /// <summary>
+        /// Compares two JSON strings after normalizing line breaks and parsing then writing them back out
+        /// to ignore any whitespace differences.
+        /// </summary>
+        /// <param name="actualJson">Actual value.</param>
+        /// <param name="expectedJson">Expected value.</param>
         public static void ShouldBeCrossPlatJson(this string actualJson, string expectedJson)
         {
             using var actualJsonDoc = JsonDocument.Parse(Normalize(actualJson));
