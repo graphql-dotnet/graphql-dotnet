bd247cf43ede961d7265e9a1c91f68ba1dc94ac9
SimonCropp <simon.cropp@gmail.com>
2020-02-06 22:33:30 +1100

    fix utc

--- src/GraphQL.Tests/Types/DateGraphTypeTests.cs
+++ src/GraphQL.Tests/Types/DateGraphTypeTests.cs
@@ -65,7 +65,7 @@ namespace GraphQL.Tests.Types
         {
             CultureTestHelper.UseCultures(() =>
             {
-                ((DateTime) _type.ParseValue("2015-11-21T19:59:32.987+0200")).Kind.ShouldBe(
+                ((DateTime) _type.ParseValue("2015-11-21")).Kind.ShouldBe(
                     DateTimeKind.Utc);
             });
         }
--- src/GraphQL/Types/DateGraphType.cs
+++ src/GraphQL/Types/DateGraphType.cs
@@ -37,9 +37,9 @@ namespace GraphQL.Types
 
             if (value is string valueAsString)
             {
-                if (DateTime.TryParseExact(valueAsString, "yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None, out var date))
+                if (DateTime.TryParseExact(valueAsString, "yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AssumeUniversal, out var date))
                 {
-                    return date;
+                    return date.ToUniversalTime();
                 }
                 throw new FormatException($"Could not parse date. Expected yyyy-MM-dd. Value: {valueAsString}");
             }
