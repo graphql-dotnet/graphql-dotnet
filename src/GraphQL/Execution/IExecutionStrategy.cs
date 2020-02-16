d5a3a95f1d2f4d8dec1591c5ac3d8f03f72b01ca
Shane Krueger <Shane@acdmail.com>
2020-02-16 09:42:16 -0500

    Add xml comments to IExecutionStrategy

--- src/GraphQL/Execution/IExecutionStrategy.cs
+++ src/GraphQL/Execution/IExecutionStrategy.cs
@@ -4,6 +4,10 @@ namespace GraphQL.Execution
 {
     public interface IExecutionStrategy
     {
+        /// <summary>
+        /// Executes a GraphQL request and returns the result
+        /// </summary>
+        /// <param name="context">The execution parameters</param>
         Task<ExecutionResult> ExecuteAsync(ExecutionContext context);
     }
 }
