6edcf492b2bb52a6e19752cc5f79458362ebf679
Shane Krueger <Shane@acdmail.com>
2020-02-16 10:04:22 -0500

    Add xml comments to IDocumentBuilder

--- src/GraphQL/Execution/IDocumentBuilder.cs
+++ src/GraphQL/Execution/IDocumentBuilder.cs
@@ -2,8 +2,14 @@ using GraphQL.Language.AST;
 
 namespace GraphQL.Execution
 {
+    /// <summary>
+    /// Creates a Document representing a GraphQL query from a GraphQL request string
+    /// </summary>
     public interface IDocumentBuilder
     {
+        /// <summary>
+        /// Parse a GraphQL request and return a Document representing the request
+        /// </summary>
         Document Build(string body);
     }
 }
