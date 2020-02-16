6b8c8e28912146331b2598f61fe61f34ca867b94
Shane Krueger <Shane@acdmail.com>
2020-02-16 18:02:12 -0500

    Update comment with see

--- src/GraphQL/Execution/IDocumentBuilder.cs
+++ src/GraphQL/Execution/IDocumentBuilder.cs
@@ -3,12 +3,12 @@ using GraphQL.Language.AST;
 namespace GraphQL.Execution
 {
     /// <summary>
-    /// Creates a Document representing a GraphQL query from a GraphQL request string
+    /// Creates a <see cref="Document">Document</see> representing a GraphQL query from a GraphQL request string
     /// </summary>
     public interface IDocumentBuilder
     {
         /// <summary>
-        /// Parse a GraphQL request and return a Document representing the request
+        /// Parse a GraphQL request and return a <see cref="Document">Document</see> representing the request
         /// </summary>
         Document Build(string body);
     }
