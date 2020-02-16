20918bc3b60affdd6f71087ed2575feafd52ca91
Shane Krueger <Shane@acdmail.com>
2020-02-16 09:21:16 -0500

    Update xml comments in IDocumentExecuter

--- src/GraphQL/Execution/IDocumentExecuter.cs
+++ src/GraphQL/Execution/IDocumentExecuter.cs
@@ -5,8 +5,16 @@ namespace GraphQL
 {
     public interface IDocumentExecuter
     {
+        /// <summary>
+        /// Executes a GraphQL request and returns the result
+        /// </summary>
+        /// <param name="options">The options of the execution</param>
         Task<ExecutionResult> ExecuteAsync(ExecutionOptions options);
 
+        /// <summary>
+        /// Executes a GraphQL request and returns the result
+        /// </summary>
+        /// <param name="configure">A delegate which configures the execution options</param>
         Task<ExecutionResult> ExecuteAsync(Action<ExecutionOptions> configure);
     }
 }
