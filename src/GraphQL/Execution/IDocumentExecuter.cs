7adaafe06aaa326790879b2b3261f811bef869dc
Shane Krueger <Shane@acdmail.com>
2020-02-16 15:30:04 -0500

    Comment update

--- src/GraphQL/Execution/IDocumentExecuter.cs
+++ src/GraphQL/Execution/IDocumentExecuter.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace GraphQL
 {
     /// <summary>
-    /// Processes a GraphQL request and returns the result
+    /// Processes an entire GraphQL request chain, including parsing the input string, running document listeners, handling exceptions and providing metrics (when enabled)
     /// </summary>
     public interface IDocumentExecuter
     {
