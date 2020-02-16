e4de6e98bb61c95aecef8b5f5ca14627651a065f
Shane Krueger <Shane@acdmail.com>
2020-02-16 16:04:20 -0500

    Xml comment update

--- src/GraphQL/Execution/IDocumentExecuter.cs
+++ src/GraphQL/Execution/IDocumentExecuter.cs
@@ -4,7 +4,12 @@ using System.Threading.Tasks;
 namespace GraphQL
 {
     /// <summary>
-    /// Processes an entire GraphQL request chain, including parsing the input string, running document listeners, handling exceptions and providing metrics (when enabled)
+    /// Processes an entire GraphQL request, given an input GraphQL request string.  This is intended to
+    /// be called by user code to process a query.<br/>
+    /// <br/>
+    /// Typical implementation starts metrics if enabled, relies on IDocumentBuilder to parse the query,
+    /// executes document listeners if attached, selects an execution strategy, and executes the query
+    /// via IExecutionStrategy.  Unhandled exceptions are handled as appropriate for the selected options.
     /// </summary>
     public interface IDocumentExecuter
     {
