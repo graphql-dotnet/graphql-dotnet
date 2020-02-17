c8141c2af07eef107d78c5c26333e3d237bef916
Shane Krueger <Shane@acdmail.com>
2020-02-17 16:59:14 -0500

    Update comment

--- src/GraphQL/Execution/ExecutionOptions.cs
+++ src/GraphQL/Execution/ExecutionOptions.cs
@@ -27,11 +27,7 @@ namespace GraphQL
         /// <summary>GraphQL query operation name; optional, defaults to first (if any) operation defined in query</summary>
         public string OperationName { get; set; }
 
-        /// <summary>
-        /// Parsed GraphQL request; will parse <see cref="Query"/> if not set.<br/>
-        /// <br/>
-        /// Can be used when implementing a cache of parsed GraphQL requests (a <see cref="Language.AST.Document"/>)
-        /// </summary>
+        /// <summary>Parsed GraphQL request; can be used to increase performance when implementing a cache of parsed GraphQL requests (a <see cref="Language.AST.Document"/>). If not set, it will be parsed from <see cref="Query"/></summary>
         public Document Document { get; set; }
 
         /// <summary>Input variables to GraphQL request</summary>
