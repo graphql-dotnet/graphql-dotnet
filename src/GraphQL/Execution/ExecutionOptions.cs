b928053ba1c087945436ea7dd492671ab8f4bfc3
Shane Krueger <Shane@acdmail.com>
2020-02-17 07:52:09 -0500

    Add xml comments to ExecutionOptions

--- src/GraphQL/Execution/ExecutionOptions.cs
+++ src/GraphQL/Execution/ExecutionOptions.cs
@@ -14,20 +14,36 @@ namespace GraphQL
 {
     public class ExecutionOptions : IProvideUserContext
     {
+        /// <summary>Schema of graph to use; required<br/><br/>Schema will be initialized if has not yet been initialized.</summary>
         public ISchema Schema { get; set; }
+
+        /// <summary>Object to pass to the <see cref="IResolveFieldContext.Source"/> property of first-level resolvers</summary>
         public object Root { get; set; }
+
+        /// <summary>GraphQL query to parse and execute; required</summary>
         public string Query { get; set; }
+
+        /// <summary>GraphQL query operation name; defaults to first operation defined in query</summary>
         public string OperationName { get; set; }
+
+        /// <summary>Parsed GraphQL request; will parse <see cref="Query"/> if not set</summary>
         public Document Document { get; set; }
+
+        /// <summary>Input variables to GraphQL request</summary>
         public Inputs Inputs { get; set; }
-        
+
+        /// <summary><see cref="System.Threading.CancellationToken">CancellationToken</see> of request; optional</summary>
         public CancellationToken CancellationToken { get; set; }
 
-        /// <summary>
-        /// Note if not set then standard list of validation rules will be used.
-        /// </summary>
+        /// <summary>Validation rules to be used by the <see cref="IDocumentValidator"/>; defaults to standard list of of validation rules - see <see cref="DocumentValidator.CoreRules"/></summary>
         public IEnumerable<IValidationRule> ValidationRules { get; set; }
 
+        /// <summary>
+        /// User-defined variables to be passed to the field resolvers.<br/>
+        /// <br/>
+        /// A custom implementation of <see cref="IDictionary{TKey, TValue}">IDictionary</see> may be
+        /// used in place of the default <see cref="Dictionary{TKey, TValue}">Dictionary</see>.
+        /// </summary>
         public IDictionary<string, object> UserContext { get; set; } = new Dictionary<string, object>();
 
         /// <summary>
@@ -37,35 +53,35 @@ namespace GraphQL
         /// </summary>
         public IFieldMiddlewareBuilder FieldMiddleware { get; set; } = new FieldMiddlewareBuilder();
 
+        /// <summary>Configuration options for <see cref="IComplexityAnalyzer"/> to use validating maximum query complexity</summary>
         public ComplexityConfiguration ComplexityConfiguration { get; set; }
 
+        /// <summary>A list of <see cref="IDocumentExecutionListener"/>s, enabling code to be executed at various points during the processing of the GraphQL query</summary>
         public List<IDocumentExecutionListener> Listeners { get; } = new List<IDocumentExecutionListener>();
 
+        /// <summary>Field and argument names are sanitized by the provided <see cref="IFieldNameConverter"/>; defaults to <see cref="CamelCaseFieldNameConverter"/></summary>
         public IFieldNameConverter FieldNameConverter { get; set; } = CamelCaseFieldNameConverter.Instance;
 
+        /// <summary>This property does nothing, but it is copied to the <see cref="ExecutionResult"/></summary>
         public bool ExposeExceptions { get; set; }
 
-        /// <summary>
-        /// This setting essentially allows Apollo Tracing. Disabling will increase performance.
-        /// </summary>
+        /// <summary>This setting essentially allows Apollo Tracing. Disabling will increase performance.</summary>
         public bool EnableMetrics { get; set; } = true;
 
+        /// <summary>When false, captures unhandled exceptions and returns them within <see cref="ExecutionResult.Errors">ExecutionResult.Errors</see></summary>
         public bool ThrowOnUnhandledException { get; set; }
 
         /// <summary>
-        /// Allows to override, hide, modify or just log the unhandled exception before wrap it into ExecutionError.
+        /// A delegate that can override, hide, modify, or log unhandled exceptions before they are stored
+        /// within <see cref="ExecutionResult.Errors"/> as an <see cref="ExecutionError"/>.
         /// This can be useful for hiding error messages that reveal server implementation details.
         /// </summary>
         public Action<UnhandledExceptionContext> UnhandledExceptionDelegate { get; set; } = context => { };
 
-        /// <summary>
-        /// If set, limits the maximum number of nodes executed in parallel
-        /// </summary>
+        /// <summary>If set, limits the maximum number of nodes executed in parallel</summary>
         public int? MaxParallelExecutionCount { get; set; }
 
-        /// <summary>
-        /// Provides the ability to filter the schema upon introspection to hide types.
-        /// </summary>
+        /// <summary>Provides the ability to filter the schema upon introspection to hide types; by default no types are hidden.</summary>
         public ISchemaFilter SchemaFilter { get; set; } = new DefaultSchemaFilter();
     }
 }
