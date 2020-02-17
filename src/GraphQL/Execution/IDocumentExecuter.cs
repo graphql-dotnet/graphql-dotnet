dfb366a65604d4626820a0e3be6d36e67c4fea15
Shane Krueger <Shane@acdmail.com>
2020-02-17 08:24:21 -0500

    Add more detail to IDocumentExecuter

--- src/GraphQL/Execution/IDocumentExecuter.cs
+++ src/GraphQL/Execution/IDocumentExecuter.cs
@@ -7,8 +7,11 @@ namespace GraphQL
     /// Processes an entire GraphQL request, given an input GraphQL request string. This is intended to
     /// be called by user code to process a query.<br/>
     /// <br/>
-    /// Typical implementation starts metrics if enabled, relies on <see cref="Execution.IDocumentBuilder">IDocumentBuilder</see>
-    /// to parse the query, executes document listeners if attached, selects an execution strategy, and executes the query
+    /// Typical implementation starts metrics if enabled (see <see cref="Instrumentation.Metrics">Metrics</see>),
+    /// relies on <see cref="Execution.IDocumentBuilder">IDocumentBuilder</see> to parse the query,
+    /// <see cref="Validation.IDocumentValidator">IDocumentValidator</see> to validate it, and
+    /// <see cref="Validation.Complexity.IComplexityAnalyzer">IComplexityAnalyzer</see> to validate the complexity constraints.
+    /// Then it executes document listeners if attached, selects an execution strategy, and executes the query
     /// via <see cref="Execution.IExecutionStrategy">IExecutionStrategy</see>. Unhandled exceptions are handled as appropriate for the selected options.
     /// </summary>
     public interface IDocumentExecuter
