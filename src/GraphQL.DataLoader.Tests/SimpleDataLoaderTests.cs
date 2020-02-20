7688070f12c885d6c6a95464633b2a029361f8bd
Shane32 <shane@acdmail.com>
2020-02-20 09:07:58 -0500

    Update SimpleDataLoaderTests.cs

--- src/GraphQL.DataLoader.Tests/SimpleDataLoaderTests.cs
+++ src/GraphQL.DataLoader.Tests/SimpleDataLoaderTests.cs
@@ -59,7 +59,7 @@ namespace GraphQL.DataLoader.Tests
             mock.Setup(store => store.GetAllUsersAsync(cts.Token))
                 .Returns(async (CancellationToken ct) =>
                 {
-                    await Task.Delay(5000, ct);
+                    await Task.Delay(60000, ct);
                     ct.ThrowIfCancellationRequested();
 
                     return users;
