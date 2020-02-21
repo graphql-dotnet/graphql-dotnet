6fb2bc057815a94ddc6abcc8c676c3e97c9d35a8
Shane32 <shane@acdmail.com>
2020-02-21 01:23:00 -0500

    Add xml comments to field resolvers (#1556)

--- src/GraphQL/Resolvers/FuncFieldResolver.cs
+++ src/GraphQL/Resolvers/FuncFieldResolver.cs
@@ -3,6 +3,9 @@ using GraphQL.Types;
 
 namespace GraphQL.Resolvers
 {
+    /// <summary>
+    /// When resolving a field, this implementation calls a predefined <see cref="Func{T, TResult}"/> and returns the result
+    /// </summary>
     public class FuncFieldResolver<TReturnType> : IFieldResolver<TReturnType>
     {
         private readonly Func<IResolveFieldContext, TReturnType> _resolver;
@@ -23,6 +26,11 @@ namespace GraphQL.Resolvers
         }
     }
 
+    /// <summary>
+    /// When resolving a field, this implementation calls a predefined <see cref="Func{T, TResult}"/> and returns the result.<br/>
+    /// <br/>
+    /// This implementation provides a typed <see cref="IResolveFieldContext{TSource}"/> to the resolver function.
+    /// </summary>
     public class FuncFieldResolver<TSourceType, TReturnType> : IFieldResolver<TReturnType>
     {
         private readonly Func<IResolveFieldContext<TSourceType>, TReturnType> _resolver;
