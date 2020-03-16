5910fbac57868bbfd4abb0c6c8dc16162fb380ee
SimonCropp <simon.cropp@gmail.com>
2020-03-17 10:15:10 +1100

    Revert "Update DateGraphTypeTests.cs"
    This reverts commit 4fce30923fae906a3b36b9bf9a55bf705c4e9deb.

--- src/GraphQL.Tests/Types/DateGraphTypeTests.cs
+++ src/GraphQL.Tests/Types/DateGraphTypeTests.cs
@@ -21,6 +21,46 @@ namespace GraphQL.Tests.Types
         }
 
         [Fact]
+        public void serialize_local_date_returns_date_only()
+        {
+            CultureTestHelper.UseCultures(() =>
+            {
+                var date = new DateTime(2000, 1, 2, 3, 4, 5, 6, DateTimeKind.Local);
+
+                var actual = _type.Serialize(date);
+
+                actual.ShouldBe("2000-01-02");
+            });
+        }
+
+        [Fact]
+        public void serialize_utc_date_returns_date_only()
+        {
+            CultureTestHelper.UseCultures(() =>
+            {
+                var date = new DateTime(2000, 1, 2, 3, 4, 5, 6, DateTimeKind.Utc);
+
+                var actual = _type.Serialize(date);
+
+                actual.ShouldBe("2000-01-02");
+            });
+        }
+
+        [Fact]
+        public void coerces_valid_date()
+        {
+            CultureTestHelper.UseCultures(() =>
+            {
+                var expected = DateTime.UtcNow;
+                var input = expected.ToLocalTime().ToString("O", DateTimeFormatInfo.InvariantInfo);
+
+                var actual = _type.ParseValue(input);
+
+                actual.ShouldBe(expected);
+            });
+        }
+
+        [Fact]
         public void coerces_datetimes_to_utc()
         {
             CultureTestHelper.UseCultures(() =>
@@ -53,8 +93,18 @@ namespace GraphQL.Tests.Types
         {
             CultureTestHelper.UseCultures(() =>
             {
-                _type.ParseValue("2015-12-01").ShouldBe(
-                    new DateTime(2015, 12, 01, 0, 0, 0));
+                _type.ParseValue("2015-12-01T14:15:07.123Z").ShouldBe(
+                    new DateTime(2015, 12, 01, 14, 15, 7) + TimeSpan.FromMilliseconds(123));
+            });
+        }
+
+        [Fact]
+        public void coerces_iso8601_string_with_tzone_to_date()
+        {
+            CultureTestHelper.UseCultures(() =>
+            {
+                _type.ParseValue("2015-11-21T19:59:32.987+0200").ShouldBe(
+                    new DateTime(2015, 11, 21, 17, 59, 32) + TimeSpan.FromMilliseconds(987));
             });
         }
     }
