842e3a440fa47d9573c5da3c2ad7eff91aaf2963
Shane Krueger <Shane@acdmail.com>
2020-03-15 23:18:18 -0400

    Update

--- src/GraphQL/Execution/ExecutionResult.cs
+++ src/GraphQL/Execution/ExecutionResult.cs
@@ -39,7 +39,7 @@ namespace GraphQL
         public PerfRecord[] Perf { get; set; }
 
         /// <summary>
-        /// Indicates that unhandled <see cref="Exception"/> stack traces should be serialized into GraphQL query result json along with exception messages; defaults to only <see cref="Exception.Message"/>
+        /// Indicates that unhandled <see cref="Exception"/> stack traces should be serialized into GraphQL query result json along with exception messages; otherwise only <see cref="Exception.Message"/> should be serialized
         /// </summary>
         public bool ExposeExceptions { get; set; }
 
