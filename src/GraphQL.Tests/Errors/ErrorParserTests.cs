b0a0b3eda8c997818efafab27c151883e4677e03
Shane Krueger <shane@acdmail.com>
2020-04-30 09:11:39 -0400

    Update src/GraphQL.Tests/Errors/ErrorParserTests.cs

--- src/GraphQL.Tests/Errors/ErrorParserTests.cs
+++ src/GraphQL.Tests/Errors/ErrorParserTests.cs
@@ -143,8 +143,7 @@ namespace GraphQL.Tests.Errors
             var parsed = new ErrorParser().Parse(error);
             parsed.Message.ShouldBe(error.Message);
             parsed.Locations.ShouldBeNull();
-            //parsed.Path.ShouldBe(new object[] { "root", 23, "child" });
-            parsed.Path.ShouldBe(new string[] { "root", "23", "child" });
+            parsed.Path.ShouldBe(new object[] { "root", 23, "child" });
             parsed.Extensions.ShouldBeNull();
         }
 
