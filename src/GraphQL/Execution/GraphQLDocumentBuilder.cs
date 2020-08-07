3acb08e55411cfdb26ce091d8a79f3c8b4223456
Shane Krueger <Shane@acdmail.com>
2020-08-07 13:02:44 -0400

    Add location information to parsing errors

--- src/GraphQL/Execution/GraphQLDocumentBuilder.cs
+++ src/GraphQL/Execution/GraphQLDocumentBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using GraphQL.Language;
 using GraphQL.Language.AST;
 using GraphQLParser;
@@ -17,7 +18,41 @@ namespace GraphQL.Execution
         public Document Build(string body)
         {
             var source = new Source(body);
-            var result = _parser.Parse(source);
+            GraphQLParser.AST.GraphQLDocument result;
+            try
+            {
+                result = _parser.Parse(source);
+            }
+            catch (GraphQLParser.Exceptions.GraphQLSyntaxErrorException ex)
+            {
+                ExecutionError rethrowException = null;
+                try
+                {
+                    //e.g. message = "Syntax Error GraphQL (1:1) Unexpected Name \"unknownoperation\"\n1: unknownoperation { firstAsync }\n   ^\n"
+                    var message = ex.Message.Substring(0, ex.Message.IndexOf('\n'));
+                    var paren = ex.Message.IndexOf('(');
+                    var colon = ex.Message.IndexOf(':', paren);
+                    var paren2 = ex.Message.IndexOf(')', colon);
+                    var line = ex.Message.Substring(paren + 1, colon - paren - 1);
+                    var column = ex.Message.Substring(colon + 1, paren2 - colon - 1);
+                    var messageDescription = message.Substring(paren2 + 1).Trim();
+
+                    //e.g. newMessageDescription = "Error parsing query: Unexpected Name \"unknownoperation\""
+                    var newMessageDescription = "Error parsing query: " + messageDescription;
+
+                    rethrowException = new ExecutionError(newMessageDescription, ex);
+                    rethrowException.AddLocation(int.Parse(line), int.Parse(column));
+                    //rethrowException.Code will default to "SYNTAX_ERROR"
+                }
+                catch
+                {
+                }
+
+                if (rethrowException != null)
+                    throw rethrowException;
+
+                throw;
+            }
 
             var document = CoreToVanillaConverter.Convert(body, result);
             document.OriginalQuery = body;
