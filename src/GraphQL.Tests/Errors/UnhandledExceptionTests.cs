bfc42f6c94b5e518f948db99e11b702de3004957
Shane Krueger <Shane@acdmail.com>
2020-08-07 11:36:43 -0400

    Add test

--- src/GraphQL.Tests/Errors/UnhandledExceptionTests.cs
+++ src/GraphQL.Tests/Errors/UnhandledExceptionTests.cs
@@ -29,11 +29,46 @@ namespace GraphQL.Tests.Errors
             });
         }
 
+        [Fact]
+        public void unhandled_error_delegate_can_rethrow_custom_exception()
+        {
+            var def = @"
+                type Query {
+                  hello2: Int
+                }
+            ";
+
+            Builder.Types.Include<Query>();
+
+            var expectedError = new ExecutionError("Test error message");
+            expectedError.AddLocation(1, 3);
+            expectedError.Path = new[] { "hello2" };
+            
+            AssertQuery(options =>
+            {
+                options.Schema = Builder.Build(def);
+                options.Query = "{ hello2 }";
+                options.UnhandledExceptionDelegate = ctx =>
+                {
+                    if (ctx.Exception is ApplicationException)
+                    {
+                        ctx.Exception = new ExecutionError("Test error message");
+                    }
+                };
+            }, new ExecutionResult { Errors = new ExecutionErrors { expectedError }, Data = new { hello2 = (object)null } });
+
+        }
+
         public class Query
         {
             public int Hello()
             {
                 throw new Exception("arrgh");
+            }
+
+            public int Hello2()
+            {
+                throw new ApplicationException("error!");
             }
         }
     }
