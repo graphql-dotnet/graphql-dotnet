b58dbe983af58d9c732c4df6b328c7502a57d189
Ivan Maximov <sungam3r@yandex.ru>
2020-08-14 02:55:02 +0300

    Update src/GraphQL/Language/AST/Document.cs

--- src/GraphQL/Language/AST/Document.cs
+++ src/GraphQL/Language/AST/Document.cs
@@ -36,7 +36,7 @@ namespace GraphQL.Language.AST
             }
             else
             {
-                throw new ArgumentOutOfRangeException(nameof(definition), "Unhandled document definition");
+                throw new ArgumentOutOfRangeException(nameof(definition), $"Unhandled document definition '{definition.GetType().Name}'");
             }
         }
 
