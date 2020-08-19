672460553eca4ec947a60dbb48289e7f11001775
Shane Krueger <shane@acdmail.com>
2020-08-18 23:55:03 -0400

    Update src/GraphQL/Execution/DocumentError.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/GraphQL/Execution/DocumentError.cs
+++ src/GraphQL/Execution/DocumentError.cs
@@ -5,6 +5,7 @@ namespace GraphQL.Execution
     public abstract class DocumentError : ExecutionError
     {
         public DocumentError(string message) : base(message) { }
+        
         public DocumentError(string message, Exception innerException) : base(message, innerException) { }
     }
 }
