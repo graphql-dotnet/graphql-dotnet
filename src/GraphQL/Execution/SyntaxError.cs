ce7e87f52b3d81cd364997db5c0cdd6a0a7f1db2
Shane Krueger <shane@acdmail.com>
2020-08-21 02:30:30 -0400

    Add xml comment

--- src/GraphQL/Execution/SyntaxError.cs
+++ src/GraphQL/Execution/SyntaxError.cs
@@ -3,9 +3,17 @@ using GraphQLParser.Exceptions;
 
 namespace GraphQL.Execution
 {
+    /// <summary>
+    /// Represents an error generated while parsing the document.
+    /// </summary>
     [Serializable]
     public class SyntaxError : DocumentError
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SyntaxError"/> class from a specified
+        /// <see cref="GraphQLSyntaxErrorException"/> instance, setting the <see cref="Exception.Message">Message</see>
+        /// and <see cref="ExecutionError.Locations">Locations</see> properties appropriately.
+        /// </summary>
         public SyntaxError(GraphQLSyntaxErrorException ex)
             : base("Error parsing query: " + ex.Description, ex)
         {
