8d87c4fc9676f604bd42b38a76779c651bfd3e2a
Shane Krueger <shane@acdmail.com>
2020-08-21 17:17:55 -0400

    Make ExecutionError.Codes and related virtual; add xml comments (#1800)

--- src/GraphQL/Execution/NoOperationError.cs
+++ src/GraphQL/Execution/NoOperationError.cs
@@ -2,9 +2,15 @@ using System;
 
 namespace GraphQL.Execution
 {
+    /// <summary>
+    /// Represents an error triggered when the document does not include any operations.
+    /// </summary>
     [Serializable]
     public class NoOperationError : DocumentError
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoOperationError"/> class.
+        /// </summary>
         public NoOperationError()
             : base("Document does not contain any operations.")
         {
