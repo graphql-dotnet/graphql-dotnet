70449f932c31557eeae2dbc43cc9b7d84996b242
Shane Krueger <shane@acdmail.com>
2020-08-21 18:51:32 -0400

    Update src/GraphQL.Tests/Utilities/SchemaBuilderTestBase.cs

--- src/GraphQL.Tests/Utilities/SchemaBuilderTestBase.cs
+++ src/GraphQL.Tests/Utilities/SchemaBuilderTestBase.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using GraphQL.Execution;
 using GraphQL.SystemTextJson;
 using GraphQL.Utilities;
 using GraphQLParser.Exceptions;
