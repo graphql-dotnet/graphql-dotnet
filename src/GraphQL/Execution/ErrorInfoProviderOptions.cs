021454a61f348a72c82de11c98f18f517f9549dd
Shane Krueger <shane@acdmail.com>
2020-08-23 11:53:59 -0400

    Draft additional ErrorInfoProviderOptions

--- src/GraphQL/Execution/ErrorInfoProviderOptions.cs
+++ src/GraphQL/Execution/ErrorInfoProviderOptions.cs
@@ -6,9 +6,23 @@ namespace GraphQL.Execution
     public class ErrorInfoProviderOptions
     {
         /// <summary>
-        /// Specifies whether stack traces should be serialized
+        /// Specifies whether stack traces should be serialized.
         /// </summary>
         public bool ExposeExceptionStackTrace { get; set; }
-        // public bool ExposeExtensions { get; set; }
+
+        /// <summary>
+        /// Specifies whether the extensions property, including the 'code', 'codes' and 'data' properties, should be serialized.
+        /// </summary>
+        public bool ExposeExtensions { get; set; }
+
+        /// <summary>
+        /// Specifies whether the codes of inner exceptions should be returned. Not applicable when <see cref="ExposeExtensions"/> is false.
+        /// </summary>
+        public bool ExposeInnerCodes { get; set; }
+
+        /// <summary>
+        /// Specifies whether data (typically from inner exceptions) should be returned. Not applicable when <see cref="ExposeExtensions"/> is false.
+        /// </summary>
+        public bool ExposeData { get; set; }
     }
 }
