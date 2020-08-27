6ae4464616000269cda788f64af3130839096c0f
Shane Krueger <shane@acdmail.com>
2020-08-27 09:15:54 -0400

    Update src/GraphQL.Tests/Validation/ScalarLeafTests.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/GraphQL.Tests/Validation/ScalarLeafTests.cs
+++ src/GraphQL.Tests/Validation/ScalarLeafTests.cs
@@ -160,7 +160,7 @@ namespace GraphQL.Tests.Validation
             });
         }
 
-        private string NoSubselectionAllowedMessage(string field, string type)
+        private static string NoSubselectionAllowedMessage(string field, string type)
             => ScalarLeafsError.NoSubselectionAllowedMessage(field, type);
 
         private string RequiredSubselectionMessage(string field, string type)
