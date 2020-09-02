34fa897cf4b8b6ba1528159756887c6c0419184d
Joe McBride <joe@uicraftsman.com>
2020-09-02 11:57:16 -0700

    Do not include revision

--- tools/tasks/settings.js
+++ tools/tasks/settings.js
@@ -7,7 +7,7 @@ const target = process.env.CONFIGURATION || 'Debug'
 const buildNumber = process.env.APPVEYOR_BUILD_NUMBER
 let version = pjson.version
 const revision = buildNumber || moment().format('HHmm')
-const includeRevision = true
+const includeRevision = false
 const assemblyVersion = includeRevision ? `${version}.${revision}` : `${version}.0`
 
 const appVeyorJobId = process.env.APPVEYOR_JOB_ID
