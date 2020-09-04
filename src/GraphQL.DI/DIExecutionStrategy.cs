2ea31e781e8f9490f7699ae74ae0cf45ae1f5f3d
Shane Krueger <Shane@acdmail.com>
2020-09-04 12:38:16 -0400

    Update DI Execution Strategy

--- src/GraphQL.DI/DIExecutionStrategy.cs
+++ src/GraphQL.DI/DIExecutionStrategy.cs
@@ -32,6 +32,37 @@ namespace GraphQL.DI
             int maxTasks = context.MaxParallelExecutionCount ?? int.MaxValue;
             if (maxTasks < 1) throw new InvalidOperationException("Invalid maximum number of tasks");
 
+            void DICompleteNode(ExecutionNode node)
+            {
+                //if the result of the node is an IDelayLoadedResult, then add this
+                //  node to a list of nodes to be loaded once everything else possible
+                //  has been loaded
+                if (node.Result is IDataLoaderResult)
+                {
+                    pendingNodes.Enqueue(node);
+                }
+                else
+                {
+                    // Push any child nodes on top of the stack
+                    if (node is IParentExecutionNode parentNode)
+                    {
+                        // Add in reverse order so fields are executed in the correct order (for synchronous tasks)
+                        foreach (var child in parentNode.GetChildNodes().Reverse())
+                        {
+                            //add node to async list or sync list, as appropriate
+                            if (child.FieldDefinition is DIFieldType fieldType && fieldType.Concurrent)
+                            {
+                                asyncNodes.Push(child);
+                            }
+                            else
+                            {
+                                nodes.Push(child);
+                            }
+                        }
+                    }
+                }
+            }
+
             // Process each node in the queue
             while (true)
             {
@@ -42,89 +73,69 @@ namespace GraphQL.DI
                     var node = asyncNodes.Pop();
                     //execute it (asynchronously)
                     var task = ExecuteNodeAsync(context, node);
-                    var taskWithNode = taskFunc(task, node);
-                    //add this task to the list of tasks waiting to be completed
-                    waitingTasks.Add(taskWithNode);
+                    if (task.IsCompleted)
+                    {
+                        await task;
+                        DICompleteNode(node);
+                    }
+                    else
+                    {
+                        var taskWithNode = taskFunc(task, node);
+                        //add this task to the list of tasks waiting to be completed
+                        waitingTasks.Add(taskWithNode);
+                    }
                 }
 
                 //start executing one synchronous task, if none is yet waiting to be completed
-                if (nodes.Count > 0 && waitingSyncTask == null && waitingTasks.Count < maxTasks)
+                while (nodes.Count > 0 && waitingSyncTask == null && waitingTasks.Count < maxTasks)
                 {
                     //grab a synchronous node to execute
                     var node = nodes.Pop();
                     //execute it (asynchronously)
                     var task = ExecuteNodeAsync(context, node);
-                    var taskWithNode = taskFunc(task, node);
-                    //notate the synchronous task that is currently executing
-                    waitingSyncTask = taskWithNode;
-                    //add this task to the list of tasks waiting to be completed
-                    waitingTasks.Add(taskWithNode);
-                }
-
-                //complete one or more tasks
-                if (waitingTasks.Count > 0)
-                {
-                    //check if there are any listeners
-                    IEnumerable<Task<ExecutionNode>> completedTasks;
-                    if (context.Listeners.Count() == 0)
+                    if (task.IsCompleted)
                     {
-                        //wait for at least one task to complete
-                        var completedTask = await Task.WhenAny(waitingTasks).ConfigureAwait(false);
-                        //note: errors are not thrown here, but rather down at task.Result
-                        completedTasks = new Task<ExecutionNode>[] { completedTask };
-                        waitingTasks.Remove(completedTask);
-                        if (waitingSyncTask == completedTask) waitingSyncTask = null;
+                        await task;
+                        DICompleteNode(node);
                     }
                     else
                     {
-                        //wait for listeners (this really makes no sense, and
-                        //  is a really poor way of implementing an IDataLoader)
-                        await OnBeforeExecutionStepAwaitedAsync(context)
-                            .ConfigureAwait(false);
-
-                        //execute all pending tasks
-                        await Task.WhenAll(waitingTasks).ConfigureAwait(false);
-
-                        completedTasks = waitingTasks;
-                        waitingTasks = new List<Task<ExecutionNode>>();
-                        waitingSyncTask = null;
+                        var taskWithNode = taskFunc(task, node);
+                        //notate the synchronous task that is currently executing
+                        waitingSyncTask = taskWithNode;
+                        //add this task to the list of tasks waiting to be completed
+                        waitingTasks.Add(taskWithNode);
                     }
+                }
+
+                //complete one or more asynchronously-executing tasks
+                if (waitingTasks.Count == 1)
+                {
+                    await OnBeforeExecutionStepAwaitedAsync(context)
+                        .ConfigureAwait(false);
+
+                    DICompleteNode(await waitingTasks[0]);
+                    waitingTasks.Clear();
+                    waitingSyncTask = null;
+                } 
+                else if (waitingTasks.Count > 0)
+                {
+                    //wait for listeners (this really makes no sense, and
+                    //  is a really poor way of implementing an IDataLoader)
+                    await OnBeforeExecutionStepAwaitedAsync(context)
+                        .ConfigureAwait(false);
+
+                    //wait for at least one task to complete
+                    var completedTask = await Task.WhenAny(waitingTasks).ConfigureAwait(false);
+                    //note: errors are not thrown here, but rather down at task.Result
+                    waitingTasks.Remove(completedTask);
+                    if (waitingSyncTask == completedTask) waitingSyncTask = null;
 
                     //if the request was canceled, quit out now
                     context.CancellationToken.ThrowIfCancellationRequested();
 
-                    //check each completed task
-                    foreach (var task in completedTasks)
-                    {
-                        var node = task.Result;
-                        //if the result of the node is an IDelayLoadedResult, then add this
-                        //  node to a list of nodes to be loaded once everything else possible
-                        //  has been loaded
-                        if (node.Result is IDataLoaderResult)
-                        {
-                            pendingNodes.Enqueue(node);
-                        }
-                        else
-                        {
-                            // Push any child nodes on top of the stack
-                            if (node is IParentExecutionNode parentNode)
-                            {
-                                // Add in reverse order so fields are executed in the correct order (for synchronous tasks)
-                                foreach (var child in parentNode.GetChildNodes().Reverse())
-                                {
-                                    //add node to async list or sync list, as appropriate
-                                    if (child.FieldDefinition is DIFieldType fieldType && fieldType.Concurrent)
-                                    {
-                                        asyncNodes.Push(child);
-                                    }
-                                    else
-                                    {
-                                        nodes.Push(child);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    //process the completed task
+                    DICompleteNode(await completedTask);
                 }
 
                 //if there's no sync/async nodes being processed or waiting to be processed,
@@ -139,9 +150,16 @@ namespace GraphQL.DI
                     while (pendingNodes.Count > 0)
                     {
                         var pendingNode = pendingNodes.Dequeue();
-                        var task = CompleteNodeAsync(context, pendingNode);
+                        var task = CompleteDataLoaderNodeAsync(context, pendingNode);
+                        if (!task.IsCompleted)
+                        {
+                            //wait for listeners (this really makes no sense, and
+                            //  is a really poor way of implementing an IDataLoader)
+                            await OnBeforeExecutionStepAwaitedAsync(context)
+                                .ConfigureAwait(false);
+                        }
                         await task.ConfigureAwait(false); //execute synchronously
-                        waitingTasks.Add(task);
+                        DICompleteNode(pendingNode);
                     }
                     //if there were no pending nodes, there would be no waitingTasks, and that means there's no more nodes left to execute
                     if (waitingTasks.Count == 0) return;
@@ -149,143 +167,5 @@ namespace GraphQL.DI
                 }
             }
         }
-
-
-        /// <summary>
-        /// Execute a single node
-        /// </summary>
-        /// <remarks>
-        /// Except for IDelayLoadedResult, builds child nodes, but does not execute them
-        /// </remarks>
-        protected override async Task ExecuteNodeAsync(ExecutionContext context, ExecutionNode node)
-        {
-            context.CancellationToken.ThrowIfCancellationRequested();
-
-            if (node.IsResultSet) return;
-
-            ReadonlyResolveFieldContext resolveContext = null;
-
-            try
-            {
-                resolveContext = new ReadonlyResolveFieldContext(node, context);
-
-                var resolver = node.FieldDefinition.Resolver ?? NameFieldResolver.Instance;
-                var result = resolver.Resolve(resolveContext);
-
-                if (result is Task task)
-                {
-                    await task.ConfigureAwait(false);
-                    result = task.GetResult();
-                }
-
-                node.Result = result;
-
-                if (!(result is IDataLoaderResult))
-                {
-                    ValidateNodeResult(context, node);
-
-                    // Build child nodes
-                    if (node.Result != null)
-                    {
-                        if (node is ObjectExecutionNode objectNode)
-                        {
-                            SetSubFieldNodes(context, objectNode);
-                        }
-                        else if (node is ArrayExecutionNode arrayNode)
-                        {
-                            SetArrayItemNodes(context, arrayNode);
-                        }
-                    }
-                }
-            }
-            catch (ExecutionError error)
-            {
-                error.AddLocation(node.Field, context.Document);
-                error.Path = node.Path;
-                context.Errors.Add(error);
-
-                node.Result = null;
-            }
-            catch (Exception ex)
-            {
-                if (context.ThrowOnUnhandledException)
-                    throw;
-
-                if (context.UnhandledExceptionDelegate != null)
-                {
-                    var exceptionContext = new UnhandledExceptionContext(context, resolveContext, ex);
-                    context.UnhandledExceptionDelegate(exceptionContext);
-                    ex = exceptionContext.Exception;
-                }
-
-                var error = new ExecutionError($"Error trying to resolve {node.Name}.", ex);
-                error.AddLocation(node.Field, context.Document);
-                error.Path = node.Path;
-                context.Errors.Add(error);
-
-                node.Result = null;
-            }
-        }
-
-        protected virtual async Task<ExecutionNode> CompleteNodeAsync(ExecutionContext context, ExecutionNode node)
-        {
-            context.CancellationToken.ThrowIfCancellationRequested();
-
-            if (!node.IsResultSet) throw new InvalidOperationException("This execution node has not yet been executed");
-            if (!(node.Result is IDataLoaderResult delayLoadedResult)) throw new InvalidOperationException("This execution node is not pending completion");
-
-            ResolveFieldContext resolveContext = null;
-
-            try
-            {
-
-                node.Result = await delayLoadedResult.GetResultAsync();
-
-                ValidateNodeResult(context, node);
-
-                // Build child nodes
-                if (node.Result != null)
-                {
-                    if (node is ObjectExecutionNode objectNode)
-                    {
-                        SetSubFieldNodes(context, objectNode);
-                    }
-                    else if (node is ArrayExecutionNode arrayNode)
-                    {
-                        SetArrayItemNodes(context, arrayNode);
-                    }
-                }
-            }
-            catch (ExecutionError error)
-            {
-                error.AddLocation(node.Field, context.Document);
-                error.Path = node.Path;
-                context.Errors.Add(error);
-
-                node.Result = null;
-            }
-            catch (Exception ex)
-            {
-                if (context.ThrowOnUnhandledException)
-                    throw;
-
-                if (context.UnhandledExceptionDelegate != null)
-                {
-                    var exceptionContext = new UnhandledExceptionContext(context, resolveContext, ex);
-                    context.UnhandledExceptionDelegate(exceptionContext);
-                    ex = exceptionContext.Exception;
-                }
-
-                var error = new ExecutionError($"Error trying to resolve {node.Name}.", ex);
-                error.AddLocation(node.Field, context.Document);
-                error.Path = node.Path;
-                context.Errors.Add(error);
-
-                node.Result = null;
-            }
-
-            return node;
-        }
-
     }
 }
