198172c2b879a76456815bfc170ddda380d8557d
Chris Grinton <cgrinton@gmail.com>
2020-09-29 02:06:06 +1000

    Add test to demonstrate bug with aliased fields in input types used in a mutation list field

---
+++ src/GraphQL.Tests/Bugs/BugAliasedInputFieldInList.cs
@@ -0,0 +1,70 @@
+using GraphQL.Types;
+using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests.Bugs
+{
+    public class BugAliasedInputFieldInList : QueryTestBase<AliasedInputFieldSchema>
+    {
+        [Fact]
+        public void supports_aliased_fields_in_input_list()
+        {
+			string query = @"mutation { mutateMyEntity(
+                singleEntity: { aField: 1 fieldAlias: 2 }
+                multipleEntities: [{ aField: 3 fieldAlias: 4 }]
+            ) }";
+
+            string expected = @"{ ""mutateMyEntity"": true }";
+
+            AssertQuerySuccess(query, expected, null);
+        }
+    }
+
+    public class AliasedInputFieldSchema : Schema
+    {
+        public AliasedInputFieldSchema() 
+        {
+            Mutation = new AliasedInputFieldMutation();
+        }
+    }
+
+    public class AliasedInputFieldMutation : ObjectGraphType
+    {
+        public AliasedInputFieldMutation()
+        {
+            Field<BooleanGraphType>()
+				.Name("mutateMyEntity")
+				.Argument<MyEntityInputType>("singleEntity")
+				.Argument<ListGraphType<MyEntityInputType>>("multipleEntities")
+				.Resolve(
+					context => {
+						var singleEntity = context.GetArgument<MyEntity>("singleEntity");
+						singleEntity.AField.ShouldBe(1); // <<<<<< This is OK
+						singleEntity.BField.ShouldBe(2); // <<<<<< This is OK
+
+						var multipleEntities = context.GetArgument<List<MyEntity>>("multipleEntities");
+						multipleEntities[0].AField.ShouldBe(3); // <<<<< This is OK
+						multipleEntities[0].BField.ShouldBe(4); // <<<<< This is fails
+
+						return true;
+					}
+				);
+        }
+    }
+
+    public class MyEntityInputType : InputObjectGraphType<MyEntity>
+    {
+        public MyEntityInputType()
+        { 
+            Field(x => x.AField);
+            Field("fieldAlias", x => x.BField);
+        }
+    }
+
+    public class MyEntity
+    {
+        public int AField { get; set; }
+        public int BField { get; set; }
+    }
+}
