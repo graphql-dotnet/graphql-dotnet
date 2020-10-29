36c4ba3a7f180deb6c71910ad493ee6ab11ecaf4
Ivan Maximov <sungam3r@yandex.ru>
2020-10-29 23:10:48 +0300

    oops

--- src/GraphQL.Subscriptions/SubscriptionDocumentExecuter.cs
+++ src/GraphQL.Subscriptions/SubscriptionDocumentExecuter.cs
@@ -18,11 +18,8 @@ namespace GraphQL
 
         protected override IExecutionStrategy SelectExecutionStrategy(ExecutionContext context)
         {
-            // TODO: Should we use cached instances of the default execution strategies?
             return context.Operation.OperationType switch
             {
-                OperationType.Query => new ParallelExecutionStrategy(),
-                OperationType.Mutation => new SerialExecutionStrategy(),
                 OperationType.Subscription => new SubscriptionExecutionStrategy(),
                 _ => base.SelectExecutionStrategy(context)
             };
