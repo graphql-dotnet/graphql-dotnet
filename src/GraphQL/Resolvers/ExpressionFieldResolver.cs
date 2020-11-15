cf2021208d47d89bcad185a68f718a063d386e99
Shane32 <shane@acdmail.com>
2020-11-15 13:37:33 -0500

    Additional xml comments

--- src/GraphQL/Resolvers/ExpressionFieldResolver.cs
+++ src/GraphQL/Resolvers/ExpressionFieldResolver.cs
@@ -4,17 +4,22 @@ using System.Linq.Expressions;
 namespace GraphQL.Resolvers
 {
     /// <summary>
-    /// Returns a value from the field's graph type's source object, based on a predefined expression
+    /// Returns a value from the field's graph type's source object, based on a predefined expression.
     /// </summary>
     public class ExpressionFieldResolver<TSourceType, TProperty> : IFieldResolver<TProperty>
     {
         private readonly Func<TSourceType, TProperty> _property;
 
+        /// <summary>
+        /// Initializes a new instance that runs the specified expression.
+        /// </summary>
+        /// <param name="property"></param>
         public ExpressionFieldResolver(Expression<Func<TSourceType, TProperty>> property)
         {
             _property = property.Compile();
         }
 
+        /// <inheritdoc/>
         public TProperty Resolve(IResolveFieldContext context) => _property((TSourceType)context.Source);
 
         object IFieldResolver.Resolve(IResolveFieldContext context) => Resolve(context);
--- src/GraphQL/Resolvers/FuncFieldResolver.cs
+++ src/GraphQL/Resolvers/FuncFieldResolver.cs
@@ -9,18 +9,22 @@ namespace GraphQL.Resolvers
     {
         private readonly Func<IResolveFieldContext, TReturnType> _resolver;
 
+        /// <summary>
+        /// Initializes a new instance that runs the specified delegate.
+        /// </summary>
         public FuncFieldResolver(Func<IResolveFieldContext, TReturnType> resolver)
         {
             _resolver = resolver;
         }
 
+        /// <inheritdoc/>
         public TReturnType Resolve(IResolveFieldContext context) => _resolver(context);
 
         object IFieldResolver.Resolve(IResolveFieldContext context) => Resolve(context);
     }
 
     /// <summary>
-    /// When resolving a field, this implementation calls a predefined <see cref="Func{T, TResult}"/> and returns the result.
+    /// <inheritdoc cref="FuncFieldResolver{TReturnType}"/>
     /// <br/><br/>
     /// This implementation provides a typed <see cref="IResolveFieldContext{TSource}"/> to the resolver function.
     /// </summary>
@@ -28,11 +32,13 @@ namespace GraphQL.Resolvers
     {
         private readonly Func<IResolveFieldContext<TSourceType>, TReturnType> _resolver;
 
+        /// <inheritdoc cref="FuncFieldResolver{TReturnType}.FuncFieldResolver(Func{IResolveFieldContext, TReturnType})"/>
         public FuncFieldResolver(Func<IResolveFieldContext<TSourceType>, TReturnType> resolver)
         {
             _resolver = resolver ?? throw new ArgumentNullException(nameof(resolver), "A resolver function must be specified");
         }
 
+        /// <inheritdoc/>
         public TReturnType Resolve(IResolveFieldContext context) => _resolver(context.As<TSourceType>());
 
         object IFieldResolver.Resolve(IResolveFieldContext context) => Resolve(context);
