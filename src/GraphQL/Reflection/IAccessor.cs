600235006b0e23aa9c802580d1cd9a6f15268ddf
Ivan Maximov <sungam3r@yandex.ru>
2020-11-15 14:20:43 +0300

    Retain stack trace and unwrap exception for SinglePropertyAccessor + add tests for #1155 (#2005)

--- src/GraphQL/Reflection/IAccessor.cs
+++ src/GraphQL/Reflection/IAccessor.cs
@@ -10,11 +10,23 @@ namespace GraphQL.Reflection
     public interface IAccessor
     {
         string FieldName { get; }
+
         Type ReturnType { get; }
+
         Type DeclaringType { get; }
+
         ParameterInfo[] Parameters { get; }
+
         MethodInfo MethodInfo { get; }
+
+        /// <summary>
+        /// Get return value of method or property.
+        /// </summary>
+        /// <param name="target">Target object.</param>
+        /// <param name="arguments">Arguments for method; not used for property.</param>
+        /// <returns>Return value.</returns>
         object GetValue(object target, object[] arguments);
+
         IEnumerable<T> GetAttributes<T>() where T : Attribute;
     }
 }
