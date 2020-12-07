0b1184c3f4aa326cad9f947f0169de38e2ad2503
Ivan Maximov <sungam3r@yandex.ru>
2020-12-07 19:30:43 +0300

    ISchema inherits IProvideMetadata (#2036)

--- src/GraphQL/Utilities/MetadataProvider.cs
+++ src/GraphQL/Utilities/MetadataProvider.cs
@@ -5,21 +5,30 @@ using GraphQL.Types;
 
 namespace GraphQL.Utilities
 {
+    /// <summary>
+    /// Default implementation of <see cref="IProvideMetadata"/>. This is the base class for numerous
+    /// descendants like <see cref="GraphType"/>, <see cref="FieldType"/>, <see cref="Schema"/> and others.
+    /// </summary>
     public class MetadataProvider : IProvideMetadata
     {
+        /// <inheritdoc />
         public IDictionary<string, object> Metadata { get; set; } = new ConcurrentDictionary<string, object>();
 
+        /// <inheritdoc />
         public TType GetMetadata<TType>(string key, TType defaultValue = default)
         {
-            return GetMetadata(key, () => defaultValue);
+            var local = Metadata;
+            return local != null && local.TryGetValue(key, out object item) ? (TType)item : defaultValue;
         }
 
+        /// <inheritdoc />
         public TType GetMetadata<TType>(string key, Func<TType> defaultValueFactory)
         {
             var local = Metadata;
-            return local != null && local.TryGetValue(key, out var item) ? (TType)item : defaultValueFactory();
+            return local != null && local.TryGetValue(key, out object item) ? (TType)item : defaultValueFactory();
         }
 
+        /// <inheritdoc />
         public bool HasMetadata(string key) => Metadata?.ContainsKey(key) ?? false;
     }
 }
