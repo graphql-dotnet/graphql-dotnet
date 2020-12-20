ad5b527bb3dff1e05d56d879ea3035fadd579e84
Ivan Maximov <sungam3r@yandex.ru>
2020-12-20 23:39:40 +0300

    wow

--- src/GraphQL.Tests/Bugs/Bug138DecimalPrecisionTests.cs
+++ src/GraphQL.Tests/Bugs/Bug138DecimalPrecisionTests.cs
@@ -6,7 +6,11 @@ namespace GraphQL.Tests.Bugs
 {
     public class Bug138DecimalPrecisionTests : QueryTestBase<DecimalSchema>
     {
+#if NETCOREAPP3_1
         [Fact]
+#else
+        [Fact(Skip = "24.149999999999999 with .NET Core < 3.1")]
+#endif
         public void double_to_decimal_does_not_lose_precision()
         {
             var query = @"
