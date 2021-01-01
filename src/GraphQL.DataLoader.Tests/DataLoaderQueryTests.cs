51e422386fe84c579fda888fe88ef55dffb5a1ca
Ivan Maximov <sungam3r@yandex.ru>
2021-01-02 02:20:32 +0300

    more fixes

--- src/GraphQL.DataLoader.Tests/DataLoaderQueryTests.cs
+++ src/GraphQL.DataLoader.Tests/DataLoaderQueryTests.cs
@@ -379,7 +379,7 @@ mutation {
         /// <see cref="Execution.ExecutionNode.GraphType"/> is returning proper values. Without a dataloader,
         /// <see cref="Execution.ExecutionStrategy.SetArrayItemNodes(Execution.ExecutionContext, Execution.ArrayExecutionNode)"/>
         /// skips execution of <see cref="Execution.ExecutionStrategy.ValidateNodeResult(Execution.ExecutionContext, Execution.ExecutionNode)"/>
-        /// because it is not relevant, and that method is the only one that calls <see cref="Execution.ExecutionNode.ResolvedType"/>.
+        /// because it is not relevant, and that method is the only one that calls Execution.ExecutionNode.ResolvedType.
         /// </summary>
         [Fact]
         public void ExerciseListsOfLists()
