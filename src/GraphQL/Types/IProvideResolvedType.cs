0dc9321efb752afea1537069b9cabb36660fabba
Shane Krueger <shane@acdmail.com>
2021-01-02 15:12:20 -0500

    Move IHaveDefaultValue.Type to IProvideResolvedType.Type

--- src/GraphQL/Types/IProvideResolvedType.cs
+++ src/GraphQL/Types/IProvideResolvedType.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace GraphQL.Types
 {
     public interface IProvideResolvedType
     {
         IGraphType ResolvedType { get; }
+
+        /// <summary>
+        /// Returns the graph type of this argument or field.
+        /// </summary>
+        Type Type { get; }
     }
 }
