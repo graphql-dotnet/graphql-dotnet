e3608e3445feb4052eec9d55ebfd860702b91ef0
Shane Krueger <shane@acdmail.com>
2021-01-03 01:55:19 -0500

    Add xml comments to ExecutionErrors

--- src/GraphQL/Execution/ExecutionErrors.cs
+++ src/GraphQL/Execution/ExecutionErrors.cs
@@ -4,25 +4,41 @@ using System.Collections.Generic;
 
 namespace GraphQL
 {
+    /// <summary>
+    /// Contains a list of execution errors.
+    /// </summary>
     public class ExecutionErrors : IEnumerable<ExecutionError>
     {
         private readonly List<ExecutionError> _errors = new List<ExecutionError>();
 
+        /// <summary>
+        /// Adds an execution error to the list.
+        /// </summary>
         public virtual void Add(ExecutionError error)
         {
             _errors.Add(error ?? throw new ArgumentNullException(nameof(error)));
         }
 
+        /// <summary>
+        /// Adds a list of execution errors to the list.
+        /// </summary>
         public virtual void AddRange(IEnumerable<ExecutionError> errors)
         {
             foreach (var error in errors)
                 Add(error);
         }
 
+        /// <summary>
+        /// Returns the number of execution errors in the list.
+        /// </summary>
         public int Count => _errors.Count;
 
+        /// <summary>
+        /// Returns the execution error at the specified index.
+        /// </summary>
         public ExecutionError this[int index] => _errors[index];
 
+        /// <inheritdoc/>
         public IEnumerator<ExecutionError> GetEnumerator() => _errors.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
