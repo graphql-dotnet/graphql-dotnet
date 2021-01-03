80d0f507ccfdecb9da4f944b6a5613515d046bc7
Shane Krueger <shane@acdmail.com>
2021-01-03 12:49:57 -0500

    Apply suggestions from code review

--- src/GraphQL/Introspection/ISchemaFilter.cs
+++ src/GraphQL/Introspection/ISchemaFilter.cs
@@ -4,7 +4,7 @@ using GraphQL.Types;
 namespace GraphQL.Introspection
 {
     /// <summary>
-    /// Represents an filter for information returned from introspection queries.
+    /// Provides the ability to filter the schema upon introspection to hide types, fields, arguments, enum values, and directives.
     /// Can be used to hide information, such as graph types, from clients that have an inadequate permission level.
     /// </summary>
     public interface ISchemaFilter
@@ -12,43 +12,53 @@ namespace GraphQL.Introspection
         /// <summary>
         /// Returns a boolean indicating whether the specified graph type should be returned within the introspection query.
         /// </summary>
+        /// <param name="type">The graph type to consider.</param>
         Task<bool> AllowType(IGraphType type);
         /// <summary>
         /// Returns a boolean indicating whether the specified field should be returned within the introspection query.
         /// </summary>
+        /// <param name="parent">The parent type to which the field belongs.</param>
+        /// <param name="field">The field to consider.</param>
         Task<bool> AllowField(IGraphType parent, IFieldType field);
         /// <summary>
         /// Returns a boolean indicating whether the specified argument should be returned within the introspection query.
         /// </summary>
+        /// <param name="field">The field to which the argument belongs.</param>
+        /// <param name="argument">The argument to consider.</param>
         Task<bool> AllowArgument(IFieldType field, QueryArgument argument);
         /// <summary>
         /// Returns a boolean indicating whether the specified enumeration value should be returned within the introspection query.
         /// </summary>
+        /// <param name="parent">The enumeration to which the enumeration value belongs.</param>
+        /// <param name="enumValue">The enumeration value to consider.</param>
         Task<bool> AllowEnumValue(EnumerationGraphType parent, EnumValueDefinition enumValue);
         /// <summary>
         /// Returns a boolean indicating whether the specified directive should be returned within the introspection query.
         /// </summary>
+        /// <param name="directive">The directive to consider.</param>
         Task<bool> AllowDirective(DirectiveGraphType directive);
     }
 
     /// <summary>
-    /// A schema filter which allows all requested information to introspection queries.
+    /// The default schema filter.  By default nothing is hidden.
     /// </summary>
     public class DefaultSchemaFilter : ISchemaFilter
     {
-        /// <inheritdoc/>
-        public virtual Task<bool> AllowType(IGraphType type) => Task.FromResult(true);
+        private static readonly Task<bool> _completed = Task.FromResult(true);
 
         /// <inheritdoc/>
-        public virtual Task<bool> AllowField(IGraphType parent, IFieldType field) => Task.FromResult(true);
+        public virtual Task<bool> AllowType(IGraphType type) => _completed;
 
         /// <inheritdoc/>
-        public virtual Task<bool> AllowArgument(IFieldType field, QueryArgument argument) => Task.FromResult(true);
+        public virtual Task<bool> AllowField(IGraphType parent, IFieldType field) => _completed;
 
         /// <inheritdoc/>
-        public virtual Task<bool> AllowEnumValue(EnumerationGraphType parent, EnumValueDefinition enumValue) => Task.FromResult(true);
+        public virtual Task<bool> AllowArgument(IFieldType field, QueryArgument argument) => _completed;
 
         /// <inheritdoc/>
-        public virtual Task<bool> AllowDirective(DirectiveGraphType directive) => Task.FromResult(true);
+        public virtual Task<bool> AllowEnumValue(EnumerationGraphType parent, EnumValueDefinition enumValue) => _completed;
+
+        /// <inheritdoc/>
+        public virtual Task<bool> AllowDirective(DirectiveGraphType directive) => _completed;
     }
 }
