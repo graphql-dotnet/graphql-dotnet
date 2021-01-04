54755f7ed937266552dd4d24a2aaffca5deaeae1
Shane Krueger <shane@acdmail.com>
2021-01-04 07:58:45 -0500

    Add xml comments to VariableReference (#2112)

--- src/GraphQL/Language/AST/VariableReference.cs
+++ src/GraphQL/Language/AST/VariableReference.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace GraphQL.Language.AST
 {
+    /// <summary>
+    /// Represents a value node that represents a reference to a variable within a document.
+    /// </summary>
     public class VariableReference : AbstractNode, IValue
     {
+        /// <summary>
+        /// Initializes a new instance with the specified <see cref="NameNode"/> containing the name of the variable being referenced.
+        /// </summary>
         public VariableReference(NameNode name)
         {
             Name = name.Name;
@@ -11,17 +17,29 @@ namespace GraphQL.Language.AST
         }
 
         object IValue.Value => Name;
+
+        /// <summary>
+        /// Returns the name of the variable being referenced.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// Returns a <see cref="NameNode"/> containing the name of the variable being referenced.
+        /// </summary>
         public NameNode NameNode { get; }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public override string ToString() => $"VariableReference{{name={Name}}}";
 
+        /// <summary>
+        /// Compares this instance to another instance by comparing the name of the variable that is referenced.
+        /// </summary>
         protected bool Equals(VariableReference other)
         {
             return string.Equals(Name, other.Name, StringComparison.InvariantCulture);
         }
 
+        /// <inheritdoc/>
         public override bool IsEqualTo(INode obj)
         {
             if (obj is null)
