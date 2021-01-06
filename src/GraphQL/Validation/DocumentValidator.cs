e1075baec858bea17f3ad6727aedb5a2106f1f27
Shane Krueger <shane@acdmail.com>
2021-01-06 02:09:17 -0500

    If no rules, short-circuit entire validation

--- src/GraphQL/Validation/DocumentValidator.cs
+++ src/GraphQL/Validation/DocumentValidator.cs
@@ -71,6 +71,14 @@ namespace GraphQL.Validation
                 schema.Initialize();
             }
 
+            if (rules == null)
+            {
+                rules = CoreRules;
+            }
+
+            if (!rules.Any())
+                return SuccessfullyValidatedResult.Instance;
+
             var context = new ValidationContext
             {
                 OriginalQuery = originalQuery,
@@ -80,11 +88,6 @@ namespace GraphQL.Validation
                 UserContext = userContext,
                 Inputs = inputs
             };
-
-            if (rules == null)
-            {
-                rules = CoreRules;
-            }
 
             var awaitedVisitors = rules.Select(x => x.ValidateAsync(context)).Where(x => x != null);
             var visitors = (await Task.WhenAll(awaitedVisitors)).ToList();
