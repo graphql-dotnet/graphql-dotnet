c02dc993c3b4f9444ad70bd8ca6190b647bfb204
Shane Krueger <shane@acdmail.com>
2021-01-06 01:58:41 -0500

    Add xml comment

--- src/GraphQL/Validation/NodeVisitors.cs
+++ src/GraphQL/Validation/NodeVisitors.cs
@@ -3,9 +3,17 @@ using GraphQL.Language.AST;
 
 namespace GraphQL.Validation
 {
+    /// <summary>
+    /// Represents a set of <see cref="INodeVisitor"/> instances that each runs upon entering or leaving a node.
+    /// Be aware that all <see cref="INodeVisitor"/> instances are called in the order suplied; not in reverse order upon leaving a node.
+    /// </summary>
     public sealed class NodeVisitors : INodeVisitor
     {
         private readonly INodeVisitor[] _nodeVisitors;
+
+        /// <summary>
+        /// Initializes a new instance with the specified <see cref="INodeVisitor"/>s.
+        /// </summary>
         public NodeVisitors(params INodeVisitor[] nodeVisitors)
         {
             _nodeVisitors = nodeVisitors ?? throw new ArgumentNullException(nameof(nodeVisitors));
