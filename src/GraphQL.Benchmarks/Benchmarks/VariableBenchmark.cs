48fe07c9d2e27beac750d3c6255b927412eb8cea
Shane Krueger <shane@acdmail.com>
2021-01-12 19:42:02 -0500

    Remove usings

--- src/GraphQL.Benchmarks/Benchmarks/VariableBenchmark.cs
+++ src/GraphQL.Benchmarks/Benchmarks/VariableBenchmark.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
-using GraphQL.StarWars;
-using GraphQL.StarWars.Types;
 using GraphQL.Types;
 using GraphQL.Validation;
 using Microsoft.Extensions.DependencyInjection;
