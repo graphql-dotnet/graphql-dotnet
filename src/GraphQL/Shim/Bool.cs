fb385fdd5c7f5d017eaf94ce8bb57d81744c7a23
Ivan Maximov <sungam3r@yandex.ru>
2021-01-21 19:56:34 +0300

    remove unused cases

--- src/GraphQL/Shim/Bool.cs
+++ src/GraphQL/Shim/Bool.cs
@@ -16,28 +16,22 @@ namespace GraphQL
 
         public static bool TryParse(ReadOnlySpan<char> value, out bool result)
         {
-            if (IsTrueStringIgnoreCase(value))
+            if (value.Length == 4 &&
+                value[0] == 't' &&
+                value[1] == 'r' &&
+                value[2] == 'u' &&
+                value[3] == 'e')
             {
                 result = true;
                 return true;
             }
 
-            if (IsFalseStringIgnoreCase(value))
-            {
-                result = false;
-                return true;
-            }
-
-            // Special case: Trim whitespace as well as null characters.
-            value = TrimWhiteSpaceAndNull(value);
-
-            if (IsTrueStringIgnoreCase(value))
-            {
-                result = true;
-                return true;
-            }
-
-            if (IsFalseStringIgnoreCase(value))
+            if (value.Length == 5 &&
+                value[0] == 'f' &&
+                value[1] == 'a' &&
+                value[2] == 'l' &&
+                value[3] == 's' &&
+                value[4] == 'e')
             {
                 result = false;
                 return true;
@@ -45,52 +39,6 @@ namespace GraphQL
 
             result = false;
             return false;
-        }
-
-        private static bool IsTrueStringIgnoreCase(ReadOnlySpan<char> value)
-        {
-            return (value.Length == 4 &&
-                    (value[0] == 't' || value[0] == 'T') &&
-                    (value[1] == 'r' || value[1] == 'R') &&
-                    (value[2] == 'u' || value[2] == 'U') &&
-                    (value[3] == 'e' || value[3] == 'E'));
-        }
-
-        private static bool IsFalseStringIgnoreCase(ReadOnlySpan<char> value)
-        {
-            return (value.Length == 5 &&
-                    (value[0] == 'f' || value[0] == 'F') &&
-                    (value[1] == 'a' || value[1] == 'A') &&
-                    (value[2] == 'l' || value[2] == 'L') &&
-                    (value[3] == 's' || value[3] == 'S') &&
-                    (value[4] == 'e' || value[4] == 'E'));
-        }
-
-        private static ReadOnlySpan<char> TrimWhiteSpaceAndNull(ReadOnlySpan<char> value)
-        {
-            const char nullChar = (char)0x0000;
-
-            int start = 0;
-            while (start < value.Length)
-            {
-                if (!char.IsWhiteSpace(value[start]) && value[start] != nullChar)
-                {
-                    break;
-                }
-                start++;
-            }
-
-            int end = value.Length - 1;
-            while (end >= start)
-            {
-                if (!char.IsWhiteSpace(value[end]) && value[end] != nullChar)
-                {
-                    break;
-                }
-                end--;
-            }
-
-            return value.Slice(start, end - start + 1);
         }
 #endif
     }
