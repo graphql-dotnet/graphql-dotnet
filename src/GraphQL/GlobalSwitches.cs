98d5c44825bf8073f66f2950ca3e8bc87bc23ce1
Shane Krueger <Shane@acdmail.com>
2021-02-04 09:20:49 -0500

    Update xml comments

--- src/GraphQL/GlobalSwitches.cs
+++ src/GraphQL/GlobalSwitches.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel;
+using GraphQL.Conversion;
 using GraphQL.Utilities;
 
 namespace GraphQL
@@ -36,18 +37,21 @@ namespace GraphQL
         /// <summary>
         /// Gets or sets current validation delegate. By default this delegate validates all names according
         /// to the GraphQL <see href="http://spec.graphql.org/June2018/#sec-Names">specification</see>.
-        /// <br/>
+        /// <br/><br/>
         /// Setting this delegate allows you to use names not conforming to the specification, for example
-        /// 'enum-member'. Only change it when absolutely necessary.
+        /// 'enum-member'. Only change it when absolutely necessary. This is typically only overridden
+        /// when implementing a custom <see cref="INameConverter"/> that fixes names, making them spec-compliant.
         /// </summary>
         public static Action<string, NameType> Validation = NameValidator.ValidateDefault;
 
         /// <summary>
         /// Gets or sets current validation delegate during schema initialization. By default this delegate
         /// validates all names according to the GraphQL <see href="http://spec.graphql.org/June2018/#sec-Names">specification</see>.
-        /// <br/>
+        /// <br/><br/>
         /// Setting this delegate allows you to use names not conforming to the specification, for example
-        /// 'enum-member'. Only change it when absolutely necessary.
+        /// 'enum-member'. Only change it when absolutely necessary. Keep in mind that the parser cannot
+        /// parse incoming queries with invalid characters in the names, so it is likely that the resulting
+        /// member will be unusable.
         /// </summary>
         public static Action<string, NameType> ValidationOnSchemaInitialize = NameValidator.ValidateDefault;
     }
