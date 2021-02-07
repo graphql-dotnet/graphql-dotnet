5234d37cb6116b62b11bc1ee7afd883e8124daa3
Shane Krueger <shane@acdmail.com>
2021-02-07 18:53:28 -0500

    Add MS-specific dependency injection extensions (#2261)

---
+++ src/GraphQL.MicrosoftDI.Tests/ScopedAsyncFieldResolverTests.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.MicrosoftDI.Tests
+{
+    public class ScopedAsyncFieldResolverTests : ScopedContextBase
+    {
+        [Fact]
+        public async Task TReturn_only()
+        {
+            var resolver = new ScopedAsyncFieldResolver<string>(context =>
+            {
+                context.RequestServices.ShouldBe(_scopedServiceProvider);
+                return Task.FromResult("success");
+            });
+            (await resolver.Resolve(_scopedContext)).ShouldBe("success");
+            VerifyScoped();
+        }
+
+        [Fact]
+        public async Task TSource_and_TReturn()
+        {
+            var resolver = new ScopedAsyncFieldResolver<string, int>(context =>
+            {
+                context.Source.ShouldBe("test");
+                context.RequestServices.ShouldBe(_scopedServiceProvider);
+                return Task.FromResult(2);
+            });
+            _scopedContext.Source = "test";
+            (await resolver.Resolve(_scopedContext)).ShouldBe(2);
+            VerifyScoped();
+        }
+
+        [Fact]
+        public void RequiresRequestServices_TReturn_only()
+        {
+            var resolver = new ScopedAsyncFieldResolver<int>(context => Task.FromResult(5));
+            Should.Throw<MissingRequestServicesException>(() => resolver.Resolve(new ResolveFieldContext()));
+        }
+
+        [Fact]
+        public void RequiresRequestServices_TSource_and_TReturn()
+        {
+            var resolver = new ScopedAsyncFieldResolver<string, int>(context => Task.FromResult(5));
+            Should.Throw<MissingRequestServicesException>(() => resolver.Resolve(new ResolveFieldContext()));
+        }
+    }
+}
---
+++ src/GraphQL.MicrosoftDI.Tests/ScopedFieldResolverTests.cs
@@ -0,0 +1,48 @@
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.MicrosoftDI.Tests
+{
+    public class ScopedFieldResolverTests : ScopedContextBase
+    {
+        [Fact]
+        public void TReturn_only()
+        {
+            var resolver = new ScopedFieldResolver<string>(context =>
+            {
+                context.RequestServices.ShouldBe(_scopedServiceProvider);
+                return "success";
+            });
+            resolver.Resolve(_scopedContext).ShouldBe("success");
+            VerifyScoped();
+        }
+
+        [Fact]
+        public void TSource_and_TReturn()
+        {
+            var resolver = new ScopedFieldResolver<string, int>(context =>
+            {
+                context.Source.ShouldBe("test");
+                context.RequestServices.ShouldBe(_scopedServiceProvider);
+                return 2;
+            });
+            _scopedContext.Source = "test";
+            resolver.Resolve(_scopedContext).ShouldBe(2);
+            VerifyScoped();
+        }
+
+        [Fact]
+        public void RequiresRequestServices_TReturn_only()
+        {
+            var resolver = new ScopedFieldResolver<int>(context => 5);
+            Should.Throw<MissingRequestServicesException>(() => resolver.Resolve(new ResolveFieldContext()));
+        }
+
+        [Fact]
+        public void RequiresRequestServices_TSource_and_TReturn()
+        {
+            var resolver = new ScopedFieldResolver<string, int>(context => 5);
+            Should.Throw<MissingRequestServicesException>(() => resolver.Resolve(new ResolveFieldContext()));
+        }
+    }
+}
