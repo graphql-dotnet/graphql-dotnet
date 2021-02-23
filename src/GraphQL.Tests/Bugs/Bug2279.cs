deb1c4649a14d7ca0ab34172998e094f9e215d36
Shane Krueger <shane@acdmail.com>
2021-02-23 13:20:58 -0500

    Test for duplicate type name for generic graph type - issue #2279 (#2309)

---
+++ src/GraphQL.Tests/Bugs/Bug2279.cs
@@ -0,0 +1,60 @@
+using System;
+using GraphQL.Types;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests.Bugs
+{
+    // https://github.com/graphql-dotnet/graphql-dotnet/issues/2279
+    public class Bug2279DuplicateType : QueryTestBase<Bug2279Schema>
+    {
+        [Fact]
+        public void NoDuplicateTypeNames()
+        {
+            var s = new Bug2279Schema();
+            var e = Should.Throw<InvalidOperationException>(() => s.Initialize());
+            var t1 = typeof(Bug2279GraphType<int>).FullName;
+            var t2 = typeof(Bug2279GraphType<string>).FullName;
+            //nl will be determined at compile time dictated by how git is configured to handle line endings, not what environment the code was ran in
+            var nl = @"
+";
+            e.Message.ShouldBe($"Unable to register GraphType '{t1}' with the name 'Bug2279GraphType_1';{nl}the name 'Bug2279GraphType_1' is already registered to '{t2}'.");
+        }
+    }
+
+    public class Bug2279Schema : Schema
+    {
+        public Bug2279Schema()
+        {
+            Query = new Bug2279Query();
+        }
+    }
+
+    public class Bug2279Query : ObjectGraphType
+    {
+        public Bug2279Query()
+        {
+            Field<Bug2279GraphType<string>>(
+                "string",
+                resolve: ctx => "hello");
+            Field<Bug2279GraphType<int>>(
+                "int",
+                resolve: ctx => 3);
+        }
+    }
+
+    public class Bug2279GraphType<T> : ObjectGraphType<T>
+    {
+        public Bug2279GraphType()
+        {
+            if (typeof(T) == typeof(int))
+            {
+                Field<IntGraphType, T>().Name("value").Resolve(x => x.Source);
+            }
+            else
+            {
+                Field<StringGraphType, T>().Name("value").Resolve(x => x.Source);
+            }
+        }
+    }
+}
