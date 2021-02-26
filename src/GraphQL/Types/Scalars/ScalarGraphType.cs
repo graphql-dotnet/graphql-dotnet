50e606fe7fc392ede0d677c976b7cf0eda424a0e
Shane Krueger <shane@acdmail.com>
2021-02-26 16:23:06 -0500

    Update src/GraphQL/Types/Scalars/ScalarGraphType.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/GraphQL/Types/Scalars/ScalarGraphType.cs
+++ src/GraphQL/Types/Scalars/ScalarGraphType.cs
@@ -114,7 +114,7 @@ namespace GraphQL.Types
         /// <summary>
         /// Converts a value to an AST representation. This is necessary for introspection queries
         /// to return the default values of this scalar type when used on input fields or field and directive arguments. This method may throw an exception
-        /// or return null for a failed conversion.
+        /// or return <see langword="null"/> for a failed conversion.
         /// </summary>
         public virtual IValue ToAst(object value)
         {
