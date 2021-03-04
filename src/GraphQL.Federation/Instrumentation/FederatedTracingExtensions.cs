cfd7901f5151442c9b034c93ccd930c40bfe6bbe
Nur Sheikh <nsheikh@wayfair.com>
2021-03-04 14:11:30 -0500

    code formatting

--- src/GraphQL.Federation/Instrumentation/FederatedTracingExtensions.cs
+++ src/GraphQL.Federation/Instrumentation/FederatedTracingExtensions.cs
@@ -14,10 +14,10 @@ namespace GraphQL.Federation.Instrumentation
         /// Adds Apollo federated tracing metrics to an <see cref="ExecutionResult"/> instance,
         /// stored within <see cref="ExecutionResult.Extensions"/>["tracing"].
         /// Requires that the GraphQL document was executed with metrics enabled;
-        /// see <see cref="ExecutionOptions.EnableMetrics"/>. With <see cref="FederatedInstrumentFieldsMiddleware"/>
+        /// see <see cref="ExecutionOptions.EnableMetrics"/>. With <see cref="FederatedInstrumentFieldMiddleware"/>
         /// installed, also includes metrics from field resolvers.
         /// </summary>
-        /// <param name="result">An <see cref="ExecutionResult"/> instance.</param>
+        /// <param name="executionResult">An <see cref="ExecutionResult"/> instance.</param>
         /// <param name="start">The UTC date and time that the GraphQL document began execution.</param>
         public static void EnrichWithFederatedTracing(this ExecutionResult executionResult, DateTime start)
         {
