6061f280efc181b520892b1ce90eac2de986e154
Ivan Maximov <sungam3r@yandex.ru>
2021-03-07 01:20:35 +0300

    invert condition

--- src/GraphQL/Validation/Errors/NoUndefinedVariablesError.cs
+++ src/GraphQL/Validation/Errors/NoUndefinedVariablesError.cs
@@ -18,8 +18,10 @@ namespace GraphQL.Validation.Errors
         }
 
         internal static string UndefinedVarMessage(string varName, string opName)
-            => !string.IsNullOrWhiteSpace(opName)
-                ? $"Variable '${varName}' is not defined by operation '{opName}'."
-                : $"Variable '${varName}' is not defined.";
+        {
+            return string.IsNullOrWhiteSpace(opName)
+                ? $"Variable '${varName}' is not defined."
+                : $"Variable '${varName}' is not defined by operation '{opName}'.";
+        }
     }
 }
