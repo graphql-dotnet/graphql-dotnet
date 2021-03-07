e6d6a6e5729dde93910806deb5fc2ce87d234cd4
Shane32 <shane@acdmail.com>
2021-03-07 15:51:46 -0500

    Add comments to IAbstractGraphType

--- src/GraphQL/Types/Composite/IAbstractGraphType.cs
+++ src/GraphQL/Types/Composite/IAbstractGraphType.cs
@@ -7,6 +7,10 @@ namespace GraphQL.Types
     /// </summary>
     public interface IAbstractGraphType : IGraphType
     {
+        /// <summary>
+        /// Gets or sets a delegate that can be used to determine the proper graph type for the specified object value. See
+        /// <see cref="AbstractGraphTypeExtensions.GetObjectType(IAbstractGraphType, object, ISchema)"/> for more details.
+        /// </summary>
         Func<object, IObjectGraphType> ResolveType { get; set; }
 
         /// <summary>
@@ -14,11 +18,20 @@ namespace GraphQL.Types
         /// </summary>
         PossibleTypes PossibleTypes { get; }
 
+        /// <summary>
+        /// Adds the specified graph type to a list of possible graph types for this abstract graph type
+        /// </summary>
         void AddPossibleType(IObjectGraphType type);
     }
 
+    /// <summary>
+    /// Provides extension methods for <see cref="IAbstractGraphType"/> instances.
+    /// </summary>
     public static class AbstractGraphTypeExtensions
     {
+        /// <summary>
+        /// Returns true if the specified graph type is one of the possible graph types for this abstract graph type.
+        /// </summary>
         public static bool IsPossibleType(this IAbstractGraphType abstractType, IGraphType type)
         {
             foreach (var possible in abstractType.PossibleTypes.List)
@@ -30,6 +43,12 @@ namespace GraphQL.Types
             return false;
         }
 
+        /// <summary>
+        /// Returns the proper graph type for the specified object for this abstract graph type. If the abstract
+        /// graph type implements <see cref="IAbstractGraphType.ResolveType"/>, then this method is called to determine
+        /// the best graph type to use. Otherwise, <see cref="IObjectGraphType.IsTypeOf"/> is called on each possible
+        /// graph type supported by the abstract graph type to determine if a match can be found.
+        /// </summary>
         public static IObjectGraphType GetObjectType(this IAbstractGraphType abstractType, object value, ISchema schema)
         {
             var result = abstractType.ResolveType != null
