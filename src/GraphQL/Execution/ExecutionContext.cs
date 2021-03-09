cbff3635bd95a855beb7d3dd83e1a6c9c5c2ea01
Shane32 <shane@acdmail.com>
2021-03-09 18:35:42 -0500

    Fix xml comment

--- src/GraphQL/Execution/ExecutionContext.cs
+++ src/GraphQL/Execution/ExecutionContext.cs
@@ -131,7 +131,7 @@ namespace GraphQL.Execution
         internal ReadonlyResolveFieldContext ReusableReadonlyResolveFieldContext;
 
         /// <summary>
-        /// Allows for an execution strategy to reuse an instance of <see cref="Fields"/>.
+        /// Allows for an execution strategy to reuse an instance of <see cref="Dictionary{TKey, TValue}"/>.
         /// This field may be accessed by multiple threads at the same time, so
         /// access is restricted to <see cref="System.Threading.Interlocked.Exchange{T}(ref T, T)"/>
         /// and <see cref="System.Threading.Interlocked.CompareExchange{T}(ref T, T, T)"/>.
