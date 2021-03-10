6fa56e68bdb7be02e64eee128d94f38e8b8a7af6
Shane32 <shane@acdmail.com>
2021-03-10 00:59:57 -0500

    fix formatting for astprinter value nodes

--- src/GraphQL.Tests/Utilities/AstPrinterTests.cs
+++ src/GraphQL.Tests/Utilities/AstPrinterTests.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Globalization;
 using GraphQL.Execution;
 using GraphQL.Language.AST;
 using GraphQL.Utilities;
+using GraphQL.Utilities.Federation;
 using Shouldly;
 using Xunit;
 
@@ -78,7 +80,7 @@ namespace GraphQL.Tests.Utilities
             int value = 3;
             var val = new IntValue(value);
             var result = _printer.Visit(val);
-            result.ShouldBe(value);
+            result.ShouldBe("3");
         }
 
         [Fact]
@@ -87,7 +89,7 @@ namespace GraphQL.Tests.Utilities
             long value = 3;
             var val = new LongValue(value);
             var result = _printer.Visit(val);
-            result.ShouldBe(value);
+            result.ShouldBe("3");
         }
 
         [Fact]
@@ -112,5 +114,69 @@ namespace GraphQL.Tests.Utilities
                 .Replace("\r\n", "\n")
                 .Replace("\r", "\n");
         }
+
+        [Fact]
+        public void anynode_throws()
+        {
+            Should.Throw<InvalidOperationException>(() => AstPrinter.Print(new AnyValue("")));
+        }
+
+        [Fact]
+        public void invalid_floatvalue_throws()
+        {
+            Should.Throw<InvalidOperationException>(() => AstPrinter.Print(new FloatValue(double.PositiveInfinity)));
+            Should.Throw<InvalidOperationException>(() => AstPrinter.Print(new FloatValue(double.NegativeInfinity)));
+            Should.Throw<InvalidOperationException>(() => AstPrinter.Print(new FloatValue(double.NaN)));
+        }
+
+        [Fact]
+        public void null_stringvalue_throws()
+        {
+            Should.Throw<InvalidOperationException>(() => AstPrinter.Print(new StringValue(null)));
+        }
+
+        [Theory]
+        [MemberData(nameof(NodeTests))]
+        public void prints_node(INode node, string expected)
+        {
+            AstPrinter.Print(node).ShouldBe(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(NodeTests))]
+        public void prints_node_cultures(INode node, string expected)
+        {
+            CultureTestHelper.UseCultures(() => prints_node(node, expected));
+        }
+
+        public static object[][] NodeTests = new object[][]
+        {
+            new object[] { new StringValue("test"), "\"test\"" },
+            new object[] { new StringValue("ab\"cd"), "\"ab\\\"cd\"" },
+            new object[] { new StringValue("ab\u0019cd"), "\"ab\\u0019cd\"" },
+            new object[] { new StringValue("\"abcd\""), "\"\\\"abcd\\\"\"" },
+            new object[] { new IntValue(int.MinValue), "-2147483648" },
+            new object[] { new IntValue(0), "0" },
+            new object[] { new IntValue(int.MaxValue), "2147483647" },
+            new object[] { new LongValue(long.MinValue), "-9223372036854775808" },
+            new object[] { new LongValue(0), "0" },
+            new object[] { new LongValue(long.MaxValue), "9223372036854775807" },
+            new object[] { new BigIntValue(new System.Numerics.BigInteger(decimal.MaxValue) * 1000 + 2), "79228162514264337593543950335002" },
+            new object[] { new FloatValue(double.MinValue), "-1.79769313486232E+308" },
+            new object[] { new FloatValue(double.Epsilon), "4.94065645841247E-324" },
+            new object[] { new FloatValue(double.MaxValue), "1.79769313486232E+308" },
+            new object[] { new FloatValue(0.00000001256), "1.256E-08" },
+            new object[] { new FloatValue(12.56), "12.56" },
+            new object[] { new FloatValue(3.33), "3.33" },
+            new object[] { new FloatValue(1.0), "1" },
+            new object[] { new FloatValue(34), "34" },
+            new object[] { new DecimalValue(1.0000m), "1"},
+            new object[] { new DecimalValue(decimal.MinValue), "-79228162514264337593543950335"},
+            new object[] { new DecimalValue(decimal.MaxValue), "79228162514264337593543950335"},
+            new object[] { new DecimalValue(0.00000000000000001m), "1E-17"},
+            new object[] { new BooleanValue(false), "false"},
+            new object[] { new BooleanValue(true), "true"},
+            new object[] { new EnumValue("TEST"), "TEST"},
+        };
     }
 }
