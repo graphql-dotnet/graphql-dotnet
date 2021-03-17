8825c3f1c95204940c9c3a6104257ba8734c27c7
Ivan Maximov <sungam3r@yandex.ru>
2021-03-17 12:50:13 +0300

    add tests

--- src/GraphQL.Tests/Language/ValueNodeTests.cs
+++ src/GraphQL.Tests/Language/ValueNodeTests.cs
@@ -14,9 +14,35 @@ namespace GraphQL.Tests.Language
         }
 
         [Fact]
-        public void stringvalue_cannot_be_null()
+        public void stringvalue_cannot_contain_null()
         {
             Should.Throw<ArgumentNullException>(() => new StringValue(null));
+        }
+
+        [Fact]
+        public void namenode_cannot_contain_null()
+        {
+            Should.Throw<ArgumentNullException>(() => new NameNode(null));
+        }
+
+        [Fact]
+        public void namenode_can_contain_empty_location()
+        {
+            new NameNode("a").SourceLocation.ShouldBe(default(SourceLocation));
+            new NameNode("a", default(SourceLocation)).SourceLocation.ShouldBe(default(SourceLocation));
+        }
+
+        [Fact]
+        public void enumvalue_cannot_contain_null()
+        {
+            Should.Throw<ArgumentNullException>(() => new EnumValue(null));
+        }
+
+        [Fact]
+        public void enumvalue_cannot_contain_invalid_characters()
+        {
+            Should.Throw<ArgumentOutOfRangeException>(() => new EnumValue("kebab-enum"));
+            Should.Throw<ArgumentOutOfRangeException>(() => new EnumValue(new NameNode("kebab-enum")));
         }
     }
 }
