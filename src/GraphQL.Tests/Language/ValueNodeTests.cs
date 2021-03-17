3bf932488cbace622c31f546e66ed53d0813d9cc
Shane32 <shane@acdmail.com>
2021-03-17 01:18:10 -0400

    Update

--- src/GraphQL.Tests/Language/ValueNodeTests.cs
+++ src/GraphQL.Tests/Language/ValueNodeTests.cs
@@ -11,25 +11,25 @@ namespace GraphQL.Tests.Language
     public class ValueNodeTests
     {
         [Fact]
-        public void double_cannot_contain_nan()
+        public void floatvalue_cannot_contain_nan()
         {
             Should.Throw<ArgumentOutOfRangeException>(() => new FloatValue(double.NaN));
         }
 
         [Fact]
-        public void double_cannot_contain_infinity()
+        public void floatvalue_cannot_contain_infinity()
         {
             Should.Throw<ArgumentOutOfRangeException>(() => new FloatValue(double.PositiveInfinity));
         }
 
         [Fact]
-        public void double_cannot_contain_negativeinfinity()
+        public void floatvalue_cannot_contain_negativeinfinity()
         {
             Should.Throw<ArgumentOutOfRangeException>(() => new FloatValue(double.NegativeInfinity));
         }
 
         [Fact]
-        public void string_cannot_be_null()
+        public void stringvalue_cannot_be_null()
         {
             Should.Throw<ArgumentNullException>(() => new StringValue(null));
         }
