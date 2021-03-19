315b83c4c549241c5c4977173facc2211e996827
Shane Krueger <shane@acdmail.com>
2021-03-19 18:05:02 -0400

    Fix ToConstantCase to match 3.x semantics (#2424)

--- src/GraphQL/StringExtensions.cs
+++ src/GraphQL/StringExtensions.cs
@@ -68,9 +68,8 @@ namespace GraphQL
         /// <summary>
         /// Returns a constant case version of this string. For example, converts 'StringError' into 'STRING_ERROR'.
         /// </summary>
-        public static string ToConstantCase(this string value)
+        public static string ToConstantCase(this string value) //TODO: rewrite to stackalloc/ string.Create()
         {
-            //aka: return Regex.Replace(s, @"([A-Z])([A-Z][a-z])|([a-z0-9])([A-Z])", "$1$3_$2$4").ToUpperInvariant();
             int i;
             int strLength = value.Length;
             // iterate through each character in the string, stopping a character short of the end
@@ -78,25 +77,41 @@ namespace GraphQL
             {
                 var curChar = value[i];
                 var nextChar = value[i + 1];
-                // look for the pattern [a-z0-9][A-Z]
-                if ((char.IsLower(curChar) || char.IsDigit(curChar)) && char.IsUpper(nextChar))
+                // look for the pattern [a-z][A-Z]
+                if (char.IsLower(curChar) && char.IsUpper(nextChar))
                 {
-                    // add an underscore between the two characters, increment i to skip the underscore, and increase strLength because the string is longer now
-                    value = value.Substring(0, ++i) + '_' + value.Substring(i);
-                    ++strLength;
-                    // skip the following match check since we already found a match here
+                    InsertUnderscore();
+                    // then skip the remaining match checks since we already found a match here
+                    continue;
+                }
+                // look for the pattern [0-9][A-Za-z]
+                if (char.IsDigit(curChar) && char.IsLetter(nextChar))
+                {
+                    InsertUnderscore();
+                    continue;
+                }
+                // look for the pattern [A-Za-z][0-9]
+                if (char.IsLetter(curChar) && char.IsDigit(nextChar))
+                {
+                    InsertUnderscore();
                     continue;
                 }
                 // if there's enough characters left, look for the pattern [A-Z][A-Z][a-z]
                 if (i < strLength - 2 && char.IsUpper(curChar) && char.IsUpper(nextChar) && char.IsLower(value[i + 2]))
                 {
-                    // add an underscore between the two characters, increment i to skip the underscore, and increase strLength because the string is longer now
-                    value = value.Substring(0, ++i) + '_' + value.Substring(i);
-                    ++strLength;
+                    InsertUnderscore();
+                    continue;
                 }
             }
             // convert the resulting string to uppercase
             return value.ToUpperInvariant();
+
+            void InsertUnderscore()
+            {
+                // add an underscore between the two characters, increment i to skip the underscore, and increase strLength because the string is longer now
+                value = value.Substring(0, ++i) + '_' + value.Substring(i);
+                ++strLength;
+            }
         }
 
         private static readonly char[] _bangs = new char[] { '!', '[', ']' };
