8ba231e65139323d47aa017241976d63c54e40d8
Ivan Maximov <sungam3r@yandex.ru>
2021-04-19 20:12:13 +0300

    Adds support for default values in ctor arguments of TType when doing context.GetArgument<TType> (#2512)

---
+++ src/GraphQL.Tests/IsExternalInit.cs
@@ -0,0 +1,17 @@
+#if !NET5
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class IsExternalInit
+    {
+    }
+}
+
+#endif
