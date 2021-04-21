435f0cf2bbc0c83ba8073bd086887e696a05c007
Shane Krueger <shane@acdmail.com>
2021-04-21 04:00:20 -0400

    Make IsExternalInit internal (#2513)

--- src/GraphQL.Tests/IsExternalInit.cs
+++ src/GraphQL.Tests/IsExternalInit.cs
@@ -9,7 +9,7 @@ namespace System.Runtime.CompilerServices
     /// This class should not be used by developers in source code.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public static class IsExternalInit
+    internal static class IsExternalInit
     {
     }
 }
