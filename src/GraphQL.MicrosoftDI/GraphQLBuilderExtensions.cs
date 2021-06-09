de159f6d20ac6ca7f48bf66b8f2ab3d5c0a18b38
Shane32 <shane@acdmail.com>
2021-06-08 23:39:48 -0400

    Update configuration

--- src/GraphQL.MicrosoftDI/GraphQLBuilderExtensions.cs
+++ src/GraphQL.MicrosoftDI/GraphQLBuilderExtensions.cs
@@ -1,13 +1,16 @@
 using System;
+using GraphQL.Execution;
 using GraphQL.Types;
+using GraphQL.Validation.Complexity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace GraphQL.MicrosoftDI
 {
     public static class GraphQLBuilderExtensions
     {
         public static IGraphQLBuilder AddGraphQL(this IServiceCollection services)
-            => new GraphQLBuilder(services);
+            => new GraphQLBuilder(services); // call AddOptions here? probably the job of the caller.
 
         public static IGraphQLBuilder AddSelfActivatingSchema<TSchema>(this IGraphQLBuilder builder, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TSchema : class, ISchema
@@ -37,6 +40,18 @@ namespace GraphQL.MicrosoftDI
                 return schema;
             });
 
+            return builder;
+        }
+
+        public static IGraphQLBuilder ConfigureErrorInfoProvider(this IGraphQLBuilder builder, Action<ErrorInfoProviderOptions, IServiceProvider> configureOptions)
+        {
+            builder.Register<IConfigureOptions<ErrorInfoProviderOptions>>(ServiceLifetime.Singleton, x => new ConfigureNamedOptions<ErrorInfoProviderOptions>(Options.DefaultName, opt => configureOptions(opt, x)));
+            return builder;
+        }
+
+        public static IGraphQLBuilder ConfigureComplexity(this IGraphQLBuilder builder, Action<ComplexityConfiguration, IServiceProvider> configureOptions)
+        {
+            builder.Register<IConfigureOptions<ComplexityConfiguration>>(ServiceLifetime.Singleton, x => new ConfigureNamedOptions<ComplexityConfiguration>(Options.DefaultName, opt => configureOptions(opt, x)));
             return builder;
         }
     }
