f302d6391f7d7646e99903ffe6df70a01e24d177
Shane32 <shane@acdmail.com>
2021-06-21 12:50:02 -0400

    Add more xml comments

--- src/GraphQL.MemoryCache/GraphQLBuilderExtensions.cs
+++ src/GraphQL.MemoryCache/GraphQLBuilderExtensions.cs
@@ -3,11 +3,17 @@ using GraphQL.DI;
 
 namespace GraphQL.Caching
 {
+    /// <inheritdoc cref="GraphQL.GraphQLBuilderExtensions"/>
     public static class GraphQLBuilderExtensions
     {
+        /// <summary>
+        /// Registers <see cref="MemoryDocumentCache"/> as a singleton of type <see cref="IDocumentCache"/> within the
+        /// dependency injection framework, and configures it with the specified configuration delegate.
+        /// </summary>
         public static IGraphQLBuilder AddMemoryCache(this IGraphQLBuilder builder, Action<MemoryDocumentCacheOptions> action = null)
             => builder.AddDocumentCache<MemoryDocumentCache>().Configure(action);
 
+        /// <inheritdoc cref="AddMemoryCache(IGraphQLBuilder, Action{MemoryDocumentCacheOptions})"/>
         public static IGraphQLBuilder AddMemoryCache(this IGraphQLBuilder builder, Action<MemoryDocumentCacheOptions, IServiceProvider> action)
             => builder.AddDocumentCache<MemoryDocumentCache>().Configure(action);
     }
--- src/GraphQL.NewtonsoftJson/GraphQLBuilderExtensions.cs
+++ src/GraphQL.NewtonsoftJson/GraphQLBuilderExtensions.cs
@@ -4,20 +4,18 @@ using Newtonsoft.Json;
 
 namespace GraphQL.NewtonsoftJson
 {
+    /// <inheritdoc cref="GraphQL.GraphQLBuilderExtensions"/>
     public static class GraphQLBuilderExtensions
     {
+        /// <summary>
+        /// Registers Newtonsoft.Json <see cref="DocumentWriter"/> as a singleton of type <see cref="IDocumentWriter"/> within the
+        /// dependency injection framework and configures it with the specified configuration delegate.
+        /// </summary>
         public static IGraphQLBuilder AddNewtonsoftJson(this IGraphQLBuilder builder, Action<JsonSerializerSettings> action = null)
-        {
-            builder.AddDocumentWriter<DocumentWriter>();
-            builder.Configure(action);
-            return builder;
-        }
+            => builder.AddDocumentWriter<DocumentWriter>().Configure(action);
 
+        /// <inheritdoc cref="AddNewtonsoftJson(IGraphQLBuilder, Action{JsonSerializerSettings})"/>
         public static IGraphQLBuilder AddNewtonsoftJson(this IGraphQLBuilder builder, Action<JsonSerializerSettings, IServiceProvider> action)
-        {
-            builder.AddDocumentWriter<DocumentWriter>();
-            builder.Configure(action);
-            return builder;
-        }
+            => builder.AddDocumentWriter<DocumentWriter>().Configure(action);
     }
 }
