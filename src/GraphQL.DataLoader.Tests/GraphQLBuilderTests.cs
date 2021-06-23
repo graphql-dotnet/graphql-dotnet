d021cb5883fa0481eaaff473f70991de64e42db9
Shane Krueger <shane@acdmail.com>
2021-06-22 22:39:54 -0400

    Add test for AddDataLoader

---
+++ src/GraphQL.DataLoader.Tests/GraphQLBuilderTests.cs
@@ -0,0 +1,40 @@
+using System;
+using GraphQL.DI;
+using GraphQL.Execution;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.DataLoader.Tests
+{
+    public class GraphQLBuilderTests
+    {
+        [Fact]
+        public void AddDataLoader()
+        {
+            var instance = new DataLoaderDocumentListener(new DataLoaderContextAccessor());
+            var mockBuilder = new Mock<IGraphQLBuilder>(MockBehavior.Strict);
+            var builder = mockBuilder.Object;
+            mockBuilder.Setup(x => x.Register(typeof(IDataLoaderContextAccessor), typeof(DataLoaderContextAccessor), ServiceLifetime.Singleton)).Returns(builder).Verifiable();
+            mockBuilder.Setup(x => x.Register(typeof(IDocumentExecutionListener), typeof(DataLoaderDocumentListener), ServiceLifetime.Singleton)).Returns(builder).Verifiable();
+            mockBuilder.Setup(x => x.Register(typeof(DataLoaderDocumentListener), typeof(DataLoaderDocumentListener), ServiceLifetime.Singleton)).Returns(builder).Verifiable();
+            var mockServiceProvider = new Mock<IServiceProvider>(MockBehavior.Strict);
+            mockServiceProvider.Setup(x => x.GetService(typeof(DataLoaderDocumentListener))).Returns(instance).Verifiable();
+            mockBuilder.Setup(x => x.Register(ServiceLifetime.Singleton, It.IsAny<Func<IServiceProvider, Action<ExecutionOptions>>>()))
+                .Returns<ServiceLifetime, Func<IServiceProvider, Action<ExecutionOptions>>>((serviceLifetime, func) =>
+                {
+                    var action = func(mockServiceProvider.Object);
+                    var options = new ExecutionOptions()
+                    {
+                        RequestServices = mockServiceProvider.Object
+                    };
+                    action(options);
+                    options.Listeners.ShouldContain(instance);
+                    return builder;
+                }).Verifiable();
+            builder.AddDataLoader();
+            mockServiceProvider.Verify();
+            mockBuilder.Verify();
+        }
+    }
+}
