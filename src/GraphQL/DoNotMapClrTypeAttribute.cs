d96e67eff1df203a6f32f2d3690dc6bdfc178374
Shane32 <shane@acdmail.com>
2021-07-02 11:02:11 -0400

    Fix spelling error

--- src/GraphQL/DoNotMapClrTypeAttribute.cs
+++ src/GraphQL/DoNotMapClrTypeAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace GraphQL
 {
     /// <summary>
-    /// Indiates that <see cref="SchemaExtensions.RegisterTypeMappings(Types.ISchema)"/> should
+    /// Indicates that <see cref="SchemaExtensions.RegisterTypeMappings(Types.ISchema)"/> should
     /// skip this class when scanning an assembly for CLR type mappings.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
