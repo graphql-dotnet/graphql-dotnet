ac6e719e5307b1ba81189261b2374cd2fc6cfdc9
Shane32 <shane@acdmail.com>
2021-07-09 17:29:17 -0400

    Revert change to DeprecationReason

--- src/GraphQL/Types/Fields/FieldType.cs
+++ src/GraphQL/Types/Fields/FieldType.cs
@@ -36,7 +36,7 @@ namespace GraphQL.Types
         public string Description { get; set; }
 
         /// <inheritdoc/>
-        public string DeprecationReason
+        public string? DeprecationReason
         {
             get => this.GetDeprecationReason();
             set => this.SetDeprecationReason(value);
