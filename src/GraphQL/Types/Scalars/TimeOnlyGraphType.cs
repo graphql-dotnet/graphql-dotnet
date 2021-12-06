a222ad7b8536dfb0dcc9c6d84302984dd241cce2
Simon Cropp <simon.cropp@gmail.com>
2021-12-07 08:33:19 +1100

    Update src/GraphQL/Types/Scalars/TimeOnlyGraphType.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/GraphQL/Types/Scalars/TimeOnlyGraphType.cs
+++ src/GraphQL/Types/Scalars/TimeOnlyGraphType.cs
@@ -48,9 +48,9 @@ namespace GraphQL.Types
 
         private static TimeOnly ParseTime(string stringValue)
         {
-            if (TimeOnly.TryParseExact(stringValue, "HH:mm:ss.FFFFFFF", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None, out var date))
+            if (TimeOnly.TryParseExact(stringValue, "HH:mm:ss.FFFFFFF", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None, out var time))
             {
-                return date;
+                return time;
             }
 
             throw new FormatException($"Could not parse time. Expected HH:mm:ss.FFFFFFF. Value: {stringValue}");
