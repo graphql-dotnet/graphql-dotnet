e55fe8265ddbe51e67b389128c89133f4306e806
Ivan Maximov <sungam3r@yandex.ru>
2021-12-06 23:58:30 +0300

    Add multi-TFM support for API approval tests

--- src/GraphQL.ApiTests/ApiApprovalTests.cs
+++ src/GraphQL.ApiTests/ApiApprovalTests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using GraphQL.Types;
 using PublicApiGenerator;
 using Shouldly;
@@ -19,18 +22,30 @@ namespace GraphQL.ApiTests
         [InlineData(typeof(DataLoader.DataLoaderContext))]
         public void PublicApi(Type type)
         {
-            string publicApi = type.Assembly.GeneratePublicApi(new ApiGeneratorOptions
-            {
-                IncludeAssemblyAttributes = false,
-                //WhitelistedNamespacePrefixes = new[] { "Microsoft.Extensions.DependencyInjection" },
-                ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" }
-            }) + Environment.NewLine;
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string asmName = type.Assembly.GetName().Name;
+            string tfmsDir = Path.Combine(baseDir, "..\\..\\..\\..", asmName, "bin", "Debug");
+            Debug.Assert(Directory.Exists(tfmsDir));
+            string[] tfms = Directory.GetDirectories(tfmsDir);
+            Debug.Assert(tfms.Length > 0);
 
-            // See: https://shouldly.readthedocs.io/en/latest/assertions/shouldMatchApproved.html
-            // Note: If the AssemblyName.approved.txt file doesn't match the latest publicApi value,
-            // this call will try to launch a diff tool to help you out but that can fail on
-            // your machine if a diff tool isn't configured/setup.
-            publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((testMethodInfo, discriminator, fileType, fileExtension) => $"{type.Assembly.GetName().Name}.{fileType}.{fileExtension}"));
+            foreach (string tfmDir in tfms)
+            {
+                var asm = Assembly.LoadFile(Path.Combine(tfmDir, asmName + ".dll"));
+                string publicApi = asm.GeneratePublicApi(new ApiGeneratorOptions
+                {
+                    IncludeAssemblyAttributes = false,
+                    //WhitelistedNamespacePrefixes = new[] { "Microsoft.Extensions.DependencyInjection" },
+                    ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" }
+                }) + Environment.NewLine;
+
+                // See: https://shouldly.readthedocs.io/en/latest/assertions/shouldMatchApproved.html
+                // Note: If the AssemblyName.approved.txt file doesn't match the latest publicApi value,
+                // this call will try to launch a diff tool to help you out but that can fail on
+                // your machine if a diff tool isn't configured/setup.
+                string tfm = new DirectoryInfo(tfmDir).Name.Replace(".", "");
+                publicApi.ShouldMatchApproved(options => options.SubFolder(tfm).WithFilenameGenerator((testMethodInfo, discriminator, fileType, fileExtension) => $"{type.Assembly.GetName().Name}.{fileType}.{fileExtension}"));
+            }
         }
     }
 }
