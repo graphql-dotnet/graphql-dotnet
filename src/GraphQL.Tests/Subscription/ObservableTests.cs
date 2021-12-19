27d7237b2afedb53ca5f9d8269377591113e8ee9
Shane32 <shane@acdmail.com>
2021-12-18 19:10:50 -0500

    Update tests to work in Azure

--- src/GraphQL.Tests/Subscription/ObservableTests.cs
+++ src/GraphQL.Tests/Subscription/ObservableTests.cs
@@ -33,9 +33,43 @@ namespace GraphQL.Tests.Subscription
             executed.ShouldBeTrue();
         }
 
+        private class EnforcedOrderWaiter : IDisposable
+        {
+            private readonly List<SemaphoreSlim> _semaphores = new();
+
+            public EnforcedOrderWaiter(int count)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    _semaphores.Add(new SemaphoreSlim(0, 1));
+                }
+                _semaphores[0].Release();
+            }
+
+            public Task WaitForSequence(int value)
+                => WaitForSequence(value, () => { });
+
+            public async Task WaitForSequence(int value, Action beforeNext)
+            {
+                await Task.Yield();
+                if (await _semaphores[value - 1].WaitAsync(15000) == false)
+                    throw new Exception($"Timeout while waiting for sequence {value}");
+                beforeNext();
+                if (value < _semaphores.Count)
+                    _semaphores[value].Release();
+            }
+
+            public void Dispose()
+            {
+                foreach (var semaphore in _semaphores)
+                    semaphore.Dispose();
+            }
+        }
+
         [Fact]
         public async Task SelectAsync_DataArrivesInOrder()
         {
+            using var order = new EnforcedOrderWaiter(3);
             IObserver<string> observer = null;
             bool disposed = false;
             var disposableMock = new Mock<IDisposable>(MockBehavior.Strict);
@@ -49,46 +83,59 @@ namespace GraphQL.Tests.Subscription
             var finishedEvents = new List<string>();
             var mappedObservable = observableMock.Object.SelectAsync(async (value, token) =>
             {
-                await Task.Delay(int.Parse(value));
-                lock (finishedEvents)
-                    finishedEvents.Add(value);
+                await Task.Delay(200);
+                await order.WaitForSequence(int.Parse(value), () => finishedEvents.Add(value));
                 return value;
             });
             var events = new List<string>();
             var baseObserverMock = new Mock<IObserver<string>>(MockBehavior.Strict);
-            baseObserverMock.Setup(x => x.OnNext(It.IsAny<string>())).Callback<string>(value => events.Add($"OnNext: {value}"));
-            baseObserverMock.Setup(x => x.OnError(It.IsAny<Exception>())).Callback<Exception>(value => events.Add($"OnError: {value.GetType().Name}"));
-            baseObserverMock.Setup(x => x.OnCompleted()).Callback(() => events.Add("OnCompleted"));
+            baseObserverMock.Setup(x => x.OnNext(It.IsAny<string>())).Callback<string>(value =>
+            {
+                lock (events)
+                    events.Add($"OnNext: {value}");
+            });
+            baseObserverMock.Setup(x => x.OnError(It.IsAny<Exception>())).Callback<Exception>(value =>
+            {
+                lock (events)
+                    events.Add($"OnError: {value.GetType().Name}");
+            });
+            baseObserverMock.Setup(x => x.OnCompleted()).Callback(() =>
+            {
+                lock (events)
+                    events.Add("OnCompleted");
+            });
             var mappedDisposable = mappedObservable.Subscribe(baseObserverMock.Object);
             observer.ShouldNotBeNull();
             disposed.ShouldBeFalse();
             //these next events are all triggered immediately, but may cause a delay during SelectAsync
-            observer.OnNext("1000"); // 1-second delay
+            observer.OnNext("2"); // 1-second delay
             observer.OnError(new InvalidTimeZoneException()); //error is thrown
-            observer.OnNext("500"); // 0.5-second delay
+            observer.OnNext("1"); // 0.5-second delay
             observer.OnCompleted(); // completion
-            await Task.Delay(2000);
+            await order.WaitForSequence(3);
+            await WaitForEvents(events, 4);
             mappedDisposable.Dispose();
             disposed.ShouldBeTrue();
             //verify events were raised in order
             events.ShouldBe(new string[]
             {
-                "OnNext: 1000",
+                "OnNext: 2",
                 "OnError: InvalidTimeZoneException",
-                "OnNext: 500",
+                "OnNext: 1",
                 "OnCompleted",
             });
             //verify that async operations were completed out of order
             finishedEvents.ShouldBe(new string[]
             {
-                "500",
-                "1000",
+                "1",
+                "2",
             });
         }
 
         [Fact]
         public async Task SelectAsync_MultipleSequences()
         {
+            using var order = new EnforcedOrderWaiter(5);
             IObserver<string> observer = null;
             bool disposed = false;
             var disposableMock = new Mock<IDisposable>(MockBehavior.Strict);
@@ -102,51 +149,77 @@ namespace GraphQL.Tests.Subscription
             var finishedEvents = new List<string>();
             var mappedObservable = observableMock.Object.SelectAsync(async (value, token) =>
             {
-                await Task.Delay(int.Parse(value));
-                lock (finishedEvents)
-                    finishedEvents.Add(value);
+                await order.WaitForSequence(int.Parse(value), () => finishedEvents.Add(value));
                 return value;
             });
             var events = new List<string>();
             var baseObserverMock = new Mock<IObserver<string>>(MockBehavior.Strict);
-            baseObserverMock.Setup(x => x.OnNext(It.IsAny<string>())).Callback<string>(value => events.Add($"OnNext: {value}"));
-            baseObserverMock.Setup(x => x.OnError(It.IsAny<Exception>())).Callback<Exception>(value => events.Add($"OnError: {value.GetType().Name}"));
-            baseObserverMock.Setup(x => x.OnCompleted()).Callback(() => events.Add("OnCompleted"));
+            baseObserverMock.Setup(x => x.OnNext(It.IsAny<string>())).Callback<string>(value =>
+            {
+                lock (events)
+                    events.Add($"OnNext: {value}");
+            });
+            baseObserverMock.Setup(x => x.OnError(It.IsAny<Exception>())).Callback<Exception>(value =>
+            {
+                lock (events)
+                    events.Add($"OnError: {value.GetType().Name}");
+            });
+            baseObserverMock.Setup(x => x.OnCompleted()).Callback(() =>
+            {
+                lock (events)
+                    events.Add("OnCompleted");
+            });
             var mappedDisposable = mappedObservable.Subscribe(baseObserverMock.Object);
             observer.ShouldNotBeNull();
             disposed.ShouldBeFalse();
-            observer.OnNext("500");
-            observer.OnNext("50");
-            await Task.Delay(1000);
-            observer.OnNext("500");
-            observer.OnNext("50");
-            await Task.Delay(1000);
+            observer.OnNext("2");
+            observer.OnNext("1");
+            await WaitForEvents(events, 2);
+            observer.OnNext("4");
+            observer.OnNext("3");
+            await WaitForEvents(events, 4);
             observer.OnCompleted();
-            await Task.Delay(500);
+            await WaitForEvents(events, 5);
             mappedDisposable.Dispose();
             disposed.ShouldBeTrue();
-            //verify events were raised in order
+            //verify events were raised in order queued
             events.ShouldBe(new string[]
             {
-                "OnNext: 500",
-                "OnNext: 50",
-                "OnNext: 500",
-                "OnNext: 50",
+                "OnNext: 2",
+                "OnNext: 1",
+                "OnNext: 4",
+                "OnNext: 3",
                 "OnCompleted",
             });
-            //verify that async operations were completed out of order
+            //verify that async operations were completed out of the order they were queued
             finishedEvents.ShouldBe(new string[]
             {
-                "50",
-                "500",
-                "50",
-                "500",
+                "1",
+                "2",
+                "3",
+                "4",
             });
+        }
+
+        private async Task WaitForEvents(List<string> events, int count)
+        {
+            var expire = DateTimeOffset.UtcNow.AddSeconds(15);
+            while (DateTimeOffset.UtcNow < expire)
+            {
+                await Task.Delay(200);
+                lock (events)
+                {
+                    if (events.Count == count)
+                        return;
+                }
+            }
+            throw new Exception($"Timeout waiting for events count == {count}");
         }
 
         [Fact]
         public async Task SelectAsync_NoEventsProducedAfterDispose()
         {
+            using var order = new EnforcedOrderWaiter(6);
             //also verifies that cancellation token passed to SelectAsync delegate works properly
             IObserver<string> observer = null;
             bool disposed = false;
@@ -161,7 +234,7 @@ namespace GraphQL.Tests.Subscription
             var canceledEvents = new List<string>();
             var mappedObservable = observableMock.Object.SelectAsync(async (value, token) =>
             {
-                await Task.Delay(int.Parse(value));
+                await order.WaitForSequence(int.Parse(value));
                 if (token.IsCancellationRequested)
                 {
                     lock (canceledEvents)
@@ -177,22 +250,26 @@ namespace GraphQL.Tests.Subscription
             var mappedDisposable = mappedObservable.Subscribe(baseObserverMock.Object);
             observer.ShouldNotBeNull();
             disposed.ShouldBeFalse();
-            observer.OnNext("50");
-            observer.OnNext("1500");
+            observer.OnNext("1");
+            observer.OnNext("5");
             observer.OnError(new InvalidTimeZoneException());
-            observer.OnNext("60");
+            observer.OnNext("2");
             observer.OnCompleted();
-            await Task.Delay(500);
+            await order.WaitForSequence(3);
+            await WaitForEvents(events, 1);
+            await Task.Delay(1000);
             mappedDisposable.Dispose();
+            await order.WaitForSequence(4);
             disposed.ShouldBeTrue();
-            await Task.Delay(2500);
+            await order.WaitForSequence(6);
+            await WaitForEvents(canceledEvents, 1);
             events.ShouldBe(new string[]
             {
-                "OnNext: 50",
+                "OnNext: 1",
             });
             canceledEvents.ShouldBe(new string[]
             {
-                "1500",
+                "5",
             });
         }
 
