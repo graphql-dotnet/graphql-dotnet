19ca437068f995368e63d729cba225e1b3a8f0ee
Shane32 <shane@acdmail.com>
2021-12-23 13:31:16 -0500

    Update

--- src/GraphQL.MicrosoftDI/GraphQLBuilderExtensions.cs
+++ src/GraphQL.MicrosoftDI/GraphQLBuilderExtensions.cs
@@ -13,7 +13,8 @@ namespace GraphQL.MicrosoftDI
     public static class GraphQLBuilderExtensions
     {
         /// <summary>
-        /// Returns a new <see cref="IGraphQLBuilder"/> for the specified service collection and
+        /// Configures a GraphQL pipeline using the configuration delegate passed into
+        /// <paramref name="configure"/> for the specified service collection and
         /// registers a default set of services required by GraphQL if they have not already been registered.
         /// <br/><br/>
         /// Does not include <see cref="IDocumentWriter"/>, and the default <see cref="IDocumentExecuter"/>
