790f05ec6a1b5783ac092653485098255a860f07
Shane Krueger <Shane@acdmail.com>
2022-01-06 23:17:57 -0500

    Add comments

--- src/GraphQL.Tests/NRTTests.cs
+++ src/GraphQL.Tests/NRTTests.cs
@@ -18,20 +18,25 @@ namespace GraphQL.Tests
             var context = new NullabilityInfoContext();
             var info = context.Create(returnParameter);
 
+            //test 1
             info.Type.ShouldBe(typeof(Tuple<Tuple<string, string>, string>));
             info.ReadState.ShouldBe(NullabilityState.NotNull);
             info.GenericTypeArguments.Length.ShouldBe(2);
 
+            //test 2
             info.GenericTypeArguments[0].Type.ShouldBe(typeof(Tuple<string, string>));
             info.GenericTypeArguments[0].ReadState.ShouldBe(NullabilityState.NotNull);
             info.GenericTypeArguments[0].GenericTypeArguments.Length.ShouldBe(2);
 
+            //test 3
             info.GenericTypeArguments[0].GenericTypeArguments[0].Type.ShouldBe(typeof(string));
             info.GenericTypeArguments[0].GenericTypeArguments[0].ReadState.ShouldBe(NullabilityState.Nullable);
 
+            //test 4
             info.GenericTypeArguments[0].GenericTypeArguments[1].Type.ShouldBe(typeof(string));
             info.GenericTypeArguments[0].GenericTypeArguments[1].ReadState.ShouldBe(NullabilityState.Nullable);
 
+            //test 5
             info.GenericTypeArguments[1].Type.ShouldBe(typeof(string));
             info.GenericTypeArguments[1].ReadState.ShouldBe(NullabilityState.NotNull); //incorrectly reports nullable
         }
@@ -40,6 +45,23 @@ namespace GraphQL.Tests
         {
             //check ordering of nested types
             public static Tuple<Tuple<string?, string?>, string> Field2() => null!;
+            /*             1      2      3        4         5
+             *
+             * 1: Tuple<Tuple<string, string>, string>
+             *    non-null
+             *
+             * 2: Tuple<string, string>
+             *    non-null
+             *
+             * 3: string
+             *    nullable
+             *
+             * 4: string
+             *    nullable
+             *
+             * 5: string
+             *    non-null
+             */
         }
     }
 }
