0e36b887b66695e8e97441f5f0731f6f8d4bd4a1
Shane Krueger <Shane@acdmail.com>
2022-01-22 12:16:11 -0500

    Add test

---
+++ src/GraphQL.Tests/Types/AutoRegisteringInputObjectGraphTypeTests.cs
@@ -0,0 +1,27 @@
+using GraphQL.Types;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests.Types
+{
+    public class AutoRegisteringInputObjectGraphTypeTests
+    {
+        [Fact]
+        public void RegistersWritablePropertiesOnly()
+        {
+            var inputType = new AutoRegisteringInputObjectGraphType<TestClass>();
+            inputType.Fields.Find("Field1").ShouldNotBeNull();
+            inputType.Fields.Find("Field2").ShouldBeNull();
+            inputType.Fields.Find("Field3").ShouldNotBeNull();
+            inputType.Fields.Find("Field4").ShouldBeNull();
+        }
+
+        private class TestClass
+        {
+            public int Field1 { get; set; }
+            public int Field2 { get; }
+            public int Field3 { set { } }
+            public int Field5() => 0;
+        }
+    }
+}
--- src/GraphQL/Types/Composite/AutoRegisteringObjectGraphType.cs
+++ src/GraphQL/Types/Composite/AutoRegisteringObjectGraphType.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
