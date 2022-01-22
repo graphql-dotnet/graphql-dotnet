0b2577df55392393b5b33cb3fd24a27cb5a29359
Shane Krueger <Shane@acdmail.com>
2022-01-22 11:21:28 -0500

    Refactor existing classes

---
+++ src/GraphQL/Types/Composite/AutoRegisteringHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace GraphQL.Types
+{
+    internal static class AutoRegisteringHelper
+    {
+        internal static void SetFields<TSourceType>(ComplexGraphType<TSourceType> type, IEnumerable<PropertyInfo> properties, params Expression<Func<TSourceType, object?>>[]? excludedProperties)
+        {
+            type.Name = typeof(TSourceType).GraphQLName();
+
+            foreach (var propertyInfo in properties)
+            {
+                if (excludedProperties?.Any(p => GetPropertyName(p) == propertyInfo.Name) == true)
+                    continue;
+
+                type.Field(
+                    type: propertyInfo.PropertyType.GetGraphTypeFromType(IsNullableProperty(propertyInfo), type is IInputObjectGraphType ? TypeMappingMode.InputType : TypeMappingMode.OutputType),
+                    name: propertyInfo.Name,
+                    description: propertyInfo.Description(),
+                    deprecationReason: propertyInfo.ObsoleteMessage()
+                ).DefaultValue = (propertyInfo.GetCustomAttributes(typeof(DefaultValueAttribute), false).FirstOrDefault() as DefaultValueAttribute)?.Value;
+            }
+        }
+
+        private static bool IsNullableProperty(PropertyInfo propertyInfo)
+        {
+            if (Attribute.IsDefined(propertyInfo, typeof(RequiredAttribute)))
+                return false;
+
+            if (!propertyInfo.PropertyType.IsValueType)
+                return true;
+
+            return propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        private static string GetPropertyName<TSourceType>(Expression<Func<TSourceType, object?>> expression)
+        {
+            if (expression.Body is MemberExpression m1)
+                return m1.Member.Name;
+
+            if (expression.Body is UnaryExpression u && u.Operand is MemberExpression m2)
+                return m2.Member.Name;
+
+            throw new NotSupportedException($"Unsupported type of expression: {expression.GetType().Name}");
+        }
+    }
+}
