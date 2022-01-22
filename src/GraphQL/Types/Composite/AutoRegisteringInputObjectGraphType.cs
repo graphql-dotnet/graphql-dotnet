61ab8d3b9d9a67cd634fcb8cce44c7eda0a114eb
Shane Krueger <shane@acdmail.com>
2022-01-22 11:37:36 -0500

    Refactor existing classes to separate files (#2872)

--- src/GraphQL/Types/Composite/AutoRegisteringInputObjectGraphType.cs
+++ src/GraphQL/Types/Composite/AutoRegisteringInputObjectGraphType.cs
@@ -8,23 +8,24 @@ using System.Reflection;
 namespace GraphQL.Types
 {
     /// <summary>
-    /// Allows you to automatically register the necessary fields for the specified type.
+    /// Allows you to automatically register the necessary fields for the specified input type.
     /// Supports <see cref="DescriptionAttribute"/>, <see cref="ObsoleteAttribute"/>, <see cref="DefaultValueAttribute"/> and <see cref="RequiredAttribute"/>.
     /// Also it can get descriptions for fields from the XML comments.
+    /// Note that now __InputValue has no isDeprecated and deprecationReason fields but in the future they may appear - https://github.com/graphql/graphql-spec/pull/525
     /// </summary>
     /// <typeparam name="TSourceType"></typeparam>
-    public class AutoRegisteringObjectGraphType<TSourceType> : ObjectGraphType<TSourceType>
+    public class AutoRegisteringInputObjectGraphType<TSourceType> : InputObjectGraphType<TSourceType>
     {
         /// <summary>
         /// Creates a GraphQL type from <typeparamref name="TSourceType"/>.
         /// </summary>
-        public AutoRegisteringObjectGraphType() : this(null) { }
+        public AutoRegisteringInputObjectGraphType() : this(null) { }
 
         /// <summary>
         /// Creates a GraphQL type from <typeparamref name="TSourceType"/> by specifying fields to exclude from registration.
         /// </summary>
         /// <param name="excludedProperties"> Expressions for excluding fields, for example 'o => o.Age'. </param>
-        public AutoRegisteringObjectGraphType(params Expression<Func<TSourceType, object?>>[]? excludedProperties)
+        public AutoRegisteringInputObjectGraphType(params Expression<Func<TSourceType, object?>>[]? excludedProperties)
         {
             AutoRegisteringHelper.SetFields(this, GetRegisteredProperties(), excludedProperties);
         }
