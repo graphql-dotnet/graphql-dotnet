8ebde14150253226a552ba477c4c74950d83074b
Shane Krueger <Shane@acdmail.com>
2022-01-22 12:17:21 -0500

    Remove using

--- src/GraphQL/Types/Composite/AutoRegisteringObjectGraphType.cs
+++ src/GraphQL/Types/Composite/AutoRegisteringObjectGraphType.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
