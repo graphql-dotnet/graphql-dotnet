a2d584f47f55a6b6b49decbc496e9fd806538b00
Ivan Maximov <sungam3r@yandex.ru>
2022-01-26 18:07:46 +0300

    fix tests

--- src/GraphQL.Tests/Types/FloatGraphTypeBadValueTests.cs
+++ src/GraphQL.Tests/Types/FloatGraphTypeBadValueTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 using GraphQL.Types;
 using GraphQLParser.AST;
@@ -10,16 +11,16 @@ namespace GraphQL.Tests.Bugs
     // https://github.com/graphql-dotnet/graphql-dotnet/pulls/1781
     public class FloatGraphTypeBadValueTests : QueryTestBase<PR1781Schema>
     {
+        private static readonly string _minNumber = ((FormattableString)$"{double.MinValue:0}0.0").ToString(CultureInfo.InvariantCulture);
+        private static readonly string _maxNumber = ((FormattableString)$"{double.MaxValue:0}0.0").ToString(CultureInfo.InvariantCulture);
+
         [Fact]
         public void BadFloatValues()
         {
             var type = new FloatGraphType();
 
-            var literal1 = $"{double.MaxValue:0}0.0";
-            var literal2 = $"{double.MinValue:0}0.0";
-
-            var value1 = new GraphQLFloatValue(literal1);
-            var value2 = new GraphQLFloatValue(literal2);
+            var value1 = new GraphQLFloatValue(_maxNumber);
+            var value2 = new GraphQLFloatValue(_minNumber);
 
             type.CanParseLiteral(value1).ShouldBeFalse();
             type.CanParseLiteral(value2).ShouldBeFalse();
@@ -32,40 +33,38 @@ namespace GraphQL.Tests.Bugs
         public async Task DocumentExecuter_really_big_double_Invalid()
         {
             var de = new DocumentExecuter();
-            var number = $"{double.MaxValue:0}0.0";
-            number.ShouldBe("1797693134862320000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0");
+            _maxNumber.ShouldBe("1797693134862320000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0");
             var valid = await de.ExecuteAsync(new ExecutionOptions
             {
                 // create a floating-point value that is larger than double.MaxValue
                 // in the expression "{double.MaxValue:0}0.0" below, the 0.0 effectively
                 // multiplies double.MaxValue by 10 and the .0 forces the parser to
                 // assume it is a floating point value rather than a large integer
-                Query = $"{{ test(arg:{number}) }}",
+                Query = $"{{ test(arg:{_maxNumber}) }}",
                 Schema = Schema,
             });
             valid.ShouldNotBeNull();
             valid.Data.ShouldBeNull();
             valid.Errors.ShouldNotBeNull();
             valid.Errors.Count.ShouldBe(1);
-            valid.Errors[0].Message.ShouldBe($"Argument 'arg' has invalid value. Expected type 'Float', found {number}.");
+            valid.Errors[0].Message.ShouldBe($"Argument 'arg' has invalid value. Expected type 'Float', found {_maxNumber}.");
         }
 
         [Fact]
         public async Task DocumentExecuter_really_small_double_Invalid()
         {
             var de = new DocumentExecuter();
-            var number = $"{double.MinValue:0}0.0";
-            number.ShouldBe("-1797693134862320000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0");
+            _minNumber.ShouldBe("-1797693134862320000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0");
             var valid = await de.ExecuteAsync(new ExecutionOptions
             {
-                Query = $"{{ test(arg:{number}) }}",
+                Query = $"{{ test(arg:{_minNumber}) }}",
                 Schema = Schema,
             });
             valid.ShouldNotBeNull();
             valid.Data.ShouldBeNull();
             valid.Errors.ShouldNotBeNull();
             valid.Errors.Count.ShouldBe(1);
-            valid.Errors[0].Message.ShouldBe($"Argument 'arg' has invalid value. Expected type 'Float', found {number}.");
+            valid.Errors[0].Message.ShouldBe($"Argument 'arg' has invalid value. Expected type 'Float', found {_minNumber}.");
         }
     }
 
