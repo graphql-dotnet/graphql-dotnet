fa5491b66801fcc5df97d026af9457fe8e142aa5
Shane Krueger <Shane@acdmail.com>
2022-01-28 13:49:02 -0500

    Increase code coverage

---
+++ src/GraphQL.Tests/TypeExtensionTests.cs
@@ -0,0 +1,24 @@
+using System;
+using GraphQL.Types;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Tests
+{
+    public class TypeExtensionTests
+    {
+        [Theory]
+        [InlineData(typeof(IdGraphType), true)]
+        [InlineData(typeof(ObjectGraphType), true)]
+        [InlineData(typeof(InputObjectGraphType), true)]
+        [InlineData(typeof(NonNullGraphType<IdGraphType>), false)]
+        [InlineData(typeof(ListGraphType<IdGraphType>), false)]
+        [InlineData(typeof(NonNullGraphType), false)]
+        [InlineData(typeof(ListGraphType), false)]
+        [InlineData(null, false)]
+        public void IsNamedGraphType(Type type, bool expected)
+        {
+            type.IsNamedGraphType().ShouldBe(expected);
+        }
+    }
+}
