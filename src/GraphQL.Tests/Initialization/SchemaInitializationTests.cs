31f53a5ff6c6d2c7b3b38a74e41adb9558d47e18
Ivan Maximov <sungam3r@yandex.ru>
2022-02-01 18:14:42 +0300

    Tests for empty enum

--- src/GraphQL.Tests/Initialization/SchemaInitializationTests.cs
+++ src/GraphQL.Tests/Initialization/SchemaInitializationTests.cs
@@ -76,6 +76,13 @@ namespace GraphQL.Tests.Initialization
             ShouldThrow<SchemaWithInvalidDefault1, InvalidOperationException>("The default value of argument 'argOne' of field 'Object.field' is invalid.");
             ShouldThrow<SchemaWithInvalidDefault2, InvalidOperationException>("The default value of argument 'argOne' of field 'Object.field' is invalid.");
         }
+
+        [Fact]
+        public void SchemaWithEnumWithoutValues_Should_Throw()
+        {
+            ShouldThrow<SchemaWithEnumWithoutValues1, InvalidOperationException>("An Enum type 'EnumWithoutValues' must define one or more unique enum values.");
+            ShouldThrow<SchemaWithEnumWithoutValues2, InvalidOperationException>("An Enum type 'Enumeration' must define one or more unique enum values.");
+        }
     }
 
     public class EmptyQuerySchema : Schema
@@ -300,6 +307,28 @@ namespace GraphQL.Tests.Initialization
                        DefaultValue = new SchemaWithInvalidDefault1.SomeInput { Names = new List<string> { "a", null, "b" } }
                    }));
             Query = root;
+        }
+    }
+
+    public class SchemaWithEnumWithoutValues1 : Schema
+    {
+        public enum EnumWithoutValues
+        {
+        }
+
+        public SchemaWithEnumWithoutValues1()
+        {
+            var type = new EnumerationGraphType<EnumWithoutValues>();
+            RegisterType(type);
+        }
+    }
+
+    public class SchemaWithEnumWithoutValues2 : Schema
+    {
+        public SchemaWithEnumWithoutValues2()
+        {
+            var type = new EnumerationGraphType();
+            RegisterType(type);
         }
     }
 }
