da8f032ab1ccaf1a3dff93ca8d220ef9f1c8c5cd
Shane32 <shane@acdmail.com>
2022-02-24 15:10:07 -0500

    Update xml comment

--- src/GraphQL/FromUserContext.cs
+++ src/GraphQL/FromUserContext.cs
@@ -1,9 +1,10 @@
+using GraphQL.Execution;
 using GraphQL.Types;
 
 namespace GraphQL
 {
     /// <summary>
-    /// Specifies that the method argument should be pulled from <see cref="IResolveFieldContext.Source"/>,
+    /// Specifies that the method argument should be pulled from <see cref="IResolveFieldContext"/>.<see cref="IProvideUserContext.UserContext">UserContext</see>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter)]
     public class FromUserContextAttribute : GraphQLAttribute
