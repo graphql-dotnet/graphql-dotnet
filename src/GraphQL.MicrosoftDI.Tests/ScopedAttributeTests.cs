0d9fcc28f4c9d24178d392f989bd3ef86f9956f5
Shane Krueger <shane@acdmail.com>
2022-03-02 11:07:35 -0500

    Allow ScopedAttribute to work with subscription fields (#2975)

--- src/GraphQL.MicrosoftDI.Tests/ScopedAttributeTests.cs
+++ src/GraphQL.MicrosoftDI.Tests/ScopedAttributeTests.cs
@@ -1,3 +1,4 @@
+using GraphQL.Subscription;
 using GraphQL.Types;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -39,6 +40,35 @@ namespace GraphQL.MicrosoftDI.Tests
             Class1.DisposedCount.ShouldBe(5);
         }
 
+        [Fact]
+        public async void ScopedSubscriptionWorks()
+        {
+            Class1.DisposedCount = 0;
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<Class1>();
+            serviceCollection.AddScoped<Class2>();
+            var rootServiceProvider = serviceCollection.BuildServiceProvider(false);
+            var graphType = new AutoRegisteringObjectGraphType<TestClass>();
+            var context = new ResolveEventStreamContext
+            {
+                Source = new TestClass(),
+                RequestServices = rootServiceProvider,
+            };
+            var unscopedSubscriptionResolver = ((EventStreamFieldType)graphType.Fields.Find(nameof(TestClass.UnscopedAsyncSubscription))!).AsyncSubscriber!;
+            var scopedAsyncSubscriptionResolver = ((EventStreamFieldType)graphType.Fields.Find(nameof(TestClass.ScopedAsyncSubscription))!).AsyncSubscriber!;
+            (await unscopedSubscriptionResolver.SubscribeAsync(context)).Subscribe(new SampleObserver("0 1"));
+            (await unscopedSubscriptionResolver.SubscribeAsync(context)).Subscribe(new SampleObserver("1 2"));
+            (await unscopedSubscriptionResolver.SubscribeAsync(context)).Subscribe(new SampleObserver("2 3"));
+            Class1.DisposedCount.ShouldBe(0);
+            (await scopedAsyncSubscriptionResolver.SubscribeAsync(context)).Subscribe(new SampleObserver("0 1"));
+            Class1.DisposedCount.ShouldBe(1);
+            (await scopedAsyncSubscriptionResolver.SubscribeAsync(context)).Subscribe(new SampleObserver("0 1"));
+            Class1.DisposedCount.ShouldBe(2);
+            (await unscopedSubscriptionResolver.SubscribeAsync(context)).Subscribe(new SampleObserver("3 4"));
+            rootServiceProvider.Dispose();
+            Class1.DisposedCount.ShouldBe(3);
+        }
+
         private class TestClass
         {
             public string UnscopedField([FromServices] Class1 arg1, [FromServices] Class2 arg2)
@@ -58,6 +88,39 @@ namespace GraphQL.MicrosoftDI.Tests
                 await Task.Yield();
                 return $"{arg1.Value++} {arg2.Value}";
             }
+
+            public async Task<IObservable<string>> UnscopedAsyncSubscription([FromServices] Class1 arg1, [FromServices] Class2 arg2)
+            {
+                await Task.Yield();
+                return new SampleObservable($"{arg1.Value++} {arg2.Value}");
+            }
+
+            [Scoped]
+            public async Task<IObservable<string>> ScopedAsyncSubscription([FromServices] Class1 arg1, [FromServices] Class2 arg2)
+            {
+                await Task.Yield();
+                return new SampleObservable($"{arg1.Value++} {arg2.Value}");
+            }
+        }
+
+        private class SampleObservable : IObservable<string>, IDisposable
+        {
+            private readonly string _data;
+
+            public SampleObservable(string data)
+            {
+                _data = data;
+            }
+
+            public IDisposable Subscribe(IObserver<string> observer)
+            {
+                //immediately push the predefined data
+                observer.OnNext(_data);
+                //return a dummy value
+                return this;
+            }
+
+            void IDisposable.Dispose() { }
         }
 
         private class Class1 : IDisposable
@@ -91,6 +154,24 @@ namespace GraphQL.MicrosoftDI.Tests
             }
 
             public int Value => _class1.Value;
+        }
+
+        private class SampleObserver : IObserver<object>
+        {
+            private string _expectedData;
+
+            public SampleObserver(string expectedData)
+            {
+                _expectedData = expectedData;
+            }
+
+            public void OnCompleted() => throw new NotImplementedException();
+            public void OnError(Exception error) => throw new NotImplementedException();
+            public void OnNext(object value)
+            {
+                value.ShouldBe(_expectedData);
+                _expectedData = null; //match only once
+            }
         }
     }
 }
