3b289380495f8f8d5eb0626df540cbdfa421459c
Shane Krueger <shane@acdmail.com>
2022-03-07 14:05:05 -0500

    Update src/GraphQL/Resolvers/IEventStreamResolver.cs
    Co-authored-by: sungam3r2022 <100795854+sungam3r2022@users.noreply.github.com>

--- src/GraphQL/Resolvers/IEventStreamResolver.cs
+++ src/GraphQL/Resolvers/IEventStreamResolver.cs
@@ -4,7 +4,7 @@ namespace GraphQL.Resolvers
     /// Returns an <see cref="IObservable{T}"/> for a field. The <see cref="IObservable{T}"/> provides
     /// a sequence of event notifications (aka 'an event stream') to the subscription execution engine,
     /// which will resolve child fields using the event data as the source. Then the resolved graph is
-    /// returned to the client, and the process repeats for additional event notifications. 
+    /// returned to the client, and the process repeats for further event notifications. 
     /// </summary>
     public interface IEventStreamResolver
     {
