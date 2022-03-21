8d4e0444a81e670edf91c89abae2afc450360360
Shane32 <shane@acdmail.com>
2022-03-21 12:38:39 -0400

    Update

--- src/GraphQL.Tests/Serialization/SystemTextJson/GraphQLSerializerTests.cs
+++ src/GraphQL.Tests/Serialization/SystemTextJson/GraphQLSerializerTests.cs
@@ -6,6 +6,29 @@ namespace GraphQL.Tests.Serialization.SystemTextJson;
 
 public class GraphQLSerializerTests
 {
+#if NET5_0_OR_GREATER
+    [Fact]
+    public void DoesNotModifyOptions_1()
+    {
+        var options = new JsonSerializerOptions();
+        _ = JsonSerializer.Serialize("hello", options);
+        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
+        var serializer = new GraphQLSerializer(options);
+        _ = serializer.Serialize("hello");
+        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void DoesNotModifyOptions_2()
+    {
+        var options = new JsonSerializerOptions();
+        _ = JsonSerializer.Serialize("hello", options);
+        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
+        var serializer = new GraphQLSerializer(options, new ErrorInfoProvider());
+        _ = serializer.Serialize("hello");
+        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
+    }
+#else
     [Fact]
     public void GraphQLSerializer_Should_Be_Created_With_Same_Options_Multiple_Times()
     {
@@ -15,29 +38,6 @@ public class GraphQLSerializerTests
         _ = serializer.Serialize<object>(new { name = "Tom", age = 42 });
         new GraphQLSerializer(options);
         options.Converters.Count.ShouldBe(8);
-    }
-
-#if NET5_0_OR_GREATER
-    [Fact]
-    public void DoesNotModifyOptions1()
-    {
-        var options = new JsonSerializerOptions();
-        _ = JsonSerializer.Serialize("hello", options);
-        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
-        var serializer = new GraphQLSerializer(options);
-        _ = serializer.Serialize("hello");
-        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
-    }
-
-    [Fact]
-    public void DoesNotModifyOptions2()
-    {
-        var options = new JsonSerializerOptions();
-        _ = JsonSerializer.Serialize("hello", options);
-        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
-        var serializer = new GraphQLSerializer(options, new ErrorInfoProvider());
-        _ = serializer.Serialize("hello");
-        options.Converters.Any(x => x.CanConvert(typeof(Inputs))).ShouldBeFalse();
     }
 #endif
 }
