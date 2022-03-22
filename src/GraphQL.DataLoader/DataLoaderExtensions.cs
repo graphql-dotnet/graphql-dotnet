23b79434dd2e4dd42d4d3324deed2c1218590340
Shane Krueger <shane@acdmail.com>
2022-03-22 16:57:04 -0400

    Add 'Then' data loader extension method for lists of data loader results. (#3021)

--- src/GraphQL.DataLoader/DataLoaderExtensions.cs
+++ src/GraphQL.DataLoader/DataLoaderExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -110,6 +111,71 @@ namespace GraphQL.DataLoader
             {
                 var result = await parent.GetResultAsync(cancellationToken).ConfigureAwait(false);
                 return chainedDelegate(result);
+            });
+        }
+
+        /// <summary>
+        /// Chains post-processing to a list of pending data loader operations.
+        /// <br/><br/>
+        /// Be sure the source list has been enumerated, for instance by calling
+        /// <see cref="Enumerable.ToList{TSource}(IEnumerable{TSource})">ToList</see>,
+        /// before calling this function.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of the data loaders.</typeparam>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="parents">The list of pending data loader operations.</param>
+        /// <param name="chainedDelegate">The delegate to execute once the data loaders finish loading.</param>
+        /// <returns>A pending data loader operation that can return a value once the data loaders and the chained delegate finish.</returns>
+        public static IDataLoaderResult<TResult> Then<T, TResult>(this IEnumerable<IDataLoaderResult<T>> parents, Func<IEnumerable<T>, CancellationToken, Task<TResult>> chainedDelegate)
+        {
+            return new SimpleDataLoader<TResult>(async cancellationToken =>
+            {
+                List<T> list;
+                if (parents is ICollection collection)
+                    list = new(collection.Count);
+                else
+                    list = new();
+                foreach (var parent in parents)
+                {
+                    list.Add(await parent.GetResultAsync(cancellationToken).ConfigureAwait(false));
+                }
+                return await chainedDelegate(list, cancellationToken).ConfigureAwait(false);
+            });
+        }
+
+        /// <inheritdoc cref="Then{T, TResult}(IEnumerable{IDataLoaderResult{T}}, Func{IEnumerable{T}, CancellationToken, Task{TResult}})"/>
+        public static IDataLoaderResult<TResult> Then<T, TResult>(this IEnumerable<IDataLoaderResult<T>> parents, Func<IEnumerable<T>, Task<TResult>> chainedDelegate)
+        {
+            return new SimpleDataLoader<TResult>(async cancellationToken =>
+            {
+                List<T> list;
+                if (parents is ICollection collection)
+                    list = new(collection.Count);
+                else
+                    list = new();
+                foreach (var parent in parents)
+                {
+                    list.Add(await parent.GetResultAsync(cancellationToken).ConfigureAwait(false));
+                }
+                return await chainedDelegate(list).ConfigureAwait(false);
+            });
+        }
+
+        /// <inheritdoc cref="Then{T, TResult}(IEnumerable{IDataLoaderResult{T}}, Func{IEnumerable{T}, CancellationToken, Task{TResult}})"/>
+        public static IDataLoaderResult<TResult> Then<T, TResult>(this IEnumerable<IDataLoaderResult<T>> parents, Func<IEnumerable<T>, TResult> chainedDelegate)
+        {
+            return new SimpleDataLoader<TResult>(async cancellationToken =>
+            {
+                List<T> list;
+                if (parents is ICollection collection)
+                    list = new(collection.Count);
+                else
+                    list = new();
+                foreach (var parent in parents)
+                {
+                    list.Add(await parent.GetResultAsync(cancellationToken).ConfigureAwait(false));
+                }
+                return chainedDelegate(list);
             });
         }
 
