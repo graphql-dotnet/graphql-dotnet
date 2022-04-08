ce15a01338ce27aa82fb01c3c9b35de06dad46f3
Shane Krueger <shane@acdmail.com>
2022-04-08 14:16:44 -0400

    Add role support to authorization extensions (#3067)

--- src/GraphQL/Authorization/AuthorizationExtensions.cs
+++ src/GraphQL/Authorization/AuthorizationExtensions.cs
@@ -15,6 +15,12 @@ namespace GraphQL
         public const string POLICY_KEY = "Authorization__Policies";
 
         /// <summary>
+        /// Metadata key name for storing authorization role names. Value of this key
+        /// is a simple list of strings.
+        /// </summary>
+        public const string ROLE_KEY = "Authorization__Roles";
+
+        /// <summary>
         /// Gets a list of authorization policy names for the specified metadata provider if any.
         /// Otherwise returns <see langword="null"/>.
         /// </summary>
@@ -26,6 +32,17 @@ namespace GraphQL
         public static List<string>? GetPolicies(this IProvideMetadata provider) => provider.GetMetadata<List<string>>(POLICY_KEY);
 
         /// <summary>
+        /// Gets a list of authorization roles names for the specified metadata provider if any.
+        /// Otherwise returns <see langword="null"/>.
+        /// </summary>
+        /// <param name="provider">
+        /// Metadata provider. This can be an instance of <see cref="GraphType"/>,
+        /// <see cref="FieldType"/>, <see cref="Schema"/> or others.
+        /// </param>
+        /// <returns> List of authorization role names applied to this metadata provider. </returns>
+        public static List<string>? GetRoles(this IProvideMetadata provider) => provider.GetMetadata<List<string>>(ROLE_KEY);
+
+        /// <summary>
         /// Gets a boolean value that determines whether any authorization policy is applied to this metadata provider.
         /// </summary>
         /// <param name="provider">
@@ -33,7 +50,7 @@ namespace GraphQL
         /// <see cref="FieldType"/>, <see cref="Schema"/> or others.
         /// </param>
         /// <returns> <c>true</c> if any authorization policy is applied, otherwise <c>false</c>. </returns>
-        public static bool RequiresAuthorization(this IProvideMetadata provider) => GetPolicies(provider)?.Count > 0;
+        public static bool RequiresAuthorization(this IProvideMetadata provider) => GetPolicies(provider)?.Count > 0 || GetRoles(provider)?.Count > 0;
 
         /// <summary>
         /// Adds authorization policy to the specified metadata provider. If the provider already contains
@@ -48,7 +65,7 @@ namespace GraphQL
         /// </param>
         /// <param name="policy"> Authorization policy name. </param>
         /// <returns> The reference to the specified <paramref name="provider"/>. </returns>
-        public static TMetadataProvider AuthorizeWith<TMetadataProvider>(this TMetadataProvider provider, string policy)
+        public static TMetadataProvider AuthorizeWithPolicy<TMetadataProvider>(this TMetadataProvider provider, string policy)
             where TMetadataProvider : IProvideMetadata
         {
             if (policy == null)
@@ -63,6 +80,76 @@ namespace GraphQL
             return provider;
         }
 
+        /// <inheritdoc cref="AuthorizeWithPolicy{TMetadataProvider}(TMetadataProvider, string)"/>
+        [Obsolete("Please use AuthorizeWithPolicy instead. Will be removed in v6.")]
+        public static TMetadataProvider AuthorizeWith<TMetadataProvider>(this TMetadataProvider provider, string policy)
+            where TMetadataProvider : IProvideMetadata
+            => AuthorizeWithPolicy(provider, policy);
+
+        /// <summary>
+        /// Adds authorization role(s) to the specified metadata provider. Roles should
+        /// be comma-separated and role names will be trimmed. If the underlying field already
+        /// contains a role with the same name, then it will not be added twice.
+        /// </summary>
+        /// <typeparam name="TMetadataProvider"> The type of metadata provider. Generics are used here to
+        /// let compiler infer the returning type to allow methods chaining.
+        /// </typeparam>
+        /// <param name="provider">
+        /// Metadata provider. This can be an instance of <see cref="GraphType"/>,
+        /// <see cref="FieldType"/>, <see cref="Schema"/> or others.
+        /// </param>
+        /// <param name="roles"> Comma-separated list of authorization role name(s). </param>
+        /// <returns> The reference to the specified <paramref name="provider"/>. </returns>
+        public static TMetadataProvider AuthorizeWithRoles<TMetadataProvider>(this TMetadataProvider provider, string roles)
+            where TMetadataProvider : IProvideMetadata
+        {
+            if (roles == null)
+                throw new ArgumentNullException(nameof(roles));
+
+            var list = GetRoles(provider) ?? new List<string>();
+
+            foreach (var role in roles.Split(','))
+            {
+                var role2 = role.Trim();
+                if (role2 != "" && !list.Contains(role2))
+                    list.Add(role2);
+            }
+
+            provider.Metadata[ROLE_KEY] = list;
+            return provider;
+        }
+
+        /// <summary>
+        /// Adds authorization role(s) to the specified metadata provider.  If the underlying field already
+        /// contains a role with the same name, then it will not be added twice.
+        /// </summary>
+        /// <typeparam name="TMetadataProvider"> The type of metadata provider. Generics are used here to
+        /// let compiler infer the returning type to allow methods chaining.
+        /// </typeparam>
+        /// <param name="provider">
+        /// Metadata provider. This can be an instance of <see cref="GraphType"/>,
+        /// <see cref="FieldType"/>, <see cref="Schema"/> or others.
+        /// </param>
+        /// <param name="roles"> List of authorization role name(s). </param>
+        /// <returns> The reference to the specified <paramref name="provider"/>. </returns>
+        public static TMetadataProvider AuthorizeWithRoles<TMetadataProvider>(this TMetadataProvider provider, params string[] roles)
+            where TMetadataProvider : IProvideMetadata
+        {
+            if (roles == null)
+                throw new ArgumentNullException(nameof(roles));
+
+            var list = GetRoles(provider) ?? new List<string>();
+
+            foreach (var role in roles)
+            {
+                if (role != "" && !list.Contains(role))
+                    list.Add(role);
+            }
+
+            provider.Metadata[ROLE_KEY] = list;
+            return provider;
+        }
+
         /// <summary>
         /// Adds authorization policy to the specified field builder. If the underlying field already contains
         /// a policy with the same name, then it will not be added twice.
@@ -72,10 +159,45 @@ namespace GraphQL
         /// <param name="builder"></param>
         /// <param name="policy"> Authorization policy name. </param>
         /// <returns> The reference to the specified <paramref name="builder"/>. </returns>
-        public static FieldBuilder<TSourceType, TReturnType> AuthorizeWith<TSourceType, TReturnType>(
+        public static FieldBuilder<TSourceType, TReturnType> AuthorizeWithPolicy<TSourceType, TReturnType>(
             this FieldBuilder<TSourceType, TReturnType> builder, string policy)
         {
-            builder.FieldType.AuthorizeWith(policy);
+            builder.FieldType.AuthorizeWithPolicy(policy);
+            return builder;
+        }
+
+        /// <inheritdoc cref="AuthorizeWith{TSourceType, TReturnType}(FieldBuilder{TSourceType, TReturnType}, string)"/>
+        [Obsolete("Please use AuthorizeWithPolicy instead. Will be removed in v6.")]
+        public static FieldBuilder<TSourceType, TReturnType> AuthorizeWith<TSourceType, TReturnType>(
+            this FieldBuilder<TSourceType, TReturnType> builder, string policy)
+            => AuthorizeWithPolicy(builder, policy);
+
+        /// <summary>
+        /// Adds authorization role(s) to the specified field builder. Roles should
+        /// be comma-separated and role names will be trimmed. If the underlying field already
+        /// contains a role with the same name, then it will not be added twice.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="roles"> Comma-separated list of authorization role name(s). </param>
+        /// <returns> The reference to the specified <paramref name="builder"/>. </returns>
+        public static FieldBuilder<TSourceType, TReturnType> AuthorizeWithRoles<TSourceType, TReturnType>(
+            this FieldBuilder<TSourceType, TReturnType> builder, string roles)
+        {
+            builder.FieldType.AuthorizeWithRoles(roles);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds authorization role(s) to the specified field builder. If the underlying field already
+        /// contains a role with the same name, then it will not be added twice.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="roles"> List of authorization role name(s). </param>
+        /// <returns> The reference to the specified <paramref name="builder"/>. </returns>
+        public static FieldBuilder<TSourceType, TReturnType> AuthorizeWithRoles<TSourceType, TReturnType>(
+            this FieldBuilder<TSourceType, TReturnType> builder, params string[] roles)
+        {
+            builder.FieldType.AuthorizeWithRoles(roles);
             return builder;
         }
 
@@ -87,10 +209,45 @@ namespace GraphQL
         /// <param name="builder"></param>
         /// <param name="policy"> Authorization policy name. </param>
         /// <returns> The reference to the specified <paramref name="builder"/>. </returns>
-        public static ConnectionBuilder<TSourceType> AuthorizeWith<TSourceType>(
+        public static ConnectionBuilder<TSourceType> AuthorizeWithPolicy<TSourceType>(
             this ConnectionBuilder<TSourceType> builder, string policy)
         {
-            builder.FieldType.AuthorizeWith(policy);
+            builder.FieldType.AuthorizeWithPolicy(policy);
+            return builder;
+        }
+
+        /// <inheritdoc cref="AuthorizeWithPolicy{TSourceType}(ConnectionBuilder{TSourceType}, string)"/>
+        [Obsolete("Please use AuthorizeWithPolicy instead. Will be removed in v6.")]
+        public static ConnectionBuilder<TSourceType> AuthorizeWith<TSourceType>(
+            this ConnectionBuilder<TSourceType> builder, string policy)
+            => AuthorizeWithPolicy(builder, policy);
+
+        /// <summary>
+        /// Adds authorization role(s) to the specified connection builder. Roles should
+        /// be comma-separated and role names will be trimmed. If the underlying field already
+        /// contains a role with the same name, then it will not be added twice.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="roles"> Comma-separated list of authorization role name(s). </param>
+        /// <returns> The reference to the specified <paramref name="builder"/>. </returns>
+        public static ConnectionBuilder<TSourceType> AuthorizeWithRoles<TSourceType>(
+            this ConnectionBuilder<TSourceType> builder, string roles)
+        {
+            builder.FieldType.AuthorizeWithRoles(roles);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds authorization role(s) to the specified connection builder. If the underlying field already
+        /// contains a role with the same name, then it will not be added twice.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="roles"> List of authorization role name(s). </param>
+        /// <returns> The reference to the specified <paramref name="builder"/>. </returns>
+        public static ConnectionBuilder<TSourceType> AuthorizeWithRoles<TSourceType>(
+            this ConnectionBuilder<TSourceType> builder, params string[] roles)
+        {
+            builder.FieldType.AuthorizeWithRoles(roles);
             return builder;
         }
     }
--- src/GraphQL/Authorization/GraphQLAuthorizeAttribute.cs
+++ src/GraphQL/Authorization/GraphQLAuthorizeAttribute.cs
@@ -4,10 +4,19 @@ using GraphQL.Utilities;
 namespace GraphQL
 {
     /// <summary>
-    /// Attribute to apply authorization policy when using schema first approach.
+    /// Attribute to apply an authorization policy and/or roles to a graph or field.
+    /// This attribute mimics AuthorizeAttribute from ASP.NET Core so it is something
+    /// people are likely used to, if they do any web programming in C#.
     /// </summary>
     public class GraphQLAuthorizeAttribute : GraphQLAttribute
     {
+        /// <summary>
+        /// Creates an empty instance of <see cref="GraphQLAuthorizeAttribute"/> with no policy/roles specified.
+        /// </summary>
+        public GraphQLAuthorizeAttribute()
+        {
+        }
+
         /// <summary>
         /// Creates an instance with the specified policy name.
         /// </summary>
@@ -19,18 +28,52 @@ namespace GraphQL
         /// <summary>
         /// The name of policy to apply.
         /// </summary>
-        public string Policy { get; }
+        public string? Policy { get; init; }
+
+        /// <summary>
+        /// A comma-separated list of the roles to apply.
+        /// Role names will be trimmed before adding.
+        /// </summary>
+        public string? Roles { get; init; }
 
         /// <inheritdoc />
-        public override void Modify(TypeConfig type) => type.AuthorizeWith(Policy);
+        public override void Modify(TypeConfig type)
+        {
+            if (Policy != null)
+                type.AuthorizeWithPolicy(Policy);
+
+            if (Roles != null)
+                type.AuthorizeWithRoles(Roles);
+        }
 
         /// <inheritdoc />
-        public override void Modify(FieldConfig field) => field.AuthorizeWith(Policy);
+        public override void Modify(FieldConfig field)
+        {
+            if (Policy != null)
+                field.AuthorizeWithPolicy(Policy);
+
+            if (Roles != null)
+                field.AuthorizeWithRoles(Roles);
+        }
 
         /// <inheritdoc />
-        public override void Modify(IGraphType graphType) => graphType.AuthorizeWith(Policy);
+        public override void Modify(IGraphType graphType)
+        {
+            if (Policy != null)
+                graphType.AuthorizeWithPolicy(Policy);
+
+            if (Roles != null)
+                graphType.AuthorizeWithRoles(Roles);
+        }
 
         /// <inheritdoc />
-        public override void Modify(FieldType fieldType, bool isInputType) => fieldType.AuthorizeWith(Policy);
+        public override void Modify(FieldType fieldType, bool isInputType)
+        {
+            if (Policy != null)
+                fieldType.AuthorizeWithPolicy(Policy);
+
+            if (Roles != null)
+                fieldType.AuthorizeWithRoles(Roles);
+        }
     }
 }
