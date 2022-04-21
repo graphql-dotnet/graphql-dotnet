119eec90ecbce17c748d84c9f6584abea8464381
Shane Krueger <shane@acdmail.com>
2022-04-21 09:51:09 -0400

    Add test for lists of enums (#3092)

--- src/GraphQL.Tests/Bugs/Bug1699InvalidEnum.cs
+++ src/GraphQL.Tests/Bugs/Bug1699InvalidEnum.cs
@@ -53,6 +53,15 @@ public class Bug1699InvalidEnum : QueryTestBase<Bug1699InvalidEnumSchema>
     public void Invalid_Enum_Within_NonNullList() => AssertQueryWithError("{ invalidEnumWithinNonNullList }", @"{ ""invalidEnumWithinNonNullList"": null }", "Error trying to resolve field 'invalidEnumWithinNonNullList'.", 1, 3, new object[] { "invalidEnumWithinNonNullList", 2 }, exception: new InvalidOperationException());
 
     [Fact]
+    public void Input_EnumList() => AssertQuerySuccess("{ inputList(arg: [SLEEPY]) }", @"{ ""inputList"": ""Sleepy"" }");
+
+    [Fact]
+    public void Input_EnumList_Variable() => AssertQuerySuccess("query($arg: [Bug1699Enum]) { inputList(arg: $arg) }", @"{ ""inputList"": ""Sleepy"" }", @"{""arg"":[""SLEEPY""]}".ToInputs());
+
+    [Fact]
+    public void Input_CustomEnumList() => AssertQuerySuccess("query($arg: [Bug1699CustomEnum]) { inputListCustom(arg: $arg) }", @"{ ""inputListCustom"": ""ISDOPEY"" }", @"{""arg"":[""ISDOPEY""]}".ToInputs());
+
+    [Fact]
     public void Input_Enum_Valid() => AssertQuerySuccess("{ inputEnum(arg: SLEEPY) }", @"{ ""inputEnum"": ""SLEEPY"" }");
 
     [Fact]
@@ -187,6 +196,14 @@ public class Bug1699InvalidEnumQuery : ObjectGraphType
         Field<Bug1699CustomEnumGraphType>(
             "customEnumSleepy",
             resolve: context => Bug1699Enum.Sleepy);
+        Field<StringGraphType>(
+            "inputList",
+            arguments: new QueryArguments(new QueryArgument<ListGraphType<EnumerationGraphType<Bug1699Enum>>> { Name = "arg" }),
+            resolve: ctx => string.Join(",", ctx.GetArgument<List<Bug1699Enum>>("arg").Select(x => x.ToString())));
+        Field<StringGraphType>(
+            "inputListCustom",
+            arguments: new QueryArguments(new QueryArgument<ListGraphType<Bug1699CustomEnumGraphType>> { Name = "arg" }),
+            resolve: ctx => string.Join(",", ctx.GetArgument<List<object>>("arg").Select(x => x.ToString())));
     }
 }
 
@@ -204,5 +221,6 @@ public class Bug1699CustomEnumGraphType : EnumerationGraphType
         Add(new EnumValueDefinition("ISGRUMPY", Bug1699Enum.Grumpy));
         Add(new EnumValueDefinition("ISHAPPY", Bug1699Enum.Happy));
         Add(new EnumValueDefinition("ISSLEEPY", 2));
+        Add(name: "ISDOPEY", value: "ISDOPEY", description: "testing description");
     }
 }
