e3e3dec639c82d7b6157338f4989f517442f122b
Ivan Maximov <sungam3r@yandex.ru>
2022-05-13 17:47:18 +0300

    Update src/GraphQL/Execution/Errors/RequestError.cs

--- src/GraphQL/Execution/Errors/RequestError.cs
+++ src/GraphQL/Execution/Errors/RequestError.cs
@@ -1,7 +1,8 @@
 namespace GraphQL.Execution;
 
 /// <summary>
-/// Represents an error that occurred prior to the execution of the request.
+/// Represents an error that occurred prior to the execution of the GraphQL request.
+/// This refers to any errors that arise before passing the request inside the GraphQL engine, that is, even before its validation.
 /// </summary>
 public class RequestError : ExecutionError
 {
