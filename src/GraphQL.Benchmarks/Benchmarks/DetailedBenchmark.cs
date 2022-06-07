e7c2a78211ec1f8b6a8a103e9db6f118fec849a7
Shane32 <shane@acdmail.com>
2022-06-07 14:15:38 -0400

    Convert IVariableVisitor to async

--- src/GraphQL.Benchmarks/Benchmarks/DetailedBenchmark.cs
+++ src/GraphQL.Benchmarks/Benchmarks/DetailedBenchmark.cs
@@ -223,7 +223,7 @@ public class DetailedBenchmark : IBenchmark
                 Schema = Schema,
                 Variables = Inputs ?? Inputs.Empty,
                 Operation = Operation,
-            }.GetVariableValues();
+            }.GetVariableValuesAsync().Result;
         }
 
         private static readonly DocumentValidator _documentValidator = new DocumentValidator();
