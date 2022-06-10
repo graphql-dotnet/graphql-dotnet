b96b74e18d741419ecfa988a86c448472698b20b
Shane Krueger <Shane@acdmail.com>
2022-06-09 23:54:37 -0400

    Update

--- src/GraphQL/Validation/Complexity/ComplexityAnalyzer.cs
+++ src/GraphQL/Validation/Complexity/ComplexityAnalyzer.cs
@@ -7,7 +7,7 @@ namespace GraphQL.Validation.Complexity
     /// <summary>
     /// The default complexity analyzer.
     /// </summary>
-    public class ComplexityAnalyzer
+    public class ComplexityAnalyzer : IComplexityAnalyzer
     {
         /// <inheritdoc/>
         public void Validate(GraphQLDocument document, ComplexityConfiguration complexityParameters, ISchema? schema = null)
--- src/GraphQL/Validation/Complexity/ComplexityVisitor.cs
+++ src/GraphQL/Validation/Complexity/ComplexityVisitor.cs
@@ -22,11 +22,6 @@ namespace GraphQL.Validation.Complexity
             _visitor = new TypeInfo(schema);
         }
 
-        public ComplexityVisitor(TypeInfo visitor)
-        {
-            _visitor = visitor;
-        }
-
         public override async ValueTask VisitAsync(ASTNode? node, AnalysisContext context)
         {
             if (node != null)
