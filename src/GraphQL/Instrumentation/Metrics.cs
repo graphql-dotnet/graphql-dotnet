d48612f9b6fb9f7b5a44fc0f1f6372743e872cc2
Ivan Maximov <sungam3r@yandex.ru>
2022-06-14 09:22:57 +0300

    move Marker back

--- src/GraphQL/Instrumentation/Metrics.cs
+++ src/GraphQL/Instrumentation/Metrics.cs
@@ -94,36 +94,36 @@ namespace GraphQL.Instrumentation
             _main?.MarkEnd(_stopwatch.Elapsed.TotalMilliseconds);
             return _records!.OrderBy(x => x.Start).ToArray();
         }
-    }
-
-    /// <summary>
-    /// An object that, when disposed, records the completion time of a performance metric in an assigned <see cref="PerfRecord"/>.
-    /// </summary>
-    public readonly struct Marker : IDisposable
-    {
-        private readonly PerfRecord _record;
-        private readonly ValueStopwatch _stopwatch;
 
         /// <summary>
-        /// Returns an instance that does not perform any action when disposed.
+        /// An object that, when disposed, records the completion time of a performance metric in an assigned <see cref="PerfRecord"/>.
         /// </summary>
-        public static readonly Marker Empty;
-
-        /// <summary>
-        /// Initializes an instance with the specified performance metric and running stopwatch.
-        /// </summary>
-        public Marker(PerfRecord record, ValueStopwatch stopwatch)
+        public readonly struct Marker : IDisposable
         {
-            _record = record;
-            _stopwatch = stopwatch;
-        }
+            private readonly PerfRecord _record;
+            private readonly ValueStopwatch _stopwatch;
 
-        /// <summary>
-        /// Stores the completion time of the assigned performance metric.
-        /// </summary>
-        public void Dispose()
-        {
-            _record?.MarkEnd(_stopwatch.Elapsed.TotalMilliseconds);
+            /// <summary>
+            /// Returns an instance that does not perform any action when disposed.
+            /// </summary>
+            public static readonly Marker Empty;
+
+            /// <summary>
+            /// Initializes an instance with the specified performance metric and running stopwatch.
+            /// </summary>
+            public Marker(PerfRecord record, ValueStopwatch stopwatch)
+            {
+                _record = record;
+                _stopwatch = stopwatch;
+            }
+
+            /// <summary>
+            /// Stores the completion time of the assigned performance metric.
+            /// </summary>
+            public void Dispose()
+            {
+                _record?.MarkEnd(_stopwatch.Elapsed.TotalMilliseconds);
+            }
         }
     }
 }
