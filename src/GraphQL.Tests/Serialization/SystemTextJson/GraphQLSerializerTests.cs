dc379ffea21f24141df2236c326486f9146ae553
Shane Krueger <shane@acdmail.com>
2022-06-20 11:02:10 -0400

    Fix SystemTextJson serializer to allow custom json converters for any data type (#3189)

--- src/GraphQL.Tests/Serialization/SystemTextJson/GraphQLSerializerTests.cs
+++ src/GraphQL.Tests/Serialization/SystemTextJson/GraphQLSerializerTests.cs
@@ -1,7 +1,10 @@
 #pragma warning disable IDE0005 // Using directive is unnecessary.
+using System.Globalization;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using GraphQL.Execution;
 using GraphQL.SystemTextJson;
+using GraphQL.Types;
 
 namespace GraphQL.Tests.Serialization.SystemTextJson;
 
@@ -41,4 +44,38 @@ public class GraphQLSerializerTests
         options.Converters.Count.ShouldBe(8);
     }
 #endif
+
+    [Fact]
+    public async Task CanOverrideDecimalDataType()
+    {
+        var result = await new DocumentExecuter().ExecuteAsync(new ExecutionOptions
+        {
+            Schema = new Schema { Query = new AutoRegisteringObjectGraphType<Query>() },
+            Query = "{hero}"
+        }).ConfigureAwait(false);
+
+        // typically a value of 1.0000m would be serialized as 1.0000
+        new GraphQLSerializer().Serialize(result).ShouldBe(@"{""data"":{""hero"":1.0000}}");
+
+        // tests that a custom converter can be registered for the decimal data type; here it serializes 1.0000m as 1
+        new GraphQLSerializer(o => o.Converters.Add(new SampleDecimalConverter())).Serialize(result).ShouldBe(@"{""data"":{""hero"":1}}");
+    }
+
+    private class SampleDecimalConverter : JsonConverter<decimal>
+    {
+        public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => throw new NotImplementedException();
+
+        public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options)
+#if NET6_0_OR_GREATER
+            => writer.WriteRawValue(value.ToString("0.#############", CultureInfo.InvariantCulture));
+#else
+            => writer.WriteNumberValue((double)value);
+#endif
+    }
+
+    private class Query
+    {
+        public static decimal Hero => 1.0000m;
+    }
 }
