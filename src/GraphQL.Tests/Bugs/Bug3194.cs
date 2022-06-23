7808f6a370a0b0285c7c401932d34a933aec2b8f
Shane32 <shane@acdmail.com>
2022-06-23 16:56:49 -0400

    Add property test

--- src/GraphQL.Tests/Bugs/Bug3194.cs
+++ src/GraphQL.Tests/Bugs/Bug3194.cs
@@ -15,16 +15,23 @@ schema {
 }
 
 type Query {
-  hello: String!
+  hello: String!    # test inherited method
+  hello2: String!   # test inherited property
 }
 ",
             c => c.Types.Include(typeof(QueryType)));
         schema.Initialize();
         var queryType = schema.AllTypes["Query"].ShouldBeAssignableTo<IComplexGraphType>();
+
         var fieldType = queryType.Fields.Find("hello").ShouldNotBeNull();
         var resolver = fieldType.Resolver.ShouldNotBeNull();
         var result = await resolver.ResolveAsync(new ResolveFieldContext()).ConfigureAwait(false);
         result.ShouldBe("World");
+
+        var fieldType2 = queryType.Fields.Find("hello2").ShouldNotBeNull();
+        var resolver2 = fieldType2.Resolver.ShouldNotBeNull();
+        var result2 = await resolver2.ResolveAsync(new ResolveFieldContext()).ConfigureAwait(false);
+        result2.ShouldBe("World");
     }
 
     [GraphQLMetadata(Name = "Query")]
@@ -35,5 +42,6 @@ type Query {
     private abstract class BaseType
     {
         public string Hello() => "World";
+        public string Hello2 => "World";
     }
 }
