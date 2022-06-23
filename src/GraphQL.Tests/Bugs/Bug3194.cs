b08e47454bae50fe8192210f734c790bec09eb58
Shane32 <shane@acdmail.com>
2022-06-23 16:38:13 -0400

    Fix inherited methods for schema builder

---
+++ src/GraphQL.Tests/Bugs/Bug3194.cs
@@ -0,0 +1,38 @@
+using GraphQL.Types;
+
+namespace GraphQL.Tests.Bugs;
+
+public class Bug3194
+{
+    [Fact]
+    public async Task InheritedMethodsWork()
+    {
+        var schema = Schema.For(
+            @"
+schema {
+  query: Query
+}
+
+type Query {
+  hello: String!
+}
+",
+            c => c.Types.Include(typeof(QueryType)));
+        schema.Initialize();
+        var queryType = schema.AllTypes["Query"].ShouldBeAssignableTo<IComplexGraphType>();
+        var fieldType = queryType.Fields.Find("hello").ShouldNotBeNull();
+        var resolver = fieldType.Resolver.ShouldNotBeNull();
+        var result = await resolver.ResolveAsync(new ResolveFieldContext()).ConfigureAwait(false);
+        result.ShouldBe("World");
+    }
+
+    [GraphQLMetadata(Name = "Query")]
+    private class QueryType : BaseType
+    {
+    }
+
+    private abstract class BaseType
+    {
+        public string Hello() => "World";
+    }
+}
