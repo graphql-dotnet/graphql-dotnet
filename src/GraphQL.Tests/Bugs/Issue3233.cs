792177c8c143df23ee6de836da392c4421d162c7
Ivan Maximov <sungam3r@yandex.ru>
2022-07-31 10:04:58 +0300

    Demonstrate issue 3233

---
+++ src/GraphQL.Tests/Bugs/Issue3233.cs
@@ -0,0 +1,22 @@
+namespace GraphQL.Tests.Bugs;
+
+public class Issue3233
+{
+    [Fact]
+    public void Throws_NotSupportedException_On_Object_Key()
+    {
+        var serializer = new SystemTextJson.GraphQLSerializer();
+        var result = new ExecutionResult();
+        result.AddError(new ExecutionError("oops1"));
+        var serialized = serializer.Serialize(result);
+        serialized.ShouldBe("{\"errors\":[{\"message\":\"oops1\"}]}");
+
+        result = new ExecutionResult();
+        var errorWithData = new ExecutionError("oops2");
+        errorWithData.Data.Add(new object(), "WOW");
+        result.AddError(errorWithData);
+
+        var ex = Should.Throw<NotSupportedException>(() => serializer.Serialize(result));
+        ex.Message.ShouldBe("The type 'System.Object' is not a supported dictionary key using converter of type 'System.Text.Json.Serialization.Converters.ObjectConverter'. Path: $.");
+    }
+}
