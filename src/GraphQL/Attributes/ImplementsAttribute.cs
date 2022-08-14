57dde499d36a7be9457dc4787afed64448fa503a
Shane Krueger <Shane@acdmail.com>
2022-08-14 13:12:02 -0400

    File-scoped namespaces

--- src/GraphQL/Attributes/ImplementsAttribute.cs
+++ src/GraphQL/Attributes/ImplementsAttribute.cs
@@ -1,45 +1,44 @@
 using GraphQL.Types;
 
-namespace GraphQL
+namespace GraphQL;
+
+/// <summary>
+/// Marks the type as implementing a specified interface.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = true)]
+public class ImplementsAttribute : GraphQLAttribute
 {
     /// <summary>
     /// Marks the type as implementing a specified interface.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = true)]
-    public class ImplementsAttribute : GraphQLAttribute
+    /// <param name="type">Can be a interface CLR type or an interface graph type.</param>
+    public ImplementsAttribute(Type type)
     {
-        /// <summary>
-        /// Marks the type as implementing a specified interface.
-        /// </summary>
-        /// <param name="type">Can be a interface CLR type or an interface graph type.</param>
-        public ImplementsAttribute(Type type)
+        if (typeof(IInterfaceGraphType).IsAssignableFrom(type))
         {
-            if (typeof(IInterfaceGraphType).IsAssignableFrom(type))
-            {
-                InterfaceGraphType = type;
-            }
-            else if (type.IsInterface)
-            {
-                InterfaceGraphType = typeof(GraphQLClrOutputTypeReference<>).MakeGenericType(type);
-            }
-            else
-            {
-                throw new ArgumentOutOfRangeException(nameof(type), "Can be a interface CLR type or an interface graph type.");
-            }
+            InterfaceGraphType = type;
         }
-
-        /// <summary>
-        /// Returns the interface graph type that this graph type implements.
-        /// </summary>
-        public Type InterfaceGraphType { get; }
-
-        /// <inheritdoc/>
-        public override void Modify(IGraphType graphType)
+        else if (type.IsInterface)
         {
-            if (graphType is not IImplementInterfaces graphType2)
-                throw new InvalidOperationException($"The {nameof(ImplementsAttribute)} can only be applied to graph types that implement {nameof(IImplementInterfaces)}.");
-
-            graphType2.Interfaces.Add(InterfaceGraphType);
+            InterfaceGraphType = typeof(GraphQLClrOutputTypeReference<>).MakeGenericType(type);
         }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(type), "Can be a interface CLR type or an interface graph type.");
+        }
+    }
+
+    /// <summary>
+    /// Returns the interface graph type that this graph type implements.
+    /// </summary>
+    public Type InterfaceGraphType { get; }
+
+    /// <inheritdoc/>
+    public override void Modify(IGraphType graphType)
+    {
+        if (graphType is not IImplementInterfaces graphType2)
+            throw new InvalidOperationException($"The {nameof(ImplementsAttribute)} can only be applied to graph types that implement {nameof(IImplementInterfaces)}.");
+
+        graphType2.Interfaces.Add(InterfaceGraphType);
     }
 }
