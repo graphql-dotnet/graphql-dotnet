3272b1503e3138d601dc1d97b020142556984914
Shane32 <shane@acdmail.com>
2022-11-21 13:02:04 -0500

    Update

--- src/GraphQL.Tests/Types/InterfaceTests.cs
+++ src/GraphQL.Tests/Types/InterfaceTests.cs
@@ -7,10 +7,46 @@ public class InterfaceTests : QueryTestBase<InterfaceTests.MySchema>
     [Fact]
     public void InterfaceWithNoObject()
     {
-        AssertQuerySuccess("{ test { id } }", """
+        AssertQuerySuccess(
+            """
             {
-                "test": {
+                noObjectTest {
+                    id                    # as there is no object type defined for this
+                                          # result, the interface resolver will be used
+                                          # to return "123_interface"
+                }
+            }
+            """,
+            """
+            {
+                "noObjectTest": {
                     "id": "123"
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public void InterfaceWithObject()
+    {
+        AssertQuerySuccess(
+            """
+            {
+                objectTest {
+                    ... on MyObject2 {
+                        idOnMyObject2: id   # returns "234"
+                    }
+                    idOnInterface: id       # because MyObject2 implements the interface,
+                                            # it will be resolved by MyObject2 and return
+                                            # "234" instead of "234_interface"
+                }
+            }
+            """,
+            """
+            {
+                "objectTest": {
+                    "idOnMyObject2": "234",
+                    "idOnInterface": "234"
                 }
             }
             """);
@@ -21,6 +57,7 @@ public class InterfaceTests : QueryTestBase<InterfaceTests.MySchema>
         public MySchema()
         {
             Query = new MyQuery();
+            RegisterType(new MyObject2GraphType());
         }
     }
 
@@ -28,7 +65,27 @@ public class InterfaceTests : QueryTestBase<InterfaceTests.MySchema>
     {
         public MyQuery()
         {
-            Field<NonNullGraphType<MyInterfaceType>>("test", resolve: _ => new MyObject());
+            Field<NonNullGraphType<MyInterfaceType>>("noObjectTest", resolve: _ => new MyObject());
+            Field<MyInterfaceType2>("objectTest", resolve: _ => new MyObject2());
+        }
+    }
+
+    public class MyInterfaceType2 : InterfaceGraphType<MyObject2>
+    {
+        public MyInterfaceType2()
+        {
+            Field<StringGraphType>("id")
+                .Resolve(ctx => ctx.Source.Id + "_interface");
+        }
+    }
+
+    public class MyObject2GraphType : ObjectGraphType<MyObject2>
+    {
+        public MyObject2GraphType()
+        {
+            Name = "MyObject2";
+            Field<StringGraphType>("id");
+            Interface<MyInterfaceType2>();
         }
     }
 
@@ -36,12 +93,18 @@ public class InterfaceTests : QueryTestBase<InterfaceTests.MySchema>
     {
         public MyInterfaceType()
         {
-            Field("id", x => x.Id);
+            Field("id", x => x.Id)
+                .Resolve(ctx => ctx.Source.Id + "_interface");
         }
     }
 
     public class MyObject
     {
         public string Id { get; set; } = "123";
+    }
+
+    public class MyObject2
+    {
+        public string Id { get; set; } = "234";
     }
 }
