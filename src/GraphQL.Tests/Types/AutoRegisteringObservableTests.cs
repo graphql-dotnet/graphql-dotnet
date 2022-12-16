03f5a0f22a0a142d481e3fd2719326ea758ca283
Shane Krueger <Shane@acdmail.com>
2022-12-16 09:28:11 -0500

    Update

--- src/GraphQL.Tests/Types/AutoRegisteringObservableTests.cs
+++ src/GraphQL.Tests/Types/AutoRegisteringObservableTests.cs
@@ -3,7 +3,9 @@
 
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
+#if NET5_0_OR_GREATER
 using System.Security.Claims;
+#endif
 using GraphQL.Execution;
 using GraphQL.Resolvers;
 using GraphQL.Types;
