089b00f19c4498aee259a1134d3a436a9e539025
Shane32 <shane@acdmail.com>
2022-12-30 12:40:35 -0500

    Update

--- src/GraphQL/Instrumentation/TelemetryOptions.cs
+++ src/GraphQL/Instrumentation/TelemetryOptions.cs
@@ -3,14 +3,16 @@
 namespace GraphQL.Instrumentation;
 
 /// <summary>
-/// Options for configuring the OpenTelemetry tracing instrumentation.
+/// Options for configuring the telemetry instrumentation.
 /// </summary>
 public class TelemetryOptions
 {
+    // see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/instrumentation/graphql.md
+
     /// <summary>
-    /// Indicates whether the GraphQL document is included in the OpenTelemetry activity.
+    /// Indicates whether the GraphQL document is included in the telemetry.
     /// </summary>
-    public bool RecordDocument { get; set; }
+    public bool RecordDocument { get; set; } = true; // recommended
 }
 
 #endif
