64df7225be296235b6d80148f702eb34814992f9
Ivan Maximov <sungam3r@yandex.ru>
2023-01-06 13:14:05 +0300

    Get rid of xunit serialization warnings

--- src/GraphQL.Tests/GraphQLSerializersTestData.cs
+++ src/GraphQL.Tests/GraphQLSerializersTestData.cs
@@ -1,23 +1,72 @@
 using System.Collections;
+using Xunit.Abstractions;
 
 namespace GraphQL.Tests;
 
+#nullable enable
+
 public class GraphQLSerializersTestData : IEnumerable<object[]>
 {
+    // See https://github.com/xunit/xunit/issues/1473
+    // Without this wrapper test output is cluttered with tons of messages like that:
+    // GraphQL.Tests: Non-serializable data ('System.Object[]') found for 'GraphQL.Tests.Extensions.GraphQLExtensionsTests.ToAST_Test'; falling back to single test case.
+    private class Wrapper : IGraphQLTextSerializer, IXunitSerializable
+    {
+        private IGraphQLTextSerializer _serializer = null!;
+
+        public Wrapper()
+        {
+        }
+
+        public Wrapper(IGraphQLTextSerializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public string _Type => _serializer!.GetType().Namespace!.Split('.').Last();
+        public bool IsNativelyAsync => _serializer.IsNativelyAsync;
+        public T? Deserialize<T>(string? value) => _serializer.Deserialize<T>(value);
+        public ValueTask<T?> ReadAsync<T>(Stream stream, CancellationToken cancellationToken = default) => _serializer.ReadAsync<T>(stream, cancellationToken);
+        public T? ReadNode<T>(object? value) => _serializer.ReadNode<T>(value);
+        public string Serialize<T>(T? value) => _serializer.Serialize<T>(value);
+        public Task WriteAsync<T>(Stream stream, T? value, CancellationToken cancellationToken = default) => _serializer.WriteAsync<T>(stream, value, cancellationToken);
+
+        public void Deserialize(IXunitSerializationInfo info)
+        {
+            string type = info.GetValue<string>("type");
+
+            if (type == "GraphQL.NewtonsoftJson.GraphQLSerializer")
+                _serializer = CreateNSJ();
+            else if (type == "GraphQL.SystemTextJson.GraphQLSerializer")
+                _serializer = CreateSTJ();
+            else
+                throw new NotSupportedException("Unknows serializer: " + type);
+        }
+
+        public void Serialize(IXunitSerializationInfo info)
+        {
+            info.AddValue("type", _serializer.GetType().FullName);
+        }
+    }
+
     public static readonly List<IGraphQLTextSerializer> AllWriters = new()
     {
-        new SystemTextJson.GraphQLSerializer(new System.Text.Json.JsonSerializerOptions
-        {
-            WriteIndented = true,
-            PropertyNameCaseInsensitive = true,
-            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping // less strict about what is encoded into \uXXXX
-        }),
-        new NewtonsoftJson.GraphQLSerializer(settings =>
-        {
-            settings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            settings.Converters.Add(new NewtonsoftJson.FixPrecisionConverter(true, true, true));
-        })
+        new Wrapper(CreateNSJ()),
+        new Wrapper(CreateSTJ())
     };
+
+    private static IGraphQLTextSerializer CreateNSJ() => new NewtonsoftJson.GraphQLSerializer(settings =>
+    {
+        settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+        settings.Converters.Add(new NewtonsoftJson.FixPrecisionConverter(true, true, true));
+    });
+
+    private static IGraphQLTextSerializer CreateSTJ() => new SystemTextJson.GraphQLSerializer(new System.Text.Json.JsonSerializerOptions
+    {
+        WriteIndented = true,
+        PropertyNameCaseInsensitive = true,
+        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping // less strict about what is encoded into \uXXXX
+    });
 
     public IEnumerator<object[]> GetEnumerator()
     {
