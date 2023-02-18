8c868273889f22045eb72be6563295e13487ac34
Shane Krueger <shane@acdmail.com>
2023-02-18 11:44:55 -0500

    Apply suggestions from code review
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/GraphQL.MemoryCache/AutomaticPersistedQueries/PersistedQueryNotFoundError.cs
+++ src/GraphQL.MemoryCache/AutomaticPersistedQueries/PersistedQueryNotFoundError.cs
@@ -10,16 +10,13 @@ public class PersistedQueryNotFoundError : RequestError
     /// <summary>
     /// Initializes a new instance.
     /// </summary>
-#pragma warning disable RCS1163 // Unused parameter.
-#pragma warning disable IDE0060 // Remove unused parameter.
     public PersistedQueryNotFoundError(string hash)
-#pragma warning restore IDE0060 // Remove unused parameter.
-#pragma warning restore RCS1163 // Unused parameter.
         : base("PersistedQueryNotFound")
     {
         // note: the message "PersistedQueryNotFound" is defined by the spec to be
         // returned when the hash cannot be found in the local cache
 
         // https://github.com/apollographql/apollo-link-persisted-queries
+        this.AddExtension("reason", $"Persisted query with hash '{hash}' was not found.");
     }
 }
--- src/GraphQL.MemoryCache/AutomaticPersistedQueries/PersistedQueryUnsupportedVersionError.cs
+++ src/GraphQL.MemoryCache/AutomaticPersistedQueries/PersistedQueryUnsupportedVersionError.cs
@@ -10,11 +10,7 @@ public class PersistedQueryUnsupportedVersionError : RequestError
     /// <summary>
     /// Initializes a new instance.
     /// </summary>
-#pragma warning disable RCS1163 // Unused parameter.
-#pragma warning disable IDE0060 // Remove unused parameter.
     public PersistedQueryUnsupportedVersionError(string? version)
-#pragma warning restore IDE0060 // Remove unused parameter.
-#pragma warning restore RCS1163 // Unused parameter.
         : base("PersistedQueryNotSupported")
     {
         // note: the message "PersistedQueryNotSupported" is defined by the spec
