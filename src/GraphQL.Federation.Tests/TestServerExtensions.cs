c810c3c66051d4cd86ccde4683089556e4136488
Shane32 <shane@acdmail.com>
2023-07-12 12:31:15 -0400

    Update

--- src/GraphQL.Federation.Tests/TestServerExtensions.cs
+++ src/GraphQL.Federation.Tests/TestServerExtensions.cs
@@ -2,7 +2,6 @@ using System.Net;
 using System.Text;
 using System.Text.Json;
 using GraphQL.SystemTextJson;
-using GraphQL.Transport;
 using Microsoft.AspNetCore.TestHost;
 
 namespace GraphQL.Federation.Tests;
@@ -10,50 +9,47 @@ namespace GraphQL.Federation.Tests;
 public static class TestServerExtensions
 {
     private static readonly GraphQLSerializer _serializer = new();
-    private static readonly JsonSerializerOptions _formattedOptions = new() { WriteIndented = true };
+    private static readonly JsonSerializerOptions _serializerOptions = new() { WriteIndented = true };
 
-    public static Task<string> ExecuteGraphQLRequest(this TestServer server, string url, string query, object? variables = null)
+    public static async Task<string> ExecuteGraphQLRequest(this TestServer server, string url, string query, object? variables = null)
     {
-        var request = new GraphQLRequest
-        {
-            Query = query,
-            Variables = GetVariables(variables),
-        };
-        return server.ExecuteGraphQLRequest(url, request);
-
-        static Inputs? GetVariables(object? variables)
-        {
-            if (variables is null)
-                return null;
-
-            if (variables is string str)
-                return _serializer.Deserialize<Inputs>(str);
-
-            // convert anonymous object to Inputs dictionary
-            return _serializer.Deserialize<Inputs>(_serializer.Serialize(variables));
-        }
-    }
-
-    public static async Task<string> ExecuteGraphQLRequest(this TestServer server, string url, GraphQLRequest request)
-    {
+        // create url
         var uriBuilder = new UriBuilder(server.BaseAddress)
         {
             Path = url
         };
         url = uriBuilder.Uri.ToString();
 
-        string json = _serializer.Serialize(request);
+        // create request
+        var request = new
+        {
+            query,
+            variables = variables switch
+            {
+                null => (object?)null,
+                // interpret a string as a json object
+                string str => JsonSerializer.Deserialize<JsonElement>(str),
+                _ => variables,
+            },
+        };
+        string json = _serializer.Serialize(request); // support Inputs for variables
         var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        // create client and send request
         using var client = server.CreateClient();
         using var response = await client
             .PostAsync(url, content)
             .ConfigureAwait(false);
+
+        // validate response
         if (response.StatusCode != HttpStatusCode.OK)
         {
             throw new InvalidOperationException($"GraphQL request failed with status code {response.StatusCode}.");
         }
         using var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+        // reformat response as an indented json string
         var ret = await JsonSerializer.DeserializeAsync<JsonElement>(stream).ConfigureAwait(false);
-        return JsonSerializer.Serialize(ret, _formattedOptions);
+        return JsonSerializer.Serialize(ret, _serializerOptions);
     }
 }
