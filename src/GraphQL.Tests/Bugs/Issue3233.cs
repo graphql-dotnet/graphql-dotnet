729c9243cdc21851554419d0ed3e260e793de147
Shane32 <shane@acdmail.com>
2023-11-21 13:15:09 -0500

    Update

--- src/GraphQL.Tests/Bugs/Issue3233.cs
+++ src/GraphQL.Tests/Bugs/Issue3233.cs
@@ -23,6 +23,7 @@ public class Issue3233
         {
             "The type 'System.Object' is not a supported Dictionary key type. Path: $.",
             "The type 'System.Object' is not a supported dictionary key using converter of type 'System.Text.Json.Serialization.Converters.ObjectConverter'. Path: $.",
+            "The type 'System.Object' is not a supported dictionary key using converter of type 'System.Text.Json.Serialization.Converters.DefaultObjectConverter'. Path: $.",
             "The collection type 'System.Collections.ListDictionaryInternal' is not supported."
         };
         if (!messages.Contains(ex.Message))
