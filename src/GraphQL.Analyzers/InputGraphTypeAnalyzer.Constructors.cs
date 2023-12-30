a4bdee584d4e0f31cbcbed4c6b60b832de52b42c
Artur Gordashnikov <gao.artur@gmail.com>
2023-12-30 21:23:50 +0200

    Extend GQL010 rule for `AutoRegisteringInputObjectGraphType` (#3865)
    * Analyze source type constructor for AutoRegisteringInputObjectGraphType
    
    * Update docs

--- src/GraphQL.Analyzers/InputGraphTypeAnalyzer.Constructors.cs
+++ src/GraphQL.Analyzers/InputGraphTypeAnalyzer.Constructors.cs
@@ -23,16 +23,36 @@ public partial class InputGraphTypeAnalyzer
         BaseTypeDeclarationSyntax inputObjectDeclarationSyntax,
         ITypeSymbol sourceTypeSymbol)
     {
+        if (!AnalyzeSourceTypeConstructors(sourceTypeSymbol))
+        {
+            var location = FindSourceSymbolUsageLocation(context, inputObjectDeclarationSyntax, sourceTypeSymbol);
+            ReportConstructorDiagnostic(context, sourceTypeSymbol, location);
+        }
+    }
+
+    private static void AnalyzeSourceTypeConstructors(
+        SyntaxNodeAnalysisContext context,
+        GenericNameSyntax autoRegisteringInputGenericName,
+        ITypeSymbol sourceTypeSymbol)
+    {
+        if (!AnalyzeSourceTypeConstructors(sourceTypeSymbol))
+        {
+            var location = FindSourceSymbolUsageLocation(context, autoRegisteringInputGenericName, sourceTypeSymbol);
+            ReportConstructorDiagnostic(context, sourceTypeSymbol, location);
+        }
+    }
+
+    private static bool AnalyzeSourceTypeConstructors(ITypeSymbol sourceTypeSymbol)
+    {
         // MyInput<SourceType> : InputObjectGraphType<SourceType>
         if (sourceTypeSymbol is ITypeParameterSymbol)
         {
-            return;
+            return true;
         }
 
         if (sourceTypeSymbol.IsAbstract)
         {
-            ReportConstructorDiagnostic();
-            return;
+            return false;
         }
 
         var constructors = sourceTypeSymbol
@@ -48,58 +68,69 @@ public partial class InputGraphTypeAnalyzer
         // no public constructor
         if (constructors.Count == 0)
         {
-            ReportConstructorDiagnostic();
-            return;
+            return false;
         }
 
         // single public or implicit default constructor
         if (constructors.Count == 1)
         {
-            return;
+            return true;
         }
 
         // explicit default constructor
         if (constructors.Any(ctor => ctor.Parameters.Length == 0))
         {
-            return;
+            return true;
         }
 
         // one constructor with [GraphQLConstructor] attribute
         if (constructors.Count(HasGraphQLAttribute) == 1)
         {
-            return;
+            return true;
         }
 
         // no default constructors and (no [GraphQLConstructor] attribute, or multiple [GraphQLConstructor] attributes)
-        ReportConstructorDiagnostic();
-        return;
-
-        void ReportConstructorDiagnostic()
-        {
-            var location = FindSourceSymbolUsageLocation();
-            if (location != null)
-            {
-                context.ReportDiagnostic(Diagnostic.Create(
-                    CanNotResolveInputSourceTypeConstructor,
-                    location,
-                    (sourceTypeSymbol as INamedTypeSymbol)?.Name));
-            }
-        }
-
-        // the location of the 'SourceType' in InputObjectGraphType<SourceType>
-        Location? FindSourceSymbolUsageLocation() =>
-            inputObjectDeclarationSyntax.BaseList
-                ?.Types
-                .Select(baseTypeSyntax => baseTypeSyntax.Type)
-                .OfType<GenericNameSyntax>()
-                .SelectMany(generic => generic.TypeArgumentList.Arguments)
-                .FirstOrDefault(arg =>
-                {
-                    var argType = context.SemanticModel.GetTypeInfo(arg).Type;
-                    return argType != null && SymbolEqualityComparer.Default.Equals(argType, sourceTypeSymbol);
-                })
-                ?.GetLocation();
+        return false;
     }
+
+    private static void ReportConstructorDiagnostic(
+        SyntaxNodeAnalysisContext context,
+        ITypeSymbol sourceTypeSymbol,
+        Location? location)
+    {
+        if (location != null)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(
+                CanNotResolveInputSourceTypeConstructor,
+                location,
+                (sourceTypeSymbol as INamedTypeSymbol)?.Name));
+        }
+    }
+
+    private static Location? FindSourceSymbolUsageLocation(
+        SyntaxNodeAnalysisContext context,
+        BaseTypeDeclarationSyntax inputObjectDeclarationSyntax,
+        ITypeSymbol sourceTypeSymbol) =>
+        inputObjectDeclarationSyntax.BaseList
+            ?.Types
+            .Select(baseTypeSyntax => baseTypeSyntax.Type)
+            .OfType<GenericNameSyntax>()
+            .Select(genericName => FindSourceSymbolUsageLocation(context, genericName, sourceTypeSymbol))
+            .FirstOrDefault(location => location != null);
+
+    // the location of the 'SourceType' in InputObjectGraphType<SourceType>
+    private static Location? FindSourceSymbolUsageLocation(
+        SyntaxNodeAnalysisContext context,
+        GenericNameSyntax genericName,
+        ITypeSymbol sourceTypeSymbol) =>
+        genericName
+            .TypeArgumentList.Arguments
+            .FirstOrDefault(arg =>
+            {
+                var argType = context.SemanticModel.GetTypeInfo(arg).Type;
+                return argType != null && SymbolEqualityComparer.Default.Equals(argType, sourceTypeSymbol);
+            })
+            ?.GetLocation();
 
     private static bool HasGraphQLAttribute(IMethodSymbol constructor) =>
         constructor
