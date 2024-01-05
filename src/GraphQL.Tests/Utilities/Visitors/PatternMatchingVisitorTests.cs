182f20172fb9fa8f03feb717d11be5b57fc394e7
Shane32 <shane@acdmail.com>
2024-01-05 14:15:48 -0500

    Add Regex directive and DirectiveAttribute

---
+++ src/GraphQL.Tests/Utilities/Visitors/PatternMatchingVisitorTests.cs
@@ -0,0 +1,62 @@
+using GraphQL.Attributes;
+using GraphQL.Types;
+using GraphQL.Utilities.Visitors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQL.Tests.Utilities.Visitors;
+
+public class PatternMatchingVisitorTests
+{
+    [Fact]
+    public async Task UppercaseRegex()
+    {
+        var services = new ServiceCollection();
+        services.AddGraphQL(b => b
+            .AddAutoSchema<Query>()
+            .ConfigureSchema(s =>
+            {
+                s.Directives.Register(new PatternMatchingDirective());
+                s.RegisterVisitor(new PatternMatchingVisitor());
+            }));
+
+        using var provider = services.BuildServiceProvider();
+        var schema = provider.GetRequiredService<ISchema>();
+
+        var result = await schema.ExecuteAsync(o => o.Query = """{ hello(arg: "HELLO") }""");
+        result.ShouldBeSimilarTo("""{"data":{"hello":"HELLO"}}""");
+
+        result = await schema.ExecuteAsync(o => o.Query = """{ hello(arg: "hello") }""");
+        result.ShouldBeSimilarTo(
+            """
+            {
+                "errors": [
+                    {
+                        "message": "Invalid value for argument \u0027arg\u0027 of field \u0027hello\u0027. Value 'hello' does not match the regex pattern '[A-Z]+'.",
+                        "locations": [
+                            {
+                                "line": 1,
+                                "column": 14
+                            }
+                        ],
+                        "extensions": {
+                            "code": "INVALID_VALUE",
+                            "codes": [
+                                "INVALID_VALUE",
+                                "ARGUMENT"
+                            ],
+                            "number": "5.6"
+                        }
+                    }
+                ]
+            }
+            """);
+    }
+
+    private class Query
+    {
+        public static string Hello(
+            [Directive("pattern", "regex", "[A-Z]+")] // uppercase only
+            string arg)
+            => arg;
+    }
+}
---
+++ src/GraphQL/Utilities/Visitors/Custom/PatternMatchingVisitor.cs
@@ -0,0 +1,67 @@
+using System.Text.RegularExpressions;
+using GraphQL.Types;
+
+namespace GraphQL.Utilities.Visitors;
+
+/// <summary>
+/// A schema visitor that adds validation for the <see cref="PatternMatchingDirective"/> directive.
+/// Use as follows to enable validation of arguments and input fields:
+/// <code>
+/// services.AddGraphQL(b => b
+///     .AddSchema&lt;MySchema&gt;()
+///     .ConfigureSchema(s =>
+///     {
+///         s.Directives.Register(new <see cref="PatternMatchingDirective"/>());
+///         s.RegisterVisitor(new <see cref="PatternMatchingVisitor"/>());
+///     }));
+///
+/// field.Field(x => x.Email)
+///     .ApplyDirective("pattern", "regex", @".+\@.+\..+");
+/// </code>
+/// </summary>
+public class PatternMatchingVisitor : BaseSchemaNodeVisitor
+{
+    /// <inheritdoc/>
+    public override void VisitInputObjectFieldDefinition(FieldType field, IInputObjectGraphType type, ISchema schema)
+    {
+        // look for @pattern directive applied to the field, and if found, use Validate to set the validation method
+        var applied = field.FindAppliedDirective("pattern");
+        if (applied?.FindArgument("regex")?.Value is not string regex)
+            return;
+
+        // compile the regex
+        var regexObject = new Regex($"^{regex}$",
+            GlobalSwitches.DynamicallyCompileToObject ? RegexOptions.Compiled : RegexOptions.None);
+
+        // set the validation method
+        field.Validator += (value) =>
+        {
+            if (value is string stringValue && !regexObject.IsMatch(stringValue))
+            {
+                throw new ArgumentException($"Value '{stringValue}' does not match the regex pattern '{regex}'.");
+            }
+        };
+    }
+
+    /// <inheritdoc/>
+    public override void VisitObjectFieldArgumentDefinition(QueryArgument argument, FieldType field, IObjectGraphType type, ISchema schema)
+    {
+        // look for @pattern directive applied to the field, and if found, use Validate to set the validation method
+        var applied = argument.FindAppliedDirective("pattern");
+        if (applied?.FindArgument("regex")?.Value is not string regex)
+            return;
+
+        // compile the regex
+        var regexObject = new Regex(regex,
+            GlobalSwitches.DynamicallyCompileToObject ? RegexOptions.Compiled : RegexOptions.None);
+
+        // set the validation method
+        argument.Validator += (value) =>
+        {
+            if (value is string stringValue && !regexObject.IsMatch(stringValue))
+            {
+                throw new ArgumentException($"Value '{stringValue}' does not match the regex pattern '{regex}'.");
+            }
+        };
+    }
+}
