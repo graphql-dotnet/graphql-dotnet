453c2a03adfcb38d35f470053c6fd448258b5dd3
Shane Krueger <shane@acdmail.com>
2024-06-04 08:51:40 -0400

    Add another Modify overload to GraphQLAttribute (#3952)
    * Add another Modify overload to GraphQLAttribute
    
    * Update API approvals

--- src/GraphQL/Types/Composite/AutoRegisteringObjectGraphType.cs
+++ src/GraphQL/Types/Composite/AutoRegisteringObjectGraphType.cs
@@ -103,7 +103,7 @@ namespace GraphQL.Types
         /// May return <see langword="null"/> to skip a member.
         /// </summary>
         protected virtual FieldType? CreateField(MemberInfo memberInfo)
-            => AutoRegisteringHelper.CreateField(memberInfo, GetTypeInformation, BuildFieldType, false);
+            => AutoRegisteringHelper.CreateField(this, memberInfo, GetTypeInformation, BuildFieldType, false);
 
         /// <inheritdoc cref="AutoRegisteringOutputHelper.BuildFieldType(MemberInfo, FieldType, Func{MemberInfo, LambdaExpression}, Func{Type, Func{FieldType, ParameterInfo, ArgumentInformation}}, Action{ParameterInfo, QueryArgument})"/>
         protected void BuildFieldType(FieldType fieldType, MemberInfo memberInfo)
