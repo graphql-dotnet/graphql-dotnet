8549ff588622cc005c8f838fc86b87f906d3befc
Shane32 <shane@acdmail.com>
2024-06-05 03:36:29 -0400

    Fix tests

--- src/GraphQL.Tests/Introspection/SchemaIntrospectionTests.cs
+++ src/GraphQL.Tests/Introspection/SchemaIntrospectionTests.cs
@@ -28,6 +28,7 @@ public class SchemaIntrospectionTests
             _.Query = "IntrospectionQuery".ReadGraphQLRequest();
         });
         var json = serializer.Serialize(executionResult);
+        json = json.Replace("\\r\\n", "\\n");
         json.ShouldMatchApproved(o => o.NoDiff().WithStringCompareOptions(StringCompareShould.IgnoreLineEndings));
     }
 
