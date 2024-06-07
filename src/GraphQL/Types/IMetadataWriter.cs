bc7d31b32cf2fe5251521645e21ab4d6f4cba14d
Shane32 <shane@acdmail.com>
2024-06-07 13:18:49 -0400

    Fix period

--- src/GraphQL/Types/IMetadataWriter.cs
+++ src/GraphQL/Types/IMetadataWriter.cs
@@ -2,7 +2,7 @@ namespace GraphQL.Types;
 
 /// <summary>
 /// Provides basic capabilities for getting and setting arbitrary meta information.
-/// This interface is implemented by field builders, field types, graph types and schemas for configuring metadata,
+/// This interface is implemented by field builders, field types, graph types and schemas for configuring metadata.
 /// </summary>
 public interface IMetadataWriter : IProvideMetadata
 {
