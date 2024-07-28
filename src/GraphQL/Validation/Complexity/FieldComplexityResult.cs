b2f0e26ea4831043ab815327bd1fabfc988f5aef
Shane Krueger <shane@acdmail.com>
2024-07-28 12:22:26 -0400

    Update

--- src/GraphQL/Validation/Complexity/FieldComplexityResult.cs
+++ src/GraphQL/Validation/Complexity/FieldComplexityResult.cs
@@ -18,11 +18,11 @@ public struct FieldComplexityResult
     /// The field impact to add to the total complexity.
     /// Note that this value is multiplied by the current child impact modifier at this level of the document.
     /// </summary>
-    public double FieldImpact;
+    public double FieldImpact { get; set; }
 
     /// <summary>
     /// The child impact multiplier to apply to the child fields of this field.
     /// This value is multiplicatively applied to the child impact mulriplier of the parent field.
     /// </summary>
-    public double ChildImpactMultiplier;
+    public double ChildImpactMultiplier { get; set; }
 }
