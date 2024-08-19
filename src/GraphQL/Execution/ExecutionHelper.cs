30112beb11922299ac7689c8e9bb0d58e70e47a7
Shane32 <shane@acdmail.com>
2024-08-19 12:39:48 -0400

    Immutable optimization for directives without arguments

--- src/GraphQL/Execution/ExecutionHelper.cs
+++ src/GraphQL/Execution/ExecutionHelper.cs
@@ -1,4 +1,6 @@
+#if !NET5_0_OR_GREATER
 using System.Collections.ObjectModel;
+#endif
 using GraphQL.Types;
 using GraphQL.Validation;
 using GraphQL.Validation.Errors;
@@ -12,7 +14,12 @@ namespace GraphQL.Execution;
 /// </summary>
 public static class ExecutionHelper
 {
-    private static readonly IDictionary<string, ArgumentValue> _emptyDirectiveArguments = new ReadOnlyDictionary<string, ArgumentValue>(new Dictionary<string, ArgumentValue>());
+    private static readonly IDictionary<string, ArgumentValue> _emptyDirectiveArguments =
+#if NET5_0_OR_GREATER
+        System.Collections.Immutable.ImmutableDictionary<string, ArgumentValue>.Empty;
+#else
+        new ReadOnlyDictionary<string, ArgumentValue>(new Dictionary<string, ArgumentValue>());
+#endif
 
     /// <summary>
     /// Returns a dictionary of directives with their arguments values for a node (field, fragment spread, inline fragment).
