89192b3375f8a4cc928db320941b7b1129be38fc
Shane32 <shane@acdmail.com>
2024-08-25 11:46:20 -0400

    Update

--- src/GraphQL.Tests/Bugs/Bug4042.cs
+++ src/GraphQL.Tests/Bugs/Bug4042.cs
@@ -2,6 +2,7 @@ using GraphQL.Types;
 
 namespace GraphQL.Tests.Bugs;
 
+[Collection("StaticTests")]
 public class Bug4042
 {
     public class TestInput
@@ -10,48 +11,59 @@ public class Bug4042
     }
 
     [Theory]
-    [InlineData(true)]
-    [InlineData(false)]
-    public async Task Test1(bool withExpression)
+    [InlineData(true, false)]
+    [InlineData(false, false)]
+    [InlineData(true, true)]
+    [InlineData(false, true)]
+    public async Task Test1(bool withExpression, bool viaReflection)
     {
-        var inputGraphType = new InputObjectGraphType<TestInput>
+        var defaultCompile = GlobalSwitches.DynamicallyCompileToObject;
+        GlobalSwitches.DynamicallyCompileToObject = !viaReflection;
+        try
         {
-            Name = "TestInput",
-        };
-        if (withExpression)
-        {
-            inputGraphType.Field(x => x.NullableId, type: typeof(IdGraphType));
-        }
-        else
-        {
-            inputGraphType.Field<IdGraphType>("nullableId");
-        }
-
-        var queryType = new ObjectGraphType() { Name = "Query" };
-        queryType.Field<IdGraphType>("inputNullableId")
-            .Argument(inputGraphType, "input")
-            .Resolve(ctx =>
+            var inputGraphType = new InputObjectGraphType<TestInput>
             {
-                var input = ctx.GetArgument<TestInput>("input");
-                return input.NullableId;
-            });
+                Name = "TestInput",
+            };
+            if (withExpression)
+            {
+                inputGraphType.Field(x => x.NullableId, type: typeof(IdGraphType));
+            }
+            else
+            {
+                inputGraphType.Field<IdGraphType>("nullableId");
+            }
 
-        var schema = new Schema() { Query = queryType };
-        schema.Initialize();
+            var queryType = new ObjectGraphType() { Name = "Query" };
+            queryType.Field<IdGraphType>("inputNullableId")
+                .Argument(inputGraphType, "input")
+                .Resolve(ctx =>
+                {
+                    var input = ctx.GetArgument<TestInput>("input");
+                    return input.NullableId;
+                });
 
-        var query = "query { inputNullableId(input: { nullableId: \"1\" })}";
+            var schema = new Schema() { Query = queryType };
+            schema.Initialize();
 
-        var executer = new DocumentExecuter();
-        var executionOptions = new ExecutionOptions
-        {
-            Schema = schema,
-            Query = query
-        };
+            var query = "query { inputNullableId(input: { nullableId: \"1\" })}";
 
-        var result = await executer.ExecuteAsync(executionOptions);
-        var ret = new SystemTextJson.GraphQLSerializer().Serialize(result);
-        ret.ShouldBe("""
+            var executer = new DocumentExecuter();
+            var executionOptions = new ExecutionOptions
+            {
+                Schema = schema,
+                Query = query
+            };
+
+            var result = await executer.ExecuteAsync(executionOptions);
+            var ret = new SystemTextJson.GraphQLSerializer().Serialize(result);
+            ret.ShouldBe("""
             {"data":{"inputNullableId":"1"}}
             """, StringCompareShould.IgnoreLineEndings);
+        }
+        finally
+        {
+            GlobalSwitches.DynamicallyCompileToObject = defaultCompile;
+        }
     }
 }
