36db79ba3681f21229a2cbb0e403412f78320bb8
Shane Krueger <shane@acdmail.com>
2024-09-04 10:53:41 -0400

    Add attributes for Parser, Validator and ValidateArguments (#4052)
    * add
    
    * Add attributes for Parser or Validator or ValidateArguments
    
    * update migration notes
    
    * Update
    
    * Update src/GraphQL/Attributes/ValidatorAttribute.cs
    
    Co-authored-by: Artur Gordashnikov <gao.artur@gmail.com>
    
    * Update src/GraphQL/Attributes/ParserAttribute.cs
    
    Co-authored-by: Artur Gordashnikov <gao.artur@gmail.com>
    
    ---------
    
    Co-authored-by: Artur Gordashnikov <gao.artur@gmail.com>

---
+++ src/GraphQL/Attributes/ValidateArgumentsAttribute.cs
@@ -0,0 +1,75 @@
+using System.Reflection;
+using GraphQL.Types;
+using GraphQL.Validation;
+
+namespace GraphQL;
+
+/// <summary>
+/// Specifies a custom argument validation method for a field in a GraphQL schema.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class ValidateArgumentsAttribute : GraphQLAttribute
+{
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)]
+    private readonly Type? _validationType;
+    private readonly string _validationMethodName;
+    private readonly bool _includePrivate;
+
+    /// <summary>
+    /// Specifies a custom argument validation method for a field in a GraphQL schema using the specified validation method name.
+    /// The method must exist on the declaring type of the field and be declared as static.
+    /// The method may be private or public and must have the signature 'ValueTask <paramref name="validationMethodName"/>(FieldArgumentsValidationContext context)'.
+    /// </summary>
+    [RequiresUnreferencedCode("Please ensure the specified method is not trimmed or use an alternative constructor.")]
+    public ValidateArgumentsAttribute(string validationMethodName)
+    {
+        _validationMethodName = validationMethodName
+            ?? throw new ArgumentNullException(nameof(validationMethodName));
+        _includePrivate = true;
+    }
+
+    /// <summary>
+    /// Specifies a custom argument validation method for a field in a GraphQL schema using the 'ValidateArguments' method from a specified type.
+    /// The method must exist on the specified type and must be static and public.
+    /// The method must have the signature 'ValueTask ValidateArguments(FieldArgumentsValidationContext context)'.
+    /// </summary>
+    public ValidateArgumentsAttribute(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)]
+        Type validationType)
+    {
+        _validationType = validationType
+            ?? throw new ArgumentNullException(nameof(validationType));
+        _validationMethodName = nameof(FieldType.ValidateArguments);
+    }
+
+    /// <summary>
+    /// Specifies a custom argument validation method for a field in a GraphQL schema using the specified method from a specified type.
+    /// The method must exist on the specified type and must be static and public.
+    /// The method must have the signature 'ValueTask <paramref name="validationMethodName"/>(FieldArgumentsValidationContext context)'.
+    /// </summary>
+    public ValidateArgumentsAttribute(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)]
+        Type validationType, string validationMethodName)
+    {
+        _validationType = validationType ?? throw new ArgumentNullException(nameof(validationType));
+        _validationMethodName = validationMethodName ?? throw new ArgumentNullException(nameof(validationMethodName));
+    }
+
+    /// <inheritdoc/>
+    public override void Modify(FieldType fieldType, bool isInputType, IGraphType graphType, MemberInfo memberInfo, ref bool ignore)
+    {
+        if (isInputType)
+            return;
+        var validationType = _validationType ?? memberInfo.DeclaringType!;
+        var bindingFlags = BindingFlags.Public | BindingFlags.Static;
+        if (_includePrivate)
+            bindingFlags |= BindingFlags.NonPublic;
+#pragma warning disable IL2075 // UnrecognizedReflectionPattern
+        var method = validationType.GetMethod(_validationMethodName, bindingFlags, null, [typeof(FieldArgumentsValidationContext)], null)
+            ?? throw new InvalidOperationException($"Could not find method '{_validationMethodName}' on CLR type '{validationType.GetFriendlyName()}' while initializing '{graphType.Name}.{fieldType.Name}'. The method must have a single parameter of type {nameof(FieldArgumentsValidationContext)}.");
+#pragma warning restore IL2075 // UnrecognizedReflectionPattern
+        if (method.ReturnType != typeof(ValueTask))
+            throw new InvalidOperationException($"Method '{_validationMethodName}' on CLR type '{validationType.GetFriendlyName()}' must have a return type of ValueTask.");
+        fieldType.ValidateArguments = (Func<FieldArgumentsValidationContext, ValueTask>)method.CreateDelegate(typeof(Func<FieldArgumentsValidationContext, ValueTask>));
+    }
+}
