9d0741d248e397e8bdf92b0ded2fc68872ec395e
Shane Krueger <shane@acdmail.com>
2024-10-15 14:39:30 -0400

    Bugfix: Treat operation name with empty string as null (#4078)
    * Fix issue 4077
    
    * Update
    
    * update

---
+++ src/GraphQL.Tests/Bugs/Bug4078.cs
@@ -0,0 +1,37 @@
+using GraphQL.Types;
+
+namespace GraphQL.Tests.Bugs;
+
+// https://github.com/graphql-dotnet/graphql-dotnet/pull/4078
+public class Bug4078
+{
+    [Fact]
+    public async Task empty_string_for_operation_name_is_treated_as_null()
+    {
+        var schema = new Schema { Query = new QueryGraphType() };
+
+        var response = await schema.ExecuteAsync(_ =>
+        {
+            // to trigger the bug, the document must contain a named operation,
+            _.Query = "query abc { test }";
+            // along with an empty string for the operation name
+            _.OperationName = "";
+        });
+
+        response.ShouldBeCrossPlatJson("""
+            {
+                "data": {
+                    "test": "abc"
+                }
+            }
+            """);
+    }
+
+    public class QueryGraphType : ObjectGraphType
+    {
+        public QueryGraphType()
+        {
+            Field<StringGraphType>("test").Resolve(ctx => "abc");
+        }
+    }
+}
