c4115ede491270b0a14aacb0f328d3b4b698bb68
MrHPotter <bastiantoepfer@gmx.de>
2024-11-29 21:16:28 +0100

    Fix build Error about query in sample project being static

--- samples/GraphQL.Federation.TypeFirst.Sample4/Schema/Query.cs
+++ samples/GraphQL.Federation.TypeFirst.Sample4/Schema/Query.cs
@@ -1,6 +1,6 @@
 namespace GraphQL.Federation.TypeFirst.Sample4.Schema;
 
-public static class Query
+public class Query
 {
     public static Task<IEnumerable<User>> Users([FromServices] Data data)
         => data.GetUsersAsync();
