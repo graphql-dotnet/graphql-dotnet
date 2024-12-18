15a5ffbf426c3a8106e8c9921edf8542adeccf9f
Shane Krueger <shane@acdmail.com>
2024-12-18 10:17:59 -0500

    Auto-add transitively implemented interfaces to object and interface types (#4108)
    * Transitive interfaces
    
    * Update api approvals
    
    * update

---
+++ src/GraphQL.Tests/Utilities/Visitors/TransitiveInterfaceVisitorTests.cs
@@ -0,0 +1,111 @@
+using GraphQL.Types;
+using GraphQL.Utilities;
+
+namespace GraphQL.Tests.Utilities.Visitors;
+
+public class TransitiveInterfaceVisitorTests
+{
+    [Fact]
+    public void Should_Add_Transitive_Interfaces_To_Object_Type()
+    {
+        // Arrange
+        var schema = new Schema();
+
+        var interfaceC = new InterfaceGraphType { Name = "C" };
+        var interfaceB = new InterfaceGraphType { Name = "B" };
+        interfaceB.AddResolvedInterface(interfaceC);
+        var interfaceA = new InterfaceGraphType { Name = "A" };
+        interfaceA.AddResolvedInterface(interfaceB);
+
+        var objectType = new ObjectGraphType { Name = "TestObject" };
+        objectType.AddResolvedInterface(interfaceA);
+
+        // Act
+        TransitiveInterfaceVisitor.Instance.VisitObject(objectType, schema);
+
+        // Assert
+        objectType.ResolvedInterfaces.Count.ShouldBe(3);
+        objectType.ResolvedInterfaces.ShouldContain(interfaceA);
+        objectType.ResolvedInterfaces.ShouldContain(interfaceB);
+        objectType.ResolvedInterfaces.ShouldContain(interfaceC);
+    }
+
+    [Fact]
+    public void Should_Add_Transitive_Interfaces_To_Interface_Type()
+    {
+        // Arrange
+        var schema = new Schema();
+
+        var interfaceC = new InterfaceGraphType { Name = "C" };
+        var interfaceB = new InterfaceGraphType { Name = "B" };
+        interfaceB.AddResolvedInterface(interfaceC);
+        var interfaceA = new InterfaceGraphType { Name = "A" };
+        interfaceA.AddResolvedInterface(interfaceB);
+
+        // Act
+        TransitiveInterfaceVisitor.Instance.VisitInterface(interfaceA, schema);
+
+        // Assert
+        interfaceA.ResolvedInterfaces.Count.ShouldBe(2);
+        interfaceA.ResolvedInterfaces.ShouldContain(interfaceB);
+        interfaceA.ResolvedInterfaces.ShouldContain(interfaceC);
+    }
+
+    [Fact]
+    public void Should_Handle_No_Interfaces()
+    {
+        // Arrange
+        var schema = new Schema();
+        var objectType = new ObjectGraphType { Name = "TestObject" };
+
+        // Act
+        TransitiveInterfaceVisitor.Instance.VisitObject(objectType, schema);
+
+        // Assert
+        objectType.ResolvedInterfaces.Count.ShouldBe(0);
+    }
+
+    [Fact]
+    public void Should_Ignore_Circular_References()
+    {
+        // Arrange
+        var schema = new Schema();
+
+        var interfaceA = new InterfaceGraphType { Name = "A" };
+        var interfaceB = new InterfaceGraphType { Name = "B" };
+
+        interfaceA.AddResolvedInterface(interfaceB);
+        interfaceB.AddResolvedInterface(interfaceA);
+
+        // Act & Assert
+        TransitiveInterfaceVisitor.Instance.VisitInterface(interfaceA, schema);
+        interfaceA.ResolvedInterfaces.Count.ShouldBe(1);
+        interfaceA.ResolvedInterfaces.ShouldContain(interfaceB);
+    }
+
+    [Fact]
+    public void Should_Not_Add_Already_Implemented_Interfaces()
+    {
+        // Arrange
+        var schema = new Schema();
+
+        var interfaceC = new InterfaceGraphType { Name = "C" };
+        var interfaceB = new InterfaceGraphType { Name = "B" };
+        interfaceB.AddResolvedInterface(interfaceC);
+        var interfaceA = new InterfaceGraphType { Name = "A" };
+        interfaceA.AddResolvedInterface(interfaceB);
+
+        var objectType = new ObjectGraphType { Name = "TestObject" };
+        objectType.AddResolvedInterface(interfaceA);
+        objectType.AddResolvedInterface(interfaceC); // Already directly implementing C
+
+        // Act
+        TransitiveInterfaceVisitor.Instance.VisitObject(objectType, schema);
+
+        // Assert
+        objectType.ResolvedInterfaces.Count.ShouldBe(3);
+        objectType.ResolvedInterfaces.ShouldContain(interfaceA);
+        objectType.ResolvedInterfaces.ShouldContain(interfaceB);
+        objectType.ResolvedInterfaces.ShouldContain(interfaceC);
+    }
+}
---
+++ src/GraphQL/Utilities/Visitors/TransitiveInterfaceVisitor.cs
@@ -0,0 +1,67 @@
+using GraphQL.Types;
+
+namespace GraphQL.Utilities;
+
+/// <summary>
+/// A schema visitor that adds direct references to any transitively implemented interfaces
+/// that are not already directly implemented.
+/// </summary>
+public sealed class TransitiveInterfaceVisitor : BaseSchemaNodeVisitor
+{
+    private TransitiveInterfaceVisitor()
+    {
+    }
+
+    /// <inheritdoc cref="TransitiveInterfaceVisitor"/>
+    public static TransitiveInterfaceVisitor Instance { get; } = new();
+
+    /// <inheritdoc/>
+    public override void VisitObject(IObjectGraphType type, ISchema schema)
+    {
+        AddTransitiveInterfaces(type);
+    }
+
+    /// <inheritdoc/>
+    public override void VisitInterface(IInterfaceGraphType type, ISchema schema)
+    {
+        AddTransitiveInterfaces(type);
+    }
+
+    private void AddTransitiveInterfaces(IImplementInterfaces type)
+    {
+        if (type.ResolvedInterfaces.Count == 0)
+            return;
+
+        var checkedInterfaces = new HashSet<IInterfaceGraphType>();
+        var transitiveInterfaces = new HashSet<IInterfaceGraphType>();
+        FindTransitiveInterfaces(type, type, transitiveInterfaces, checkedInterfaces);
+
+        // Add any transitive interfaces that aren't already directly implemented
+        foreach (var iface in transitiveInterfaces)
+        {
+            if (!type.ResolvedInterfaces.Contains(iface))
+            {
+                type.AddResolvedInterface(iface);
+            }
+        }
+    }
+
+    private void FindTransitiveInterfaces(IImplementInterfaces baseType, IImplementInterfaces type, HashSet<IInterfaceGraphType> transitiveInterfaces, HashSet<IInterfaceGraphType> checkedInterfaces)
+    {
+        foreach (var iface in type.ResolvedInterfaces.List)
+        {
+            if (checkedInterfaces.Add(iface))
+            {
+                foreach (var transitiveInterface in iface.ResolvedInterfaces.List)
+                {
+                    // Ignore circular references (will be caught in SchemaValidationVisitor)
+                    if (transitiveInterface == baseType)
+                        continue;
+
+                    transitiveInterfaces.Add(transitiveInterface);
+                }
+                FindTransitiveInterfaces(baseType, iface, transitiveInterfaces, checkedInterfaces);
+            }
+        }
+    }
+}
