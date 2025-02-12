76f777c80c95c82c50b9031bf1a80869b4202bfa
Shane Krueger <shane@acdmail.com>
2025-02-12 16:18:44 -0500

    Update src/GraphQL.MemoryCache/MemoryDocumentCacheOptions.cs

--- src/GraphQL.MemoryCache/MemoryDocumentCacheOptions.cs
+++ src/GraphQL.MemoryCache/MemoryDocumentCacheOptions.cs
@@ -25,7 +25,7 @@ public class MemoryDocumentCacheOptions : MemoryCacheOptions, IOptions<MemoryDoc
     /// <summary>
     /// A delegate that supplies an extra value to be included in the cache key.
     /// When not configured, the extra value is computed from <see cref="ExecutionOptions.Schema"/> as follows:
-    /// The schema instance is used for schema-first and dynamic schemas (when the type <see cref="Types.Schema"/>),
+    /// The schema instance is used for schema-first and dynamic schemas (when the type is <see cref="Types.Schema"/>),
     /// and the schema type is used for type-first and code-first schemas (when the type is derived from <see cref="Types.Schema"/>).
     /// </summary>
     public Func<ExecutionOptions, object?>? AdditionalCacheKeySelector { get; set; }
