e1ef4da8c0fa42a9dbcefe55519974380de6d2e3
Shane Krueger <shane@acdmail.com>
2025-08-17 14:56:41 -0400

    Mark UniqueDirectivesPerLocation ctor as obsolete

--- src/GraphQL/Validation/Rules/5.7 - Directives/3. UniqueDirectivesPerLocation.cs
+++ src/GraphQL/Validation/Rules/5.7 - Directives/3. UniqueDirectivesPerLocation.cs
@@ -15,7 +15,15 @@ public class UniqueDirectivesPerLocation : ValidationRuleBase
     /// <summary>
     /// Returns a static instance of this validation rule.
     /// </summary>
+#pragma warning disable CS0618 // Type or member is obsolete
     public static readonly UniqueDirectivesPerLocation Instance = new();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+    /// <inheritdoc cref="UniqueDirectivesPerLocation"/>
+    [Obsolete("Please use the Instance property to retrieve a static instance. This constructor will be removed in v9.")]
+    public UniqueDirectivesPerLocation()
+    {
+    }
 
     /// <inheritdoc/>
     /// <exception cref="UniqueDirectivesPerLocationError"/>
