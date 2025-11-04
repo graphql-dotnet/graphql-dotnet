1703f6f3ceff23e96fb93f2d3836bb01e6d3bdf5
Shane Krueger <shane@acdmail.com>
2025-11-04 16:50:15 -0500

    Eliminate allocations during DataLoaderContext.GetOrAdd (#4204)
    * Eliminate allocations during DataLoaderContext.GetOrAdd
    
    * update
    
    * Simplify

--- src/GraphQL.DataLoader/DataLoaderContext.cs
+++ src/GraphQL.DataLoader/DataLoaderContext.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 namespace GraphQL.DataLoader;
 
 /// <summary>
@@ -5,7 +7,10 @@ namespace GraphQL.DataLoader;
 /// </summary>
 public class DataLoaderContext
 {
-    private readonly Dictionary<string, IDataLoader> _loaders = [];
+    private ConcurrentDictionary<string, IDataLoader>? _loaders;
+
+    private ConcurrentDictionary<string, IDataLoader> GetLoaders()
+        => LazyInitializer.EnsureInitialized(ref _loaders)!;
 
     /// <summary>
     /// Add a new data loader if one does not already exist with the provided key
@@ -23,18 +28,31 @@ public class DataLoaderContext
         if (dataLoaderFactory == null)
             throw new ArgumentNullException(nameof(dataLoaderFactory));
 
-        IDataLoader? loader;
+        return (TDataLoader)GetLoaders().GetOrAdd(loaderKey, _ => dataLoaderFactory());
+    }
 
-        lock (_loaders)
-        {
-            if (!_loaders.TryGetValue(loaderKey, out loader))
-            {
-                loader = dataLoaderFactory();
+    /// <summary>
+    /// Add a new data loader if one does not already exist with the provided key
+    /// </summary>
+    /// <typeparam name="TDataLoader">The type of <seealso cref="IDataLoader"/></typeparam>
+    /// <typeparam name="TValue">The type of the factory argument</typeparam>
+    /// <param name="loaderKey">Unique string to identify the <seealso cref="IDataLoader"/> instance</param>
+    /// <param name="factoryArgument">The argument to pass to the factory function</param>
+    /// <param name="dataLoaderFactory">Function to create the TDataLoader instance if it does not already exist. The factory receives a value of type TValue as a parameter.</param>
+    /// <returns>Returns an existing TDataLoader instance or a newly created instance if it did not exist already</returns>
+    public TDataLoader GetOrAdd<TDataLoader, TValue>(string loaderKey, TValue factoryArgument, Func<TValue, TDataLoader> dataLoaderFactory)
+        where TDataLoader : IDataLoader
+    {
+        if (loaderKey == null)
+            throw new ArgumentNullException(nameof(loaderKey));
 
-                _loaders.Add(loaderKey, loader);
-            }
-        }
+        if (dataLoaderFactory == null)
+            throw new ArgumentNullException(nameof(dataLoaderFactory));
 
-        return (TDataLoader)loader;
+#if NETSTANDARD2_1 || NETCOREAPP2_0_OR_GREATER
+        return (TDataLoader)GetLoaders().GetOrAdd(loaderKey, static (_, x) => x.dataLoaderFactory(x.factoryArgument), (dataLoaderFactory, factoryArgument));
+#else
+        return (TDataLoader)GetLoaders().GetOrAdd(loaderKey, _ => dataLoaderFactory(factoryArgument));
+#endif
     }
 }
