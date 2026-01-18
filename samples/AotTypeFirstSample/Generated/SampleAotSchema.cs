c97250369ff78df9e77ad8192184852686f1b722
Shane Krueger <shane@acdmail.com>
2026-01-18 17:35:26 -0500

    Update proof of concept

--- samples/AotTypeFirstSample/Generated/SampleAotSchema.cs
+++ samples/AotTypeFirstSample/Generated/SampleAotSchema.cs
@@ -1,4 +1,5 @@
 using GraphQL;
+using GraphQL.DI;
 using GraphQL.StarWars.TypeFirst.Types;
 using GraphQL.Types;
 using GraphQL.Types.Relay.DataObjects;
@@ -8,84 +9,82 @@ namespace AotSample;
 // sample schema that would be auto generated in AOT scenarios
 public partial class SampleAotSchema : AotSchema
 {
+    // source-generated constructor (when the developer does not provide one)
+    public SampleAotSchema(IServiceProvider services, IEnumerable<IConfigureSchema> configurations) : base(services, configurations)
+    {
+    }
+
     protected override void Configure(IServiceProvider services)
     {
-        AddAotType<AutoOutputGraphType_StarWarsQuery>();
+        // register constructors for:
+        //   1. all source-generated types, including those found during source generation of other types
+        //   2. GraphQL built-in types (ID, Int, String, Float, Boolean), except where overridden via [AotRemapType]
+        //   3. Any referenced graph types found during source generation, if they have only a default constructor
+        //   4. Any graph types generated via [AotInputType] or similar attributes
+        //   5. Any graph types generated or referenced via [AotQueryType] or similar attributes, that have only a default constructor
+        //   6. Any graph types referenced via [AotTypeMapping] or [AotRemapType] attributes, that have only a default constructor
+        AddAotType<AutoOutputGraphType_StarWarsQuery>(); // source-generated type specified for root Query type
+        AddAotType<AutoOutputGraphType_IStarWarsCharacter>(); // source-generated type referenced by StarWarsQuery
+        AddAotType<EnumerationGraphType<Episodes>>(); // source-generated type referenced by IStarWarsCharacter
+        AddAotType<AutoOutputGraphType_Connection_IStarWarsCharacter>(); // source-generated type referenced by IStarWarsCharacter
+        AddAotType<AutoOutputGraphType_Edge_IStarWarsCharacter>(); // source-generated type referenced by CharacterConnection
+        AddAotType<AutoOutputGraphType_PageInfo>(); // source-generated type referenced by CharacterConnection
+        AddAotType<AutoOutputGraphType_Droid>(); // source-generated type referenced by StarWarsQuery
+        AddAotType<AutoOutputGraphType_Human>(); // source-generated type referenced by StarWarsQuery
+        AddAotType<AutoOutputGraphType_StarWarsMutation>(); // source-generated type specified for root Mutation type
+        AddAotType<AutoInputGraphType_HumanInput>(); // source-generated input type referenced by StarWarsMutation
+        AddAotType<IntGraphType>(); // built-in type
+        AddAotType<StringGraphType>(); // built-in type
+        AddAotType<BooleanGraphType>(); // built-in type
+        AddAotType<FloatGraphType>(); // built-in type
+        AddAotType<IdGraphType>(); // built-in type
 
+        // register types within the schema for:
+        //   1. all source-generated types, including those found during source generation of other types
+        //   2. GraphQL built-in types (ID, Int, String, Float, Boolean), except where overridden via [AotRemapType]
+        //   3. Any referenced graph types found during source generation
+        //   4. Any graph types generated via [AotInputType] or similar attributes
+        //   5. Any graph types generated or referenced via [AotQueryType] or similar attributes
+        //   6. Any graph types referenced via [AotTypeMapping] or [AotRemapType] attributes
+        this.RegisterType<AutoOutputGraphType_StarWarsQuery>();
+        this.RegisterType<AutoOutputGraphType_IStarWarsCharacter>();
+        this.RegisterType<EnumerationGraphType<Episodes>>();
+        this.RegisterType<AutoOutputGraphType_Connection_IStarWarsCharacter>();
+        this.RegisterType<AutoOutputGraphType_Edge_IStarWarsCharacter>();
+        this.RegisterType<AutoOutputGraphType_PageInfo>();
+        this.RegisterType<AutoOutputGraphType_Droid>();
+        this.RegisterType<AutoOutputGraphType_Human>();
+        this.RegisterType<AutoOutputGraphType_StarWarsMutation>();
+        this.RegisterType<AutoInputGraphType_HumanInput>();
+        this.RegisterType<IntGraphType>();
+        this.RegisterType<StringGraphType>();
+        this.RegisterType<BooleanGraphType>();
+        this.RegisterType<FloatGraphType>();
+        this.RegisterType<IdGraphType>();
+
+        // register type mappings for:
+        //   1. all source-generated types excluding root types, including those found during source generation of other types
+        //   2. Any explicitly mapped types
+        //   3. Built-in type mappings
         this.RegisterTypeMapping<IStarWarsCharacter, AutoOutputGraphType_IStarWarsCharacter>();
-        AddAotType<AutoOutputGraphType_IStarWarsCharacter>();
         this.RegisterTypeMapping<Episodes, EnumerationGraphType<Episodes>>(); // uses AOT-safe reflection during initialization only
-        AddAotType<EnumerationGraphType<Episodes>>();
-        this.RegisterTypeMapping<Droid, AutoOutputGraphType_Droid>();
-        AddAotType<AutoOutputGraphType_Droid>();
-        this.RegisterTypeMapping<Human, AutoOutputGraphType_Human>();
-        AddAotType<AutoOutputGraphType_Human>();
-        this.RegisterTypeMapping<StarWarsMutation, AutoOutputGraphType_StarWarsMutation>();
-        AddAotType<AutoOutputGraphType_StarWarsMutation>();
-        this.RegisterTypeMapping<HumanInput, AutoInputGraphType_HumanInput>();
-        AddAotType<AutoInputGraphType_HumanInput>();
-
-        // register relay types
-        this.RegisterTypeMapping<PageInfo, AutoOutputGraphType_PageInfo>();
-        AddAotType<AutoOutputGraphType_PageInfo>();
-        this.RegisterTypeMapping<Edge<IStarWarsCharacter>, AutoOutputGraphType_Edge_IStarWarsCharacter>();
-        AddAotType<AutoOutputGraphType_Edge_IStarWarsCharacter>();
         this.RegisterTypeMapping<Connection<IStarWarsCharacter>, AutoOutputGraphType_Connection_IStarWarsCharacter>();
-        AddAotType<AutoOutputGraphType_Connection_IStarWarsCharacter>();
+        this.RegisterTypeMapping<Edge<IStarWarsCharacter>, AutoOutputGraphType_Edge_IStarWarsCharacter>();
+        this.RegisterTypeMapping<PageInfo, AutoOutputGraphType_PageInfo>();
+        this.RegisterTypeMapping<Droid, AutoOutputGraphType_Droid>();
+        this.RegisterTypeMapping<Human, AutoOutputGraphType_Human>();
+        this.RegisterTypeMapping<HumanInput, AutoInputGraphType_HumanInput>();
+        this.RegisterTypeMapping<int, IntGraphType>();
+        this.RegisterTypeMapping<string, StringGraphType>();
+        this.RegisterTypeMapping<bool, BooleanGraphType>();
+        this.RegisterTypeMapping<double, FloatGraphType>();
 
-        // register constructors for built-in types
-        AddAotType<IntGraphType>();
-        AddAotType<StringGraphType>();
-        AddAotType<BooleanGraphType>();
-        AddAotType<FloatGraphType>();
-        AddAotType<IdGraphType>();
+        // register list conversions for:
+        //   1. all input list fields identified during source-generation
+        //   2. all input list types specified via [AotListType] attributes
+        // (none in this sample)
 
-        // register list conversions (none needed in this sample)
-
-        // [AutoQueryType] is same as [AutoOutputType] plus it configures the Query root type
+        // configure root types when specified via [AotQueryType] or similar attributes
         Query = this.GetRequiredService<AutoOutputGraphType_StarWarsQuery>();
-    }
-
-    protected override SchemaTypesBase CreateSchemaTypes()
-    {
-        // add custom mapping provider that knows about generated types or type mappings registered via [HasType] attribute
-        // does not add mapping providers for non-standard built-in types or auto-registering types
-        // however, mappings required by introspection and GraphQL specification are still included
-        var graphTypeMappingProviders = this.GetService<IEnumerable<IGraphTypeMappingProvider>>();
-        var mappingProviders = graphTypeMappingProviders != null
-            ? graphTypeMappingProviders.Prepend(new GeneratedMappingProvider())
-            : [new GeneratedMappingProvider()];
-        return new SchemaTypes(this, this, mappingProviders, OnBeforeInitializeType);
-    }
-
-    private class GeneratedMappingProvider : IGraphTypeMappingProvider
-    {
-        public Type? GetGraphTypeFromClrType(Type clrType, bool isInputType, Type? preferredGraphType)
-        {
-            if (preferredGraphType != null)
-                return preferredGraphType;
-
-            // type dictated by attribute on SampleAotSchema class
-            if (clrType == typeof(StarWarsQuery) && !isInputType)
-            {
-                return typeof(AutoOutputGraphType_StarWarsQuery);
-            }
-
-            // type required by introspection
-            if (clrType == typeof(int))
-                return typeof(IntGraphType);
-            if (clrType == typeof(string))
-                return typeof(StringGraphType);
-            if (clrType == typeof(bool))
-                return typeof(BooleanGraphType);
-
-            // defined by GraphQL specification, so make that available too
-            if (clrType == typeof(double))
-                return typeof(FloatGraphType);
-
-            // any other types (including built-in ones) can be defined by the developer via [HasType]
-
-            return null;
-        }
     }
 }
--- samples/AotTypeFirstSample/SampleAotSchema.cs
+++ samples/AotTypeFirstSample/SampleAotSchema.cs
@@ -1,14 +1,9 @@
-using GraphQL.DI;
 using GraphQL.Types;
 
 namespace AotSample;
 
-//[AutoQueryType(typeof(StarWarsQuery))] // will discover all related type-first types
-// other attributes available: [AutoMutationType], [AutoSubscriptionType], [AutoOutputType], [AutoInputType], [HasType]
+//[AotQueryType(typeof(StarWarsQuery))] // will discover all related type-first types
+// other attributes available: [AotMutationType], [AotSubscriptionType], [AotOutputType], [AotInputType], [AotGraphType], [AotInterfaceType], [AotUnionType], [AotTypeMapping]
 public partial class SampleAotSchema : AotSchema
 {
-    // ctor is available for developer's use, and code runs after construction in generated Configure method
-    public SampleAotSchema(IServiceProvider services, IEnumerable<IConfigureSchema> configurations) : base(services, configurations)
-    {
-    }
 }
