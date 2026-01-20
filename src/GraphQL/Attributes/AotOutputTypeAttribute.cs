c26e6a009c3fc2f175335418c43f817636e65c76
Shane Krueger <shane@acdmail.com>
2026-01-20 02:03:57 -0500

    Add IsInterface to AotObjectType (#4292)

--- src/GraphQL/Attributes/AotOutputTypeAttribute.cs
+++ src/GraphQL/Attributes/AotOutputTypeAttribute.cs
@@ -1,10 +1,17 @@
 namespace GraphQL;
 
 /// <summary>
-/// Specifies a CLR type that should be used to generate an <see cref="Types.ObjectGraphType{TSourceType}"/> during AOT schema compilation.
+/// Specifies a CLR type that should be used to generate an <see cref="Types.ObjectGraphType{TSourceType}"/> or <see cref="Types.InterfaceGraphType{TSourceType}"/> during AOT schema compilation.
 /// This attribute can be applied multiple times to specify multiple output types.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
 public sealed class AotOutputTypeAttribute<[NotAGraphType] T> : AotSchemaAttribute
 {
+    /// <summary>
+    /// Gets or sets whether the CLR type should be treated as an interface type.
+    /// When <see langword="true"/>, generates an <see cref="Types.InterfaceGraphType{TSourceType}"/>.
+    /// When <see langword="false"/>, generates an <see cref="Types.ObjectGraphType{TSourceType}"/>.
+    /// When <see langword="null"/> (default), the type will be auto-detected based on whether the CLR type is an interface.
+    /// </summary>
+    public bool? IsInterface { get; set; }
 }
