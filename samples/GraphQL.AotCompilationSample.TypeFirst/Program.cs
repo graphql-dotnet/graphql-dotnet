69a7c62fc3b6f4c92bfae6a26478fb5fab4ab39a
Shane Krueger <shane@acdmail.com>
2026-01-29 01:11:50 -0500

    Enable AOT type first sample

--- samples/GraphQL.AotCompilationSample.TypeFirst/Program.cs
+++ samples/GraphQL.AotCompilationSample.TypeFirst/Program.cs
@@ -3,7 +3,6 @@ using GraphQL;
 using GraphQL.StarWars.TypeFirst;
 using GraphQL.StarWars.TypeFirst.Types;
 using GraphQL.Types;
-using GraphQL.Types.Relay.DataObjects;
 using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Sample of AOT compilation of a GraphQL query on a type-first schema");
@@ -11,33 +10,12 @@ Console.WriteLine();
 
 IServiceCollection serviceCollection = new ServiceCollection();
 
-#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
-serviceCollection.AddGraphQL(b => b
+serviceCollection.AddGraphQLAot(b => b
     .AddSystemTextJsonAot()
-    .AddAutoSchema<StarWarsQuery>(c => c.WithMutation<StarWarsMutation>())
-    .ConfigureSchema(s =>
-    {
-        // All CLR types for the schema (within GraphQL.StarWars.TypeFirst) must be rooted in the csproj
-        // file via TrimmerRootAssembly or else they will be trimmed by the linker, and the auto-registering
-        // graph types will not find any properties/methods to register. However, any services such as the
-        // StarWarsData service do not need to be rooted in the csproj file, as the linker will intelligently
-        // preserve the service's constructor (due to AddSingleton) and any methods that are called on it.
-
-        // For enumeration types, and types not in the GraphQL.StarWars.TypeFirst assembly, manually calling
-        // RegisterTypeMapping or AutoRegister will root the proper classes necessary for the schema to work.
-        s.RegisterTypeMapping<Episodes, EnumerationGraphType<Episodes>>();
-        s.AutoRegister<Connection<IStarWarsCharacter>>();
-        s.AutoRegister<Edge<IStarWarsCharacter>>();
-        s.AutoRegister<PageInfo>();
-    })
+    .AddSchema<StarWarsSchema>()
 );
-#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
 
 serviceCollection.AddSingleton<StarWarsData>();
-
-// for enumeration types, although the EnumerationGraphType<Episodes> type has been properly rooted, the
-// .NET 8 DI provider will refuse to create open generic types of value types, so they must be registered manually
-serviceCollection.AddTransient<EnumerationGraphType<Episodes>>();
 
 using var services = serviceCollection.BuildServiceProvider();
 
@@ -91,4 +69,11 @@ static string LoadResource(string resourceName)
     using var stream = typeof(Program).Assembly.GetManifestResourceStream("GraphQL.AotCompilationSample.TypeFirst." + resourceName)!;
     using var reader = new StreamReader(stream);
     return reader.ReadToEnd();
+}
+
+[AotQueryType<StarWarsQuery>]
+[AotMutationType<StarWarsMutation>]
+[AotOutputType<IStarWarsCharacter>]
+internal partial class StarWarsSchema : AotSchema
+{
 }
