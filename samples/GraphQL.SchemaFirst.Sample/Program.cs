300f9a67f1856871fb8d0a2fb2668b58ce984cc7
yuliuyi717-ux <264093635+yuliuyi717-ux@users.noreply.github.com>
2026-02-28 02:19:12 +0800

    Add schema-first sample and sample index documentation

---
+++ samples/GraphQL.SchemaFirst.Sample/Program.cs
@@ -0,0 +1,50 @@
+using System.Text.Json;
+using GraphQL;
+using GraphQL.Types;
+
+Console.WriteLine("GraphQL.NET Schema-First sample");
+Console.WriteLine();
+
+var schema = Schema.For(
+    """
+    type Droid {
+      id: String!
+      name: String!
+    }
+
+    type Query {
+      hero: Droid
+    }
+    """,
+    builder => builder.Types.Include<Query>());
+
+var json = await schema.ExecuteAsync(options =>
+{
+    options.Query = "{ hero { id name } }";
+}).ConfigureAwait(false);
+
+Console.WriteLine(json);
+
+using var document = JsonDocument.Parse(json);
+var hero = document.RootElement.GetProperty("data").GetProperty("hero");
+
+if (hero.GetProperty("id").GetString() != "1" ||
+    hero.GetProperty("name").GetString() != "R2-D2")
+{
+    Console.WriteLine("Unexpected schema-first sample result.");
+    return 1;
+}
+
+return 0;
+
+public sealed class Query
+{
+    [GraphQLMetadata("hero")]
+    public Droid GetHero() => new() { Id = "1", Name = "R2-D2" };
+}
+
+public sealed class Droid
+{
+    public string Id { get; init; } = "";
+    public string Name { get; init; } = "";
+}
