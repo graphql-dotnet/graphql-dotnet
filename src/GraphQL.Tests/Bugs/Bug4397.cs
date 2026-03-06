51b6469f74a949f848f2af54794917566b458c2d
Shane Krueger <shane@acdmail.com>
2026-03-06 00:25:57 -0500

    Updates

--- src/GraphQL.Tests/Bugs/Bug4397.cs
+++ src/GraphQL.Tests/Bugs/Bug4397.cs
@@ -39,6 +39,39 @@ public class Bug4397
             """);
     }
 
+    [Fact]
+    public async Task Union_field_with_inline_fragment_on_union_type_with_aliased_field_with_arg()
+    {
+        var schema = new Schema { Query = new Bug4397Query() };
+        schema.RegisterType<Bug4397Type1>();
+
+        var result = await schema.ExecuteAsync(o => o.Query = """
+            {
+              union1 {
+                __typename
+                ... on Union1 {
+                  ... on Type1 {
+                    field1
+                    field2: field1(arg1: "abc")
+                  }
+                }
+              }
+            }
+            """);
+
+        result.ShouldBeCrossPlatJson("""
+            {
+                "data": {
+                    "union1": {
+                        "__typename": "Type1",
+                        "field1": "field1value",
+                        "field2": "abc"
+                    }
+                }
+            }
+            """);
+    }
+
     public class Bug4397Query : ObjectGraphType
     {
         public Bug4397Query()
