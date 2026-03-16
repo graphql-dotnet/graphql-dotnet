e5d0a0e47cd0523a4fdb587f33ba5407ca0badd3
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-17 00:54:13 +0400

    Update src\GraphQL.SchemaFirstDemo\Models\Genre.cs

--- src\GraphQL.SchemaFirstDemo\Models\Genre.cs
+++ src\GraphQL.SchemaFirstDemo\Models\Genre.cs
@@ -1,5 +1,8 @@
 namespace GraphQL.SchemaFirstDemo.Models;
 
+/// <summary>
+/// Book genre enum.  The values must match the SDL enum values (case-insensitive).
+/// </summary>
 public enum Genre
 {
     Fiction,
